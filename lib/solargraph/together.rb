6e78215ce6b2a9b659c18ac2ab3d9384c735e679
Fred Snyder <fsnyder@castwide.com>
2017-03-04 03:55:56 -0500

    Removed test file.

--- lib/solargraph/together.rb
+++
@@ -1,5 +0,0 @@
-`./bin/solargraph-ruby scope test.rb > test.stub`
-info = JSON.parse(`./bin/solargraph-ruby info test.rb -i 40`)
-if info['instance_method']
-  puts `./bin/solargraph-ruby instance-methods #{info['namespace']} -a private`
-end
