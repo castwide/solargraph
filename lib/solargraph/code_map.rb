d6f79e9196193f31372c8b0bb1df40f534bc23b9
Fred Snyder <fsnyder@castwide.com>
2017-05-11 12:17:49 -0400

    Disabled suggestion filter.

--- lib/solargraph/code_map.rb
+++ lib/solargraph/code_map.rb
@@ -189,7 +189,9 @@ module Solargraph
         type = infer(node_at(index - 2))
         return @api_map.get_instance_methods(type) unless type.nil?
       end
+      STDERR.puts "Getting suggestions for #{signature}"
       if signature.start_with?('@')
+        STDERR.puts "Okay, it's an instance variable"
         parts = signature.split('.')
         var = parts.shift
         if parts.length > 0 or signature.end_with?('.')
@@ -240,7 +242,7 @@ module Solargraph
           end
         end
       end
-      result = reduce_starting_with(result, word_at(index)) if filtered
+      #result = reduce_starting_with(result, word_at(index)) if filtered
       result.uniq{|s| s.path}
     end
 
