3bb3726cb1c70ab1fa25d8374998e328a44ad350
Fred Snyder <fsnyder@castwide.com>
2017-09-16 19:23:24 -0400

    SymbolPin label correction and specs.

--- lib/solargraph/api_map/symbol_pin.rb
+++ lib/solargraph/api_map/symbol_pin.rb
@@ -15,7 +15,7 @@ module Solargraph
       private
 
       def generate_suggestion
-        label = "#{node.children[0]}"
+        label = ":#{node.children[0]}"
         Suggestion.new(label, insert: label, kind: Suggestion::CONSTANT, documentation: nil, detail: 'Symbol', return_type: 'Symbol')
       end
     end
