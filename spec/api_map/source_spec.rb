9876f424f19fb400a5165507a4c544cbd48eb9c2
Fred Snyder <fsnyder@castwide.com>
2017-10-27 06:25:59 -0400

    Test attributes in YARD directives.

--- spec/api_map/source_spec.rb
+++ spec/api_map/source_spec.rb
@@ -6,6 +6,7 @@ describe Solargraph::ApiMap::Source do
     source = Solargraph::ApiMap::Source.virtual('file.rb', code)
     expect(source.required).to include('solargraph')
   end
+
   it "ignores dynamic require calls" do
     code = %(
       path = 'solargraph'
@@ -13,5 +14,17 @@ describe Solargraph::ApiMap::Source do
     )
     source = Solargraph::ApiMap::Source.virtual('file.rb', code)
     expect(source.required.length).to eq(0)
+  end
+
+  it "finds attributes in YARD directives" do
+    code = %(
+      class Foo
+        # @!attribute [r] bar
+        #   @return [String]
+      end
+    )
+    source = Solargraph::ApiMap::Source.virtual('file.rb', code)
+    expect(source.attribute_pins.length).to eq(1)
+    expect(source.attribute_pins[0].name).to eq('bar')
   end
 end
