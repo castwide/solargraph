6f534510acc979753e242359a154bc36b8c0f7e1
Fred Snyder <fsnyder@castwide.com>
2017-11-14 00:46:11 -0500

    Deprecated namespaces.

--- lib/solargraph.rb
+++ lib/solargraph.rb
@@ -3,9 +3,7 @@ require 'rubygems/package'
 require 'yard-solargraph'
 
 module Solargraph
-  autoload :Analyzer,      'solargraph/analyzer'
   autoload :Shell,         'solargraph/shell'
-  autoload :LiveParser,    'solargraph/live_parser'
   autoload :ApiMap,        'solargraph/api_map'
   autoload :CodeMap,       'solargraph/code_map'
   autoload :NodeMethods,   'solargraph/node_methods'
