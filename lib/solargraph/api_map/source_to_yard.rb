27223b6c79d214e20082366aac673d66050e5cd8
Fred Snyder <fsnyder@castwide.com>
2017-12-10 16:05:02 -0500

    Track files in code objects.

--- lib/solargraph/api_map/source_to_yard.rb
+++ lib/solargraph/api_map/source_to_yard.rb
@@ -3,7 +3,7 @@ module Solargraph
     module SourceToYard
 
       private
-      
+
       # Get the YARD CodeObject at the specified path.
       #
       # @return [YARD::CodeObjects::Base]
@@ -29,12 +29,11 @@ module Solargraph
           s.namespace_pins.each do |pin|
             if pin.kind == Solargraph::Suggestion::CLASS
               code_object_map[pin.path] ||= YARD::CodeObjects::ClassObject.new(root_code_object, pin.path)
-              # @type = [YARD::CodeObjects::ClassObject]
-              o = nil
             else
               code_object_map[pin.path] ||= YARD::CodeObjects::ModuleObject.new(root_code_object, pin.path)
             end
             code_object_map[pin.path].docstring = pin.docstring unless pin.docstring.nil?
+            code_object_map[pin.path].files.push pin.source.filename
           end
           s.namespace_includes.each_pair do |n, i|
             code_object_map[n].mixins.push code_object_map[i] unless code_object_map[i].nil?
@@ -42,11 +41,13 @@ module Solargraph
           s.attribute_pins.each do |pin|
             code_object_map[pin.path] ||= YARD::CodeObjects::MethodObject.new(code_object_at(pin.namespace), pin.name, :instance)
             code_object_map[pin.path].docstring = pin.docstring unless pin.docstring.nil?
+            code_object_map[pin.path].files.push pin.source.filename
             #code_object_map[pin.path].parameters = []
           end
           s.method_pins.each do |pin|
             code_object_map[pin.path] ||= YARD::CodeObjects::MethodObject.new(code_object_at(pin.namespace), pin.name, pin.scope)
             code_object_map[pin.path].docstring = pin.docstring unless pin.docstring.nil?
+            code_object_map[pin.path].files.push pin.source.filename
             code_object_map[pin.path].parameters = pin.parameters.map do |p|
               n = p.match(/^[a-z0-9\-]*?:?/i)[0]
               v = nil
