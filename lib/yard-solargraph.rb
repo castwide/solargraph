88b578d756fe5d1f50195b99f7cac1b6a0637317
Fred Snyder <fsnyder@castwide.com>
2018-01-22 16:11:07 -0500

    Define @yieldself tag.

--- lib/yard-solargraph.rb
+++ lib/yard-solargraph.rb
@@ -1,4 +1,6 @@
 require 'yard'
 
-# Define a @type tag to be used for documenting variables
+# Define a @type tag for documenting variables
 YARD::Tags::Library.define_tag("Type", :type, :with_types_and_name)
+# Define a @yieldself tag for documenting block contexts
+YARD::Tags::Library.define_tag("Yieldself", :yieldself, :with_types)
