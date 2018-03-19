2c3dde673f8aee3245a8b5e5ae5f711588be9702
Fred Snyder <fsnyder@castwide.com>
2018-03-18 21:46:58 -0400

    CodeMap.get_offset -> Source.get_offset

--- lib/solargraph/pin/conversions.rb
+++ lib/solargraph/pin/conversions.rb
@@ -54,7 +54,6 @@ module Solargraph
       private
 
       def completion_item_detail
-        STDERR.puts "Parameters for #{name}: #{parameters.join(', ')}"
         detail = ''
         detail += "(#{parameters.join(', ')}) " unless parameters.empty?
         detail += "=> #{return_type}" unless return_type.nil?
