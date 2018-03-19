662856cfbec8441c1ded575295aac08681cf3896
Fred Snyder <fsnyder@castwide.com>
2018-03-18 22:14:00 -0400

    Code formatting.

--- lib/solargraph/language_server/message.rb
+++ lib/solargraph/language_server/message.rb
@@ -5,17 +5,17 @@ require 'thread'
 module Solargraph
   module LanguageServer
     module Message
-      autoload :Base, 'solargraph/language_server/message/base'
-      autoload :Initialize, 'solargraph/language_server/message/initialize'
-      autoload :Initialized, 'solargraph/language_server/message/initialized'
-      autoload :TextDocument, 'solargraph/language_server/message/text_document'
-      autoload :CompletionItem, 'solargraph/language_server/message/completion_item'
-      autoload :CancelRequest, 'solargraph/language_server/message/cancel_request'
-      autoload :MethodNotFound, 'solargraph/language_server/message/method_not_found'
+      autoload :Base,                 'solargraph/language_server/message/base'
+      autoload :Initialize,           'solargraph/language_server/message/initialize'
+      autoload :Initialized,          'solargraph/language_server/message/initialized'
+      autoload :TextDocument,         'solargraph/language_server/message/text_document'
+      autoload :CompletionItem,       'solargraph/language_server/message/completion_item'
+      autoload :CancelRequest,        'solargraph/language_server/message/cancel_request'
+      autoload :MethodNotFound,       'solargraph/language_server/message/method_not_found'
       autoload :MethodNotImplemented, 'solargraph/language_server/message/method_not_implemented'
-      autoload :Extended, 'solargraph/language_server/message/extended'
-      autoload :Shutdown, 'solargraph/language_server/message/shutdown'
-      autoload :ExitNotification, 'solargraph/language_server/message/exit_notification'
+      autoload :Extended,             'solargraph/language_server/message/extended'
+      autoload :Shutdown,             'solargraph/language_server/message/shutdown'
+      autoload :ExitNotification,     'solargraph/language_server/message/exit_notification'
 
       class << self
         def register path, message_class
@@ -42,23 +42,23 @@ module Solargraph
         end
       end
 
-      register 'initialize', Initialize
-      register 'initialized', Initialized
-      register 'textDocument/completion', TextDocument::Completion
-      register 'completionItem/resolve', CompletionItem::Resolve
-      register 'textDocument/signatureHelp', TextDocument::SignatureHelp
-      register 'textDocument/didOpen', TextDocument::DidOpen
-      register 'textDocument/didChange', TextDocument::DidChange
-      register 'textDocument/didSave', TextDocument::DidSave
-      register 'textDocument/didClose', TextDocument::DidClose
-      register 'textDocument/hover', TextDocument::Hover
-      register 'textDocument/definition', TextDocument::Definition
+      register 'initialize',                    Initialize
+      register 'initialized',                   Initialized
+      register 'textDocument/completion',       TextDocument::Completion
+      register 'completionItem/resolve',        CompletionItem::Resolve
+      register 'textDocument/signatureHelp',    TextDocument::SignatureHelp
+      register 'textDocument/didOpen',          TextDocument::DidOpen
+      register 'textDocument/didChange',        TextDocument::DidChange
+      register 'textDocument/didSave',          TextDocument::DidSave
+      register 'textDocument/didClose',         TextDocument::DidClose
+      register 'textDocument/hover',            TextDocument::Hover
+      register 'textDocument/definition',       TextDocument::Definition
       register 'textDocument/onTypeFormatting', TextDocument::OnTypeFormatting
-      register '$/cancelRequest', CancelRequest
-      register '$/solargraph/document', Extended::Document
-      register '$/solargraph/search', Extended::Search
-      register 'shutdown', Shutdown
-      register 'exit', ExitNotification
+      register '$/cancelRequest',               CancelRequest
+      register '$/solargraph/document',         Extended::Document
+      register '$/solargraph/search',           Extended::Search
+      register 'shutdown',                      Shutdown
+      register 'exit',                          ExitNotification
     end
   end
 end
