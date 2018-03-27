cee1f54e2d508ee8a773b605224897535ff84ae4
Fred Snyder <fsnyder@castwide.com>
2018-03-27 12:14:11 -0400

    OnTypeFormatting message uses Library.

--- lib/solargraph/language_server/message/text_document/on_type_formatting.rb
+++ lib/solargraph/language_server/message/text_document/on_type_formatting.rb
@@ -4,7 +4,7 @@ module Solargraph
       module TextDocument
         class OnTypeFormatting < Base
           def process
-            src = host.read(params['textDocument']['uri'])
+            src = host.library.source(uri_to_file(params['textDocument']['uri']))
             offset = src.get_offset(params['position']['line'], params['position']['character'])
             if src.string_at?(offset-1) and params['ch'] == '{' and src.code[offset-2,2] == '#{'
               set_result(
