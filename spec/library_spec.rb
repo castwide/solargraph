c8f922816e1f89a1180c7d00347f3f240442c816
Fred Snyder <fsnyder@castwide.com>
2018-03-31 10:21:19 -0400

    Library#create spec.

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,4 +5,12 @@ describe Solargraph::Library do
       library.checkout 'invalid_filename.rb'
     }.to raise_error(Solargraph::Library::FileNotFoundError)
   end
+
+  it "can read a created file" do
+    library = Solargraph::Library.new
+    library.create('file.rb', 'a = b')
+    expect {
+      library.checkout 'file.rb'
+    }.not_to raise_error
+  end
 end
