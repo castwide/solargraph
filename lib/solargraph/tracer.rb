9770461c0d4ac32c95d07fea1b204111071359b3
Fred Snyder <fsnyder@castwide.com>
2018-04-04 23:00:28 -0400

    Tracer#analyze method.

--- lib/solargraph/tracer.rb
+++ lib/solargraph/tracer.rb
@@ -66,25 +66,29 @@ module Solargraph
             point_cache.push key
             abspath = File.absolute_path(tp.path)
             if workspace.has_file?(abspath)
-              actual = tp.return_value.class.to_s
               source = workspace.source(abspath)
               fragment = source.fragment_at(tp.lineno - 1, 0)
               pin = source.method_pins.select{|pin| pin.name == tp.method_id.to_s and pin.namespace == fragment.namespace and pin.scope == fragment.scope}.first
               if pin.nil?
-                log_cache.push Issue.new(:warning, "Method `#{tp.method_id}` could not be found", tp.method_id, nil, actual, caller)
+                log_cache.push Issue.new(:warning, "Method `#{tp.method_id}` could not be found", tp.method_id, nil, tp.return_value.class.to_s, caller)
               else
-                pin.resolve api_map
-                expected = pin.return_type
-                if expected.nil?
-                  log_cache.push Issue.new(:warning, "`#{pin.path}` does not have a return type", tp.method_id, nil, actual, caller)
-                elsif expected != actual
-                  log_cache.push Issue.new(:error, "`#{pin.path}` should return #{expected} but returned #{actual}", tp.method_id, expected, actual, caller)
-                end
+                analyze pin, tp.return_value, caller
               end
             end
           end
           @tracing = false
         end
+      end
+    end
+
+    def analyze pin, value, backtrace
+      return if pin.name == 'initialize' and pin.scope == :instance
+      actual = value.class.to_s
+      pin.resolve api_map
+      if pin.return_type.nil?
+        log_cache.push Issue.new(:warning, "`#{pin.path}` does not have a return type and returned #{actual}", pin.name, nil, actual, backtrace)
+      elsif pin.return_type != actual
+        log_cache.push Issue.new(:error, "`#{pin.path}` should return #{pin.return_type} but returned #{actual}", pin.name, pin.return_type, actual, backtrace)
       end
     end
   end
