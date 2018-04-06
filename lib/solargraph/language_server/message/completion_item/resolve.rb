eb3e6f1e78e72e54a418066840b178709b908f2a
Fred Snyder <fsnyder@castwide.com>
2018-04-06 14:32:26 -0400

    Unresolved pins do not emit errors.

--- lib/solargraph/language_server/message/completion_item/resolve.rb
+++ lib/solargraph/language_server/message/completion_item/resolve.rb
@@ -9,7 +9,8 @@ module Solargraph
           def process
             pin = host.locate_pin params
             if pin.nil?
-              set_error(Solargraph::LanguageServer::ErrorCodes::INVALID_REQUEST, "Completion item could not be resolved")
+              # set_error(Solargraph::LanguageServer::ErrorCodes::INVALID_REQUEST, "Completion item could not be resolved")
+              set_result params
             else
               set_result(
                 params.merge(pin.resolve_completion_item)
