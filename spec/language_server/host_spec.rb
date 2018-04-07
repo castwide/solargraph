f48469256b8f1d50f48019e917f98b6959c53de3
Fred Snyder <fsnyder@castwide.com>
2018-04-07 01:51:18 -0400

    Changed host spec.

--- spec/language_server/host_spec.rb
+++ spec/language_server/host_spec.rb
@@ -5,7 +5,8 @@ describe Solargraph::LanguageServer::Host do
     host = Solargraph::LanguageServer::Host.new
     Dir.mktmpdir do |dir|
       host.prepare (dir)
-      expect(host.library).not_to be(nil)
+      # @todo Change this test or get rid of it. The library is private now.
+      expect(host.send(:library)).not_to be(nil)
     end
   end
 end
