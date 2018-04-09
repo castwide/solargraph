53ebba900f5e76f12f57433796d28f80977033c9
Fred Snyder <fsnyder@castwide.com>
2018-04-09 05:46:55 -0400

    Enabled textDocument/hover.

--- lib/solargraph/language_server/message/text_document/hover.rb
+++ lib/solargraph/language_server/message/text_document/hover.rb
@@ -8,24 +8,23 @@ module Solargraph::LanguageServer::Message::TextDocument
         line = params['position']['line']
         col = params['position']['character']
         contents = []
-        # @todo Temporarily disabled
-        # suggestions = host.definitions_at(filename, line, col)
-        # last_path = nil
-        # suggestions.each do |pin|
-        #   parts = []
-        #   this_path = nil
-        #   if pin.kind_of?(Solargraph::Pin::BaseVariable)
-        #     this_path = pin.return_type
-        #   else
-        #     this_path = pin.path
-        #   end
-        #   if !this_path.nil? and this_path != last_path
-        #     parts.push link_documentation(this_path)
-        #   end
-        #   parts.push pin.documentation unless pin.documentation.nil? or pin.documentation.empty?
-        #   contents.push parts.join("\n\n") unless parts.empty?
-        #   last_path = this_path unless this_path.nil?
-        # end
+        suggestions = host.definitions_at(filename, line, col)
+        last_path = nil
+        suggestions.each do |pin|
+          parts = []
+          this_path = nil
+          if pin.kind_of?(Solargraph::Pin::BaseVariable)
+            this_path = pin.return_type
+          else
+            this_path = pin.path
+          end
+          if !this_path.nil? and this_path != last_path
+            parts.push link_documentation(this_path)
+          end
+          parts.push pin.documentation unless pin.documentation.nil? or pin.documentation.empty?
+          contents.push parts.join("\n\n") unless parts.empty?
+          last_path = this_path unless this_path.nil?
+        end
         set_result(
           contents: {
             kind: 'markdown',
