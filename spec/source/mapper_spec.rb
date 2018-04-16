c551d0818ba92f76ab9bd56b0553351489942c23
Fred Snyder <fsnyder@castwide.com>
2018-04-16 07:47:14 -0400

    Mapper include/extend specs.

--- spec/source/mapper_spec.rb
+++ spec/source/mapper_spec.rb
@@ -14,4 +14,30 @@ describe Solargraph::Source::Mapper do
     bar_pin = source.pins.select{|pin| pin.path == 'Foo::Bar.new'}.first
     expect(bar_pin.return_type).to eq('Foo::Bar')
   end
+
+  it "ignores include calls that are not attached to the current namespace" do
+    source = Solargraph::Source.new(%(
+      class Foo
+        include Direct
+        xyz.include Indirect
+        xyz(include Indirect)
+      end
+    ))
+    foo_pin = source.pins.select{|pin| pin.path == 'Foo'}.first
+    expect(foo_pin.include_references.map(&:name)).to include('Direct')
+    expect(foo_pin.include_references.map(&:name)).not_to include('Indirect')
+  end
+
+  it "ignores extend calls that are not attached to the current namespace" do
+    source = Solargraph::Source.new(%(
+      class Foo
+        extend Direct
+        xyz.extend Indirect
+        xyz(extend Indirect)
+      end
+    ))
+    foo_pin = source.pins.select{|pin| pin.path == 'Foo'}.first
+    expect(foo_pin.extend_references.map(&:name)).to include('Direct')
+    expect(foo_pin.extend_references.map(&:name)).not_to include('Indirect')
+  end
 end
