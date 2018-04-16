3f3e06f8bafd4f9f6309be95d9270cfd348ab77d
Fred Snyder <fsnyder@castwide.com>
2018-04-16 15:15:35 -0400

    Minor refactoring.

--- lib/solargraph/pin/localized.rb
+++ lib/solargraph/pin/localized.rb
@@ -2,17 +2,17 @@ module Solargraph
   module Pin
     module Localized
       attr_reader :block
+
+      # @return [Source::Range]
       attr_reader :presence
 
-      def visible_from?(block, position)
-        in_context?(block) and presence.contain?(position)
-      end
-
-      private
-
-      def in_context?(other)
-        return false if other.filename != filename
-        other == block or other.named_context == named_context
+      # @param other [Pin::Block] The caller's block
+      # @param position [Source::Position] The caller's position
+      # @return [Boolean]
+      def visible_from?(other, position)
+        other.filename == filename and
+          (other == block or other.named_context == named_context) and
+          presence.contain?(position)
       end
     end
   end
