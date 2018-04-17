efb2c40fab12843602ea9a463a7f870cb15e7b9e
Fred Snyder <fsnyder@castwide.com>
2018-04-17 09:43:56 -0400

    Check keys during configuration change

--- lib/solargraph/language_server/message/workspace/did_change_configuration.rb
+++ lib/solargraph/language_server/message/workspace/did_change_configuration.rb
@@ -6,17 +6,17 @@ module Solargraph::LanguageServer::Message::Workspace
       update = params['settings']['solargraph']
 
       meths = []
-      meths.push 'textDocument/completion' if update['completion'] and !host.options['completion']
-      meths.push 'textDocument/hover' if update['hover']  and !host.options['hover']
-      meths.push 'textDocument/signatureHelp' if update['hover'] and !host.options['hover']
-      meths.push 'textDocument/onTypeFormatting' if update['autoformat']  and !host.options['autoformat']
+      meths.push 'textDocument/completion' if update.has_key?('completion') and update['completion'] and !host.options['completion']
+      meths.push 'textDocument/hover' if update.has_key?('hover') and update['hover']  and !host.options['hover']
+      meths.push 'textDocument/signatureHelp' if update.has_key?('hover') and update['hover'] and !host.options['hover']
+      meths.push 'textDocument/onTypeFormatting' if update.has_key?('autoformat') and update['autoformat'] and !host.options['autoformat']
       host.register_capabilities meths unless meths.empty?
 
       meths = []
-      meths.push 'textDocument/completion' if !update['completion'] and host.options['completion']
-      meths.push 'textDocument/hover' if !update['hover'] and host.options['hover']
-      meths.push 'textDocument/signatureHelp' if !update['hover'] and host.options['hover']
-      meths.push 'textDocument/onTypeFormatting' if !update['autoformat'] and host.options['autoformat']
+      meths.push 'textDocument/completion' if update.has_key?('completion') and !update['completion'] and host.options['completion']
+      meths.push 'textDocument/hover' if update.has_key?('hover') and !update['hover'] and host.options['hover']
+      meths.push 'textDocument/signatureHelp' if update.has_key?('hover') and !update['hover'] and host.options['hover']
+      meths.push 'textDocument/onTypeFormatting' if update.has_key?('autoformat') and !update['autoformat'] and host.options['autoformat']
       host.unregister_capabilities meths unless meths.empty?
 
       host.configure params['settings']['solargraph']
