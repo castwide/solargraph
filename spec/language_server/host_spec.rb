b5a42890b52c7347dc561b1d0c25cb44236cc5da
Fred Snyder <fsnyder@castwide.com>
2018-04-18 23:31:50 -0400

    Host spec for responses from client to server.

--- spec/language_server/host_spec.rb
+++ spec/language_server/host_spec.rb
@@ -9,4 +9,21 @@ describe Solargraph::LanguageServer::Host do
       expect(host.send(:library)).not_to be(nil)
     end
   end
+
+  it "receives responses to message requests" do
+    host = Solargraph::LanguageServer::Host.new
+    done_somethings = 0
+    host.send_request 'window/showMessageRequest', {
+      'message' => 'Message',
+      'actions' => ['Do something']
+    } do |response|
+      done_somethings += 1 if response == 'Do something'
+    end
+    # Assuming the ID is 0 because it's the first message
+    host.start({
+      'id' => 0,
+      'result' => 'Do something'
+    })
+    expect(done_somethings).to eq(1)
+  end
 end
