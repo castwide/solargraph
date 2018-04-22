0bb70bb63ab67c968272b869ee24dfdefac37b9e
Sam Killgallon <Sam-Killgallon@users.noreply.github.com>
2018-04-22 20:53:32 +0100

    Follow symlinks in specs (#45)
    * Dry up workspace specs
    
    * Use File.realpath in Solargraph::Workspace specs to match the behaviour, which resolves symlinks
    
    On my mac tmp files are stored under /var/ which is symlinked to
    /private/var. Solargraph::Workspace::Config#process_globs uses
    the File.realdirpath method but the tests dont, so they fail because the
    paths are different.
    
    * Dry up workspace specs
    
    * Dry up workspace specs
    
    * Extract some setup for Solargraph::Workspace::Config specs
    
    * Use File.realpath in Solargraph::Workspace::Config specs to match the behaviour, which resolves symlinks in the workspace
    
    * Use File.realpath in Solargraph::Library specs to match the behaviour, which resolves symlinks in the workspace
    
    * Fix syntax error

--- spec/api_map/config_spec.rb
+++ spec/api_map/config_spec.rb
@@ -1,23 +1,22 @@
 require 'tmpdir'
 
 describe Solargraph::Workspace::Config do
+  let(:config) { described_class.new(workspace_path) }
+  let(:workspace_path) { File.realpath(Dir.mktmpdir) }
+
+  after(:each) { FileUtils.remove_entry(workspace_path) }
+
   it "reads workspace files from config" do
-    Dir.mktmpdir do |dir|
-      File.open(File.join(dir, 'foo.rb'), 'w') do |file|
-        file << 'test'
-      end
-      File.open(File.join(dir, 'bar.rb'), 'w') do |file|
-        file << 'test'
-      end
-      File.open(File.join(dir, '.solargraph.yml'), 'w') do |file|
-        file.puts "include:"
-        file.puts "  - foo.rb"
-        file.puts "exclude:"
-        file.puts "  - bar.rb"
-      end
-      config = Solargraph::Workspace::Config.new(dir)
-      expect(config.included).to eq([File.join(dir, 'foo.rb')])
-      expect(config.excluded).to eq([File.join(dir, 'bar.rb')])
+    File.write(File.join(workspace_path, 'foo.rb'), 'test')
+    File.write(File.join(workspace_path, 'bar.rb'), 'test')
+    File.open(File.join(workspace_path, '.solargraph.yml'), 'w') do |file|
+      file.puts "include:"
+      file.puts "  - foo.rb"
+      file.puts "exclude:"
+      file.puts "  - bar.rb"
     end
+
+    expect(config.included).to eq([File.join(workspace_path, 'foo.rb')])
+    expect(config.excluded).to eq([File.join(workspace_path, 'bar.rb')])
   end
 end
