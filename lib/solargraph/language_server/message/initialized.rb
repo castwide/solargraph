894ad418d105b03bb2a990f3e25526d527689c83
Fred Snyder <fsnyder@castwide.com>
2018-04-25 08:36:02 -0400

    Reversed logic of registration during initialized message.

--- lib/solargraph/language_server/message/initialized.rb
+++ lib/solargraph/language_server/message/initialized.rb
@@ -4,20 +4,11 @@ module Solargraph
       class Initialized < Base
         def process
           meths = []
-          # Initial registration checks for explicit false to enable features
-          # on clients that don't send configuration options
-          # meths.push 'textDocument/completion' if host.options['completion']
-          # meths.push 'textDocument/hover' if host.options['hover']
-          # meths.push 'textDocument/signatureHelp' if host.options['hover']
-          # meths.push 'textDocument/onTypeFormatting' if host.options['autoformat']
-          # host.register_capabilities meths
-
-          meths = []
-          meths.push 'textDocument/completion' unless host.options['completion']
-          meths.push 'textDocument/hover' unless host.options['hover']
-          meths.push 'textDocument/signatureHelp' unless host.options['hover']
-          meths.push 'textDocument/onTypeFormatting' unless host.options['autoformat']
-          host.unregister_capabilities meths unless meths.empty?
+          meths.push 'textDocument/completion' if host.options['completion']
+          meths.push 'textDocument/hover' if host.options['hover']
+          meths.push 'textDocument/signatureHelp' if host.options['hover']
+          meths.push 'textDocument/onTypeFormatting' if host.options['autoformat']
+          host.register_capabilities meths unless meths.empty?
         end
       end
     end
