ac3a3ba2de3d3975e9fe3fa399bbdb392d5e1775
Fred Snyder <fsnyder@castwide.com>
2018-04-25 08:24:40 -0400

    Initialize message detects dynamic registration for textDocument capabilities.

--- lib/solargraph/language_server/message/initialize.rb
+++ lib/solargraph/language_server/message/initialize.rb
@@ -5,24 +5,11 @@ module Solargraph
         def process
           host.configure params['initializationOptions']
           host.prepare params['rootPath']
-          set_result(
-            # @todo Dynamic capabilities are stubbed here.
+          result = {
             capabilities: {
               textDocumentSync: 2, # @todo What should this be?
-              # completionProvider: {
-              #   resolveProvider: true,
-              #   triggerCharacters: ['.', ':', '@']
-              # },
-              # hoverProvider: true,
               definitionProvider: true,
-              # signatureHelpProvider: {
-              #   triggerCharacters: ['(', ',']
-              # },
               documentFormattingProvider: true,
-              # documentOnTypeFormattingProvider: {
-              #   firstTriggerCharacter: '{',
-              #   moreTriggerCharacter: ['(']
-              # },
               documentSymbolProvider: true,
               workspaceSymbolProvider: true,
               workspace: {
@@ -32,7 +19,74 @@ module Solargraph
                 }
               }
             }
-          )
+          }
+          result.merge! static_completion unless dynamic_completion?
+          result.merge! static_signature_help unless dynamic_signature_help?
+          result.merge! static_on_type_formatting unless dynamic_on_type_formatting?
+          result.merge! static_hover unless dynamic_hover?
+          set_result result
+        end
+
+        private
+
+        def static_completion
+          {
+            completionProvider: {
+              resolveProvider: true,
+              triggerCharacters: ['.', ':', '@']
+            }
+          }
+        end
+
+        def dynamic_completion?
+          params['capabilities'] and
+            params['capabilities']['textDocument'] and
+            params['capabilities']['textDocument']['completion'] and
+            params['capabilities']['textDocument']['completion']['dynamicRegistration']
+        end
+
+        def static_signature_help
+          {
+            signatureHelpProvider: {
+              triggerCharacters: ['(', ',']
+            }
+          }
+        end
+
+        def dynamic_signature_help?
+          params['capabilities'] and
+            params['capabilities']['textDocument'] and
+            params['capabilities']['textDocument']['signatureHelp'] and
+            params['capabilities']['textDocument']['signatureHelp']['dynamicRegistration']
+        end
+
+        def static_on_type_formatting
+          {
+            documentOnTypeFormattingProvider: {
+              firstTriggerCharacter: '{',
+              moreTriggerCharacter: ['(']
+            }
+          }
+        end
+
+        def dynamic_on_type_formatting?
+          params['capabilities'] and
+            params['capabilities']['textDocument'] and
+            params['capabilities']['textDocument']['onTypeFormatting'] and
+            params['capabilities']['textDocument']['onTypeFormatting']['dynamicRegistration']
+        end
+
+        def static_hover
+          {
+            hoverProvider: true
+          }
+        end
+
+        def dynamic_hover?
+          params['capabilities'] and
+            params['capabilities']['textDocument'] and
+            params['capabilities']['textDocument']['hover'] and
+            params['capabilities']['textDocument']['hover']['dynamicRegistration']
         end
       end
     end
--- lib/solargraph/language_server/message/initialized.rb
+++ lib/solargraph/language_server/message/initialized.rb
@@ -6,11 +6,18 @@ module Solargraph
           meths = []
           # Initial registration checks for explicit false to enable features
           # on clients that don't send configuration options
-          meths.push 'textDocument/completion' if host.options['completion']
-          meths.push 'textDocument/hover' if host.options['hover']
-          meths.push 'textDocument/signatureHelp' if host.options['hover']
-          meths.push 'textDocument/onTypeFormatting' if host.options['autoformat']
-          host.register_capabilities meths
+          # meths.push 'textDocument/completion' if host.options['completion']
+          # meths.push 'textDocument/hover' if host.options['hover']
+          # meths.push 'textDocument/signatureHelp' if host.options['hover']
+          # meths.push 'textDocument/onTypeFormatting' if host.options['autoformat']
+          # host.register_capabilities meths
+
+          meths = []
+          meths.push 'textDocument/completion' unless host.options['completion']
+          meths.push 'textDocument/hover' unless host.options['hover']
+          meths.push 'textDocument/signatureHelp' unless host.options['hover']
+          meths.push 'textDocument/onTypeFormatting' unless host.options['autoformat']
+          host.unregister_capabilities meths unless meths.empty?
         end
       end
     end
