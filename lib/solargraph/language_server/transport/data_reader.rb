82585a916235eff6e183856fb40e5b27ef83d8bf
Fred Snyder <fsnyder@castwide.com>
2018-04-26 21:21:57 -0400

    Refactored DataReader.

--- lib/solargraph/language_server/transport/data_reader.rb
+++ lib/solargraph/language_server/transport/data_reader.rb
@@ -8,41 +8,56 @@ module Solargraph
           @buffer = ''
         end
 
+        # Declare a block to be executed for each message received from the
+        # client.
+        #
+        # @yieldparam [Hash] The message received from the client
         def set_message_handler &block
           @message_handler = block
         end
 
+        # Process raw data received from the client. The data will be parsed
+        # into messages based on the JSON-RPC protocol. Each message will be
+        # passed to the block declared via set_message_handler. Incomplete data
+        # will be buffered and subsequent data will be appended to the buffer.
+        #
         # @param data [String]
         def receive data
           data.each_char do |char|
             @buffer.concat char
             if @in_header
-              if @buffer.end_with?("\r\n\r\n")
-                @in_header = false
-                @buffer.each_line do |line|
-                  parts = line.split(':').map(&:strip)
-                  if parts[0] == 'Content-Length'
-                    @content_length = parts[1].to_i
-                    break
-                  end
-                end
-                @buffer.clear
-              end
+              prepare_to_parse_message if @buffer.end_with?("\r\n\r\n")
             else
-              if @buffer.bytesize == @content_length
-                begin
-                  msg = JSON.parse(@buffer)
-                  @message_handler.call msg unless @message_handler.nil?
-                rescue JSON::ParserError => e
-                  STDERR.puts "Failed to parse request: #{e.message}"
-                  STDERR.puts "Buffer: #{@buffer}"
-                ensure
-                  @buffer.clear
-                  @in_header = true
-                  @content_length = 0
-                end
-              end
+              parse_message_from_buffer if @buffer.bytesize == @content_length
             end
+          end
+        end
+
+        private
+
+        def prepare_to_parse_message
+          @in_header = false
+          @buffer.each_line do |line|
+            parts = line.split(':').map(&:strip)
+            if parts[0] == 'Content-Length'
+              @content_length = parts[1].to_i
+              break
+            end
+          end
+          @buffer.clear
+        end
+
+        def parse_message_from_buffer
+          begin
+            msg = JSON.parse(@buffer)
+            @message_handler.call msg unless @message_handler.nil?
+          rescue JSON::ParserError => e
+            STDERR.puts "Failed to parse request: #{e.message}"
+            STDERR.puts "Buffer: #{@buffer}"
+          ensure
+            @buffer.clear
+            @in_header = true
+            @content_length = 0
           end
         end
       end
