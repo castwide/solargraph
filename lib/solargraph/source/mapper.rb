d7dcd6e246cbb44e82db1f3383e650b90d1810da
Fred Snyder <fsnyder@castwide.com>
2018-05-08 10:26:03 -0400

    Pin mapping changes for module_function visibility.

--- lib/solargraph/source/mapper.rb
+++ lib/solargraph/source/mapper.rb
@@ -145,12 +145,15 @@ module Solargraph
                   # @todo Smelly instance variable access.
                   pins.last.instance_variable_set(:@return_type, methpin.namespace)
                   pins.push Solargraph::Pin::Method.new(methpin.location, methpin.namespace, methpin.name, methpin.docstring, methpin.scope, :private, methpin.parameters)
+                elsif visibility == :module_function
+                  pins.push Solargraph::Pin::Method.new(methpin.location, methpin.namespace, methpin.name, methpin.docstring, :class, :public, methpin.parameters)
+                  pins.push Solargraph::Pin::Method.new(methpin.location, methpin.namespace, methpin.name, methpin.docstring, :instance, :private, methpin.parameters)
                 else
                   pins.push methpin
                 end
               elsif c.type == :defs
                 s_visi = visibility
-                s_visi = :public if scope != :class
+                s_visi = :public if s_visi == :module_function or scope != :class
                 if c.children[0].is_a?(AST::Node) and c.children[0].type == :self
                   dfqn = fqn || ''
                 else
@@ -192,6 +195,24 @@ module Solargraph
                 next
               elsif c.type == :send and c.children[1] == :module_function
                 # @todo Handle module_function
+                if c.children[2].nil?
+                  visibility = :module_function
+                elsif c.children[2].type == :sym or c.children[2].type == :str
+                  # @todo What to do about references?
+                  c.children[2..-1].each do |x|
+                    cn = x.children[2].children[0].to_s
+                    ref = pins.select{|p| p.namespace == (fqn || '') and p.name == cn}.first
+                    unless ref.nil?
+                      pins.delete ref
+                      pins.push Solargraph::Pin::Method.new(ref.location, ref.namespace, ref.name, ref.docstring, :class, :public, ref.parameters)
+                      pins.push Solargraph::Pin::Method.new(ref.location, ref.namespace, ref.name, ref.docstring, :instance, :private, ref.parameters)
+                    end
+                  end
+                elsif c.children[2].type == :def
+                  # @todo A single function
+                  process c, tree, :module_function, :class, fqn, stack
+                  next
+                end
               elsif c.type == :send and c.children[1] == :include and c.children[0].nil?
                 last_node = get_last_in_stack_not_begin(stack)
                 if last_node.nil? or last_node.type == :class or last_node.type == :module or last_node.type == :source
