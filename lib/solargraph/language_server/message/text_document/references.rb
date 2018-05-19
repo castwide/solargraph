539c1e75b4ee216ef3964de802f080eb46ac9353
Fred Snyder <fsnyder@castwide.com>
2018-05-19 11:29:26 -0400

    textDocument/references filters by definition path.

--- lib/solargraph/language_server/message/text_document/references.rb
+++ lib/solargraph/language_server/message/text_document/references.rb
@@ -1,7 +1,14 @@
 module Solargraph::LanguageServer::Message::TextDocument
   class References < Base
     def process
+      definition = host.definitions_at(uri_to_file(params['textDocument']['uri']), params['position']['line'], params['position']['character']).first
+      return if definition.nil?
       locs = host.references_from(uri_to_file(params['textDocument']['uri']), params['position']['line'], params['position']['character'])
+      locs.keep_if do |loc|
+        referenced = host.definitions_at(loc.filename, loc.range.ending.line, loc.range.ending.character).first
+        !referenced.nil? and referenced.path == definition.path
+      end
+      locs.unshift definition.location if params['context'] and params['context']['includeDeclaration'] and definition.kind == Solargraph::Pin::METHOD
       result = locs.map do |loc|
         {
           uri: file_to_uri(loc.filename),
