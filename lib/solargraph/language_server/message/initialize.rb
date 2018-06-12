bf6beb169bee482d7d422b1d40a9a444da52fc43
Fred Snyder <fsnyder@castwide.com>
2018-06-12 17:07:14 -0400

    Superfluous private methods.

--- lib/solargraph/language_server/message/initialize.rb
+++ lib/solargraph/language_server/message/initialize.rb
@@ -16,16 +16,16 @@ module Solargraph
               }
             }
           }
-          result[:capabilities].merge! static_completion unless dynamic_completion?
-          result[:capabilities].merge! static_signature_help unless dynamic_signature_help?
-          result[:capabilities].merge! static_on_type_formatting unless dynamic_on_type_formatting?
-          result[:capabilities].merge! static_hover unless dynamic_hover?
-          result[:capabilities].merge! static_document_formatting unless dynamic_document_formatting?
-          result[:capabilities].merge! static_document_symbols unless dynamic_document_symbols?
-          result[:capabilities].merge! static_workspace_symbols unless dynamic_workspace_symbols?
-          result[:capabilities].merge! static_definitions unless dynamic_definitions?
-          result[:capabilities].merge! static_rename unless dynamic_rename?
-          result[:capabilities].merge! static_references unless dynamic_references?
+          result[:capabilities].merge! static_completion unless dynamic_registration_for?('textDocument', 'completion')
+          result[:capabilities].merge! static_signature_help unless dynamic_registration_for?('textDocument', 'signatureHelp')
+          result[:capabilities].merge! static_on_type_formatting unless dynamic_registration_for?('textDocument', 'onTypeFormatting')
+          result[:capabilities].merge! static_hover unless dynamic_registration_for?('textDocument', 'hover')
+          result[:capabilities].merge! static_document_formatting unless dynamic_registration_for?('textDocument', 'formatting')
+          result[:capabilities].merge! static_document_symbols unless dynamic_registration_for?('textDocument', 'documentSymbol')
+          result[:capabilities].merge! static_workspace_symbols unless dynamic_registration_for?('workspace', 'symbol')
+          result[:capabilities].merge! static_definitions unless dynamic_registration_for?('textDocument', 'definition')
+          result[:capabilities].merge! static_rename unless dynamic_registration_for?('textDocument', 'rename')
+          result[:capabilities].merge! static_references unless dynamic_registration_for?('textDocument', 'references')
           set_result result
         end
 
@@ -40,20 +40,12 @@ module Solargraph
           }
         end
 
-        def dynamic_completion?
-          dynamic_registration_for? 'textDocument', 'completion'
-        end
-
         def static_signature_help
           {
             signatureHelpProvider: {
               triggerCharacters: ['(', ',']
             }
           }
-        end
-
-        def dynamic_signature_help?
-          dynamic_registration_for? 'textDocument', 'signatureHelp'
         end
 
         def static_on_type_formatting
@@ -65,18 +57,10 @@ module Solargraph
           }
         end
 
-        def dynamic_on_type_formatting?
-          dynamic_registration_for? 'textDocument', 'onTypeFormatting'
-        end
-
         def static_hover
           {
             hoverProvider: true
           }
-        end
-
-        def dynamic_hover?
-          dynamic_registration_for? 'textDocument', 'hover'
         end
 
         def static_document_formatting
@@ -85,18 +69,10 @@ module Solargraph
           }
         end
 
-        def dynamic_document_formatting?
-          dynamic_registration_for? 'textDocument', 'formatting'
-        end
-
         def static_document_symbols
           {
             documentSymbolProvider: true
           }
-        end
-
-        def dynamic_document_symbols?
-          dynamic_registration_for? 'textDocument', 'documentSymbol'
         end
 
         def static_workspace_symbols
@@ -105,18 +81,10 @@ module Solargraph
           }
         end
 
-        def dynamic_workspace_symbols?
-          dynamic_registration_for? 'workspace', 'symbol'
-        end
-
         def static_definitions
           {
             definitionProvider: true
           }
-        end
-
-        def dynamic_definitions?
-          dynamic_registration_for? 'textDocument', 'definition'
         end
 
         def static_rename
@@ -125,18 +93,10 @@ module Solargraph
           }
         end
 
-        def dynamic_rename?
-          dynamic_registration_for? 'textDocument', 'rename'
-        end
-
         def static_references
           {
             referencesProvider: true
           }
-        end
-
-        def dynamic_references?
-          dynamic_registration_for? 'textDocument', 'references'
         end
 
         def dynamic_registration_for? section, capability
