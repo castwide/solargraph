a8eb9e665c33508e2c3e4f8b2707b3437dbd64b9
Fred Snyder <fsnyder@castwide.com>
2018-08-20 14:26:18 -0400

    Specs and minor refactoring.

--- spec/pin/base_spec.rb
+++ spec/pin/base_spec.rb
@@ -1,4 +1,7 @@
 describe Solargraph::Pin::Base do
+  let(:zero_location) { Solargraph::Source::Location.new('test.rb', Solargraph::Source::Range.from_to(0, 0, 0, 0)) }
+  let(:one_location) { Solargraph::Source::Location.new('test.rb', Solargraph::Source::Range.from_to(0, 0, 1, 0)) }
+
   it "returns its location in the source" do
     source = Solargraph::Source.load_string(%(
       class Foo
@@ -12,5 +15,20 @@ describe Solargraph::Pin::Base do
     source.method_pins.each do |pin|
       expect(pin.location).not_to be_nil
     end
+  end
+
+  it "merges pins with location changes" do
+    pin1 = Solargraph::Pin::Base.new(zero_location, '', 'Foo', '')
+    pin2 = Solargraph::Pin::Base.new(one_location, '', 'Foo', '')
+    expect(pin1.try_merge!(pin2)).to eq(true)
+    expect(pin1.location).to eq(one_location)
+  end
+
+  it "merges pins with comment changes" do
+    pin1 = Solargraph::Pin::Base.new(zero_location, '', 'Foo', 'A Foo class')
+    merge_comment = 'A modified Foo class'
+    pin2 = Solargraph::Pin::Base.new(zero_location, '', 'Foo', merge_comment)
+    expect(pin1.try_merge!(pin2)).to eq(true)
+    expect(pin1.comments).to eq(merge_comment)
   end
 end
