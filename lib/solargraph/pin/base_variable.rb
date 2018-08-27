39519ea519f39e772f828af7face744d4ca6c1ae
Fred Snyder <fsnyder@castwide.com>
2018-08-27 00:21:04 -0400

    Move variable inference to BaseVariable.

--- lib/solargraph/pin/base_variable.rb
+++ lib/solargraph/pin/base_variable.rb
@@ -37,6 +37,15 @@ module Solargraph
         true
       end
 
+      def infer api_map
+        result = super
+        return result if result.defined? or signature.nil?
+        # @todo Instead of parsing a signature, start with an assignment node
+        chain = Source::Chain.load_string(signature)
+        fragment = api_map.fragment_at(location)
+        chain.infer_type_with(api_map, context, fragment.locals)
+      end
+
       def == other
         return false unless super
         signature == other.signature
--- lib/solargraph/pin/local_variable.rb
+++ lib/solargraph/pin/local_variable.rb
@@ -13,17 +13,6 @@ module Solargraph
         Pin::LOCAL_VARIABLE
       end
 
-      def infer api_map
-        result = super
-        return result if result.defined? or signature.nil?
-        # @todo Get the return type from the assignment
-        # @todo Instead of parsing a signature, start with an assignment node
-        chain = Source::Chain.load_string(signature)
-        fragment = api_map.fragment_at(location)
-        chain.infer_type_with(api_map, context, fragment.locals)
-        # ComplexType::UNDEFINED
-      end
-
       def try_merge! pin
         return false unless super
         @presence = pin.presence
