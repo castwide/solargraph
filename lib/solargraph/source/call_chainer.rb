193dd17e0f874cebd18a9d9ca03cd2211da3ed63
Fred Snyder <fsnyder@castwide.com>
2018-08-29 23:55:32 -0400

    Stale code.

--- lib/solargraph/source/call_chainer.rb
+++ lib/solargraph/source/call_chainer.rb
@@ -23,7 +23,7 @@ module Solargraph
       # @param column [Integer]
       def initialize source, line, column
         @source = source
-        @code = source.code
+        # @source.code = source.code
         @line = line
         @column = column
         @calculated_literal = false
@@ -119,15 +119,6 @@ module Solargraph
         @remainder ||= remainder_at(offset)
       end
 
-      # Get the whole word at the current offset, including the remainder.
-      # Given the text `foobar.baz`, the whole word at any offset from 0 to 6
-      # is `foobar`.
-      #
-      # @return [String]
-      def whole_word
-        @whole_word ||= word + remainder
-      end
-
       # Get the whole signature at the current offset, including the final
       # word and its remainder.
       #
@@ -136,42 +127,12 @@ module Solargraph
         @whole_signature ||= signature + remainder
       end
 
-      # Get the word before the current offset. Given the text `foo.bar`, the
-      # word at offset 6 is `ba`.
-      #
-      # @return [String]
-      def word
-        @word ||= word_at(offset)
-      end
-
       # True if the current offset is inside a string.
       #
       # @return [Boolean]
       def string?
         # @string ||= (node.type == :str or node.type == :dstr)
         @string ||= @source.string_at?(line, character)
-      end
-
-      # True if the current offset is inside a comment.
-      #
-      # @return [Boolean]
-      def comment?
-        @comment ||= check_comment(line, column)
-      end
-
-      # Get the range of the word up to the current offset.
-      #
-      # @return [Range]
-      def word_range
-        @word_range ||= word_range_at(offset, false)
-      end
-
-      # Get the range of the whole word at the current offset, including its
-      # remainder.
-      #
-      # @return [Range]
-      def whole_word_range
-        @whole_word_range ||= word_range_at(offset, true)
       end
 
       # True if the fragment is a signature that stems from a literal value.
@@ -195,25 +156,6 @@ module Solargraph
         @base_literal
       end
 
-      # True if the fragment is inside a literal value.
-      #
-      # @return [Boolean]
-      def literal?
-        !literal.nil?
-      end
-
-      # The fragment's literal type, or nil if the fragment is not inside a
-      # literal value.
-      #
-      # @return [String]
-      def literal
-        if @literal.nil? and !@calculated_actual_literal
-          @calculated_actual_literal = true
-          pn = @source.node_at(line, column)
-          @literal = infer_literal_node_type(pn)
-        end
-      end
-
       private
 
       # @return [Integer]
@@ -222,7 +164,7 @@ module Solargraph
       end
 
       def get_offset line, column
-        Position.line_char_to_offset(@code, line, column)
+        Position.line_char_to_offset(@source.code, line, column)
       end
 
       def signature_data
@@ -237,20 +179,20 @@ module Solargraph
         index -=1
         in_whitespace = false
         while index >= 0
-          pos = Position.from_offset(@code, index)
-          break if index > 0 and check_comment(pos.line, pos.character)
+          pos = Position.from_offset(@source.code, index)
+          break if index > 0 and @source.comment_at?(pos.line, pos.character)
           unless !in_whitespace and string?
             break if brackets > 0 or parens > 0 or squares > 0
-            char = @code[index, 1]
+            char = @source.code[index, 1]
             break if char.nil? # @todo Is this the right way to handle this?
             if brackets.zero? and parens.zero? and squares.zero? and [' ', "\r", "\n", "\t"].include?(char)
               in_whitespace = true
             else
               if brackets.zero? and parens.zero? and squares.zero? and in_whitespace
-                unless char == '.' or @code[index+1..-1].strip.start_with?('.')
-                  old = @code[index+1..-1]
-                  nxt = @code[index+1..-1].lstrip
-                  index += (@code[index+1..-1].length - @code[index+1..-1].lstrip.length)
+                unless char == '.' or @source.code[index+1..-1].strip.start_with?('.')
+                  old = @source.code[index+1..-1]
+                  nxt = @source.code[index+1..-1].lstrip
+                  index += (@source.code[index+1..-1].length - @source.code[index+1..-1].lstrip.length)
                   break
                 end
               end
@@ -266,14 +208,14 @@ module Solargraph
                 brackets += 1
               elsif char == '['
                 squares += 1
-                signature = ".[]#{signature}" if parens.zero? and brackets.zero? and squares.zero? and @code[index-2] != '%'
+                signature = ".[]#{signature}" if parens.zero? and brackets.zero? and squares.zero? and @source.code[index-2] != '%'
               end
               if brackets.zero? and parens.zero? and squares.zero?
                 break if ['"', "'", ',', ';', '%'].include?(char)
-                signature = char + signature if char.match(/[a-z0-9:\._@\$\?\!]/i) and @code[index - 1] != '%'
+                signature = char + signature if char.match(/[a-z0-9:\._@\$\?\!]/i) and @source.code[index - 1] != '%'
                 break if char == '$'
                 if char == '@'
-                  signature = "@#{signature}" if @code[index-1, 1] == '@'
+                  signature = "@#{signature}" if @source.code[index-1, 1] == '@'
                   break
                 end
               end
@@ -306,58 +248,16 @@ module Solargraph
         [index + 1, signature]
       end
 
-      # Determine if the specified location is inside a comment.
-      #
-      # @param lin [Integer]
-      # @param col [Integer]
-      # @return [Boolean]
-      def check_comment(lin, col)
-        index = Position.line_char_to_offset(source_from_parser, lin, col)
-        @source.comments.each do |c|
-          return true if index > c.location.expression.begin_pos and index <= c.location.expression.end_pos
-        end
-        false
-      end
-
-      # @return Solargraph::Source::Range
-      def word_range_at index, whole
-        cursor = beginning_of_word_at(index)
-        start_offset = cursor
-        start_offset -= 1 if (start_offset > 1 and @code[start_offset - 1] == ':') and (start_offset == 1 or @code[start_offset - 2] != ':')
-        cursor = index
-        if whole
-          while cursor < @code.length
-            char = @code[cursor, 1]
-            break if char.nil? or char == ''
-            break unless char.match(/[a-z0-9_\?\!]/i)
-            cursor += 1
-          end
-        end
-        end_offset = cursor
-        end_offset = start_offset if end_offset < start_offset
-        start_pos = get_position_at(start_offset)
-        end_pos = get_position_at(end_offset)
-        Solargraph::Source::Range.from_to(start_pos[0], start_pos[1], end_pos[0], end_pos[1])
-      end
-
       # @return [String]
       def remainder_at index
         cursor = index
-        while cursor < @code.length
-          char = @code[cursor, 1]
+        while cursor < @source.code.length
+          char = @source.code[cursor, 1]
           break if char.nil? or char == ''
           break unless char.match(/[a-z0-9_\?\!]/i)
           cursor += 1
         end
-        @code[index..cursor-1].to_s
-      end
-
-      # Range tests that depend on positions identified from parsed code, such
-      # as comment ranges, need to normalize EOLs to \n.
-      #
-      # @return [String]
-      def source_from_parser
-        @source_from_parser ||= @source.code.gsub(/\r\n/, "\n")
+        @source.code[index..cursor-1].to_s
       end
     end
   end
