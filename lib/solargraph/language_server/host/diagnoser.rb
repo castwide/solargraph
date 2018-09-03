3820f3a912000ec57a62576900001304c129eb4f
Fred Snyder <fsnyder@castwide.com>
2018-09-03 14:29:20 -0400

    Diagnoser namespace.

--- lib/solargraph/language_server/host/diagnoser.rb
+++ lib/solargraph/language_server/host/diagnoser.rb
@@ -1,73 +1,75 @@
 module Solargraph
   module LanguageServer
     class Host
-      def initialize host
-        @host = host
-        @mutex = Mutex.new
-        @queue = []
-        @stopped = true
-      end
+      class Diagnoser
+        def initialize host
+          @host = host
+          @mutex = Mutex.new
+          @queue = []
+          @stopped = true
+        end
 
-      def schedule filename
-        @mutex.synchronize { queue.push filename }
-      end
+        def schedule filename
+          @mutex.synchronize { queue.push filename }
+        end
 
-      def stop
-        @stopped = true
-      end
+        def stop
+          @stopped = true
+        end
 
-      def stopped?
-        @stopped
-      end
+        def stopped?
+          @stopped
+        end
 
-      # @return [self]
-      def start
-        return unless @stopped
-        @stopped = false
-        Thread.new do
-          until stopped?
-            sleep 0.1
-            if !host.options['diagnostics']
-              mutex.synchronize { queue.clear }
-              next
-            end
-            begin
-              # Diagnosis is broken into two parts to reduce the number of
-              # times it runs while a document is changing
-              current = nil
-              already_changing = nil
-              mutex.synchronize do
-                current = queue.shift
+        # @return [self]
+        def start
+          return unless @stopped
+          @stopped = false
+          Thread.new do
+            until stopped?
+              sleep 0.1
+              if !host.options['diagnostics']
+                mutex.synchronize { queue.clear }
+                next
               end
-              next if current.nil?
-              results = library.diagnose(current)
-              send_notification "textDocument/publishDiagnostics", {
-                uri: current,
-                diagnostics: results
-              }
-            rescue DiagnosticsError => e
-              STDERR.puts "Error in diagnostics: #{e.message}"
-              options['diagnostics'] = false
-              send_notification 'window/showMessage', {
-                type: LanguageServer::MessageTypes::ERROR,
-                message: "Error in diagnostics: #{e.message}"
-              }
+              begin
+                # Diagnosis is broken into two parts to reduce the number of
+                # times it runs while a document is changing
+                current = nil
+                already_changing = nil
+                mutex.synchronize do
+                  current = queue.shift
+                end
+                next if current.nil?
+                results = library.diagnose(current)
+                send_notification "textDocument/publishDiagnostics", {
+                  uri: current,
+                  diagnostics: results
+                }
+              rescue DiagnosticsError => e
+                STDERR.puts "Error in diagnostics: #{e.message}"
+                options['diagnostics'] = false
+                send_notification 'window/showMessage', {
+                  type: LanguageServer::MessageTypes::ERROR,
+                  message: "Error in diagnostics: #{e.message}"
+                }
+              end
             end
           end
+          self
         end
-        self
+
+        private
+
+        # @return [Host]
+        attr_reader :host
+
+        # @return [Mutex]
+        attr_reader :mutex
+
+        # @return [Set]
+        attr_reader :queue
       end
-
-      private
-
-      # @return [Host]
-      attr_reader :host
-
-      # @return [Mutex]
-      attr_reader :mutex
-
-      # @return [Set]
-      attr_reader :queue
     end
   end
 end
