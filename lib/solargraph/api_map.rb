6e81f764f6d8c40820032cd3cc3f0c88959d8769
Fred Snyder <fsnyder@castwide.com>
2018-09-05 14:17:28 -0400

    ApiMap caches method stacks.

--- lib/solargraph/api_map.rb
+++ lib/solargraph/api_map.rb
@@ -340,11 +340,10 @@ module Solargraph
     # @param scope [Symbol] :instance or :class
     # @return [Array<Solargraph::Pin::Base>]
     def get_method_stack fqns, name, scope: :instance
-      # @todo Caches don't work on this query
-      # cached = cache.get_method_stack(fqns, name, scope)
-      # return cached.clone unless cached.nil?
+      cached = cache.get_method_stack(fqns, name, scope)
+      return cached unless cached.nil?
       result = get_methods(fqns, scope: scope, visibility: [:private, :protected, :public]).select{|p| p.name == name}
-      # cache.set_method_stack(fqns, name, scope, result)
+      cache.set_method_stack(fqns, name, scope, result)
       result
     end
 
