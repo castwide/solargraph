5ae3235b23128327f2ff1940638565645e3c70de
Fred Snyder <fsnyder@castwide.com>
2018-09-09 00:57:59 -0400

    Chain::Head super spec.

--- spec/source/chain/head_spec.rb
+++ spec/source/chain/head_spec.rb
@@ -10,4 +10,13 @@ describe Solargraph::Source::Chain::Head do
     expect(ipin.return_type.namespace).to eq('Foo')
     expect(ipin.return_type.scope).to eq(:class)
   end
+
+  it "resolves super" do
+    head = Solargraph::Source::Chain::Head.new('super')
+    npin = Solargraph::Pin::Namespace.new(nil, '', 'Substring', '', :class, :public, 'String')
+    mpin = Solargraph::Pin::Method.new(nil, 'Substring', 'upcase', '', :instance, :public, [])
+    api_map = Solargraph::ApiMap.new(pins: [npin, mpin])
+    spin = head.resolve(api_map, mpin, []).first
+    expect(spin.path).to eq('String#upcase')
+  end
 end
