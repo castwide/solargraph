cedea747b474369020622bc84339e960387d0b05
Fred Snyder <fsnyder@castwide.com>
2018-12-02 17:18:49 -0500

    Pin type checks for module_function, private_class_method, and private_constant.

--- spec/source_map/mapper_spec.rb
+++ spec/source_map/mapper_spec.rb
@@ -847,4 +847,37 @@ describe Solargraph::SourceMap::Mapper do
       ), 'test.rb')
     }.not_to raise_error
   end
+
+  it "ignores private_class_methods that do not map to methods or attributes" do
+    expect {
+      smap = Solargraph::SourceMap.load_string(%(
+        class Foo
+          var = some_method
+          private_class_method :var
+        end
+      ), 'test.rb')
+    }.not_to raise_error
+  end
+
+  it "ignores private_constants that do not map to namespaces or constants" do
+    expect {
+      smap = Solargraph::SourceMap.load_string(%(
+        class Foo
+          var = some_method
+          private_constant :var
+        end
+      ), 'test.rb')
+    }.not_to raise_error
+  end
+
+  it "ignores module_functions that do not map to methods or attributes" do
+    expect {
+      smap = Solargraph::SourceMap.load_string(%(
+        class Foo
+          var = some_method
+          module_function :var
+        end
+      ), 'test.rb')
+    }.not_to raise_error
+  end
 end
