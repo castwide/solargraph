0f11f1ad0ed27ef8387e1268df448debec1e3854
Fred Snyder <fsnyder@castwide.com>
2018-12-06 10:51:46 -0500

    Chains match constants on complete symbols.

--- lib/solargraph/source/chain/constant.rb
+++ lib/solargraph/source/chain/constant.rb
@@ -20,7 +20,9 @@ module Solargraph
           else
             ns = ''
           end
-          api_map.get_constants(ns, context).select{|p| p.path.end_with?(bottom)}
+          result = api_map.get_constants(ns, context)
+          last_name = bottom.split('::').last
+          result.select{ |p| p.path.split('::').last == last_name }
         end
       end
     end
