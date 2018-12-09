82a598d675a4f197123c42985a24e7f0bacc18b0
Fred Snyder <fsnyder@castwide.com>
2018-12-09 12:04:53 -0500

    Refactoring method and attribute pins.

--- lib/solargraph/pin/method.rb
+++ lib/solargraph/pin/method.rb
@@ -1,13 +1,7 @@
 module Solargraph
   module Pin
-    class Method < Base
+    class Method < BaseMethod
       include Source::NodeMethods
-
-      # @return [Symbol] :instance or :class
-      attr_reader :scope
-
-      # @return [Symbol] :public, :private, or :protected
-      attr_reader :visibility
 
       # @return [Array<String>]
       attr_reader :parameters
@@ -54,10 +48,6 @@ module Solargraph
       # @return [Integer]
       def symbol_kind
         LanguageServer::SymbolKinds::METHOD
-      end
-
-      def return_complex_type
-        @return_complex_type ||= generate_complex_type
       end
 
       def documentation
@@ -117,22 +107,6 @@ module Solargraph
 
       private
 
-      # @return [ComplexType]
-      def generate_complex_type
-        tag = docstring.tag(:return)
-        if tag.nil?
-          ol = docstring.tag(:overload)
-          tag = ol.tag(:return) unless ol.nil?
-        end
-        return ComplexType::UNDEFINED if tag.nil? or tag.types.nil? or tag.types.empty?
-        begin
-          ComplexType.parse *tag.types
-        rescue Solargraph::ComplexTypeError => e
-          STDERR.puts e.message
-          ComplexType::UNDEFINED
-        end
-      end
-
       # @param api_map [ApiMap]
       def infer_from_return_nodes api_map
         return ComplexType::UNDEFINED if node.nil? ||
@@ -149,27 +123,6 @@ module Solargraph
         end
         return ComplexType::UNDEFINED if result.empty?
         ComplexType.parse(*result.map(&:tag))
-      end
-
-      # @param [ApiMap]
-      def see_reference api_map
-        docstring.ref_tags.each do |ref|
-          next unless ref.tag_name == 'return' && ref.owner
-          parts = ref.owner.to_s.split(/[\.#]/)
-          if parts.first.empty?
-            path = "#{namespace}#{ref.owner.to_s}"
-          else
-            fqns = api_map.qualify(parts.first, namespace)
-            return ComplexType::UNDEFINED if fqns.nil?
-            path = fqns + ref.owner.to_s[parts.first.length] + parts.last
-          end
-          pins = api_map.get_path_pins(path)
-          pins.each do |pin|
-            type = pin.typify(api_map)
-            return type unless type.undefined?
-          end
-        end
-        nil
       end
     end
   end
