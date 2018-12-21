b25abcfd80bc6f870e95a627114b9ca840a6c814
Fred Snyder <fsnyder@castwide.com>
2018-12-21 13:19:49 -0500

    Exception in Logging spec for Ruby < 2.3.

--- spec/logging_spec.rb
+++ spec/logging_spec.rb
@@ -2,6 +2,9 @@ require 'tempfile'
 
 describe Solargraph::Logging do
   it "logs messages with levels" do
+    # @todo Logger#reopen is only available in Ruby 2.3+. This is a quick and
+    #   dirty hack to avoid erroneous failures in Travis.
+    next if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.3.0')
     file = Tempfile.new('log')
     Solargraph::Logging.logger.reopen file
     Solargraph::Logging.logger.warn "Test"
