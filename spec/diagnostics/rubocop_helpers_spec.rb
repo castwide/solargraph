ffb485299ebe482375c9c78647775e249063443a
Fred Snyder <fsnyder@castwide.com>
2018-12-30 19:50:51 -0500

    RuboCopHelpers drive letter specs.

--- spec/diagnostics/rubocop_helpers_spec.rb
+++ spec/diagnostics/rubocop_helpers_spec.rb
@@ -5,4 +5,16 @@ describe Solargraph::Diagnostics::RubocopHelpers do
     found = Solargraph::Diagnostics::RubocopHelpers.find_rubocop_file(file)
     expect(found).to eq(conf)
   end
+
+  it "converts lower-case drive letters to upper-case" do
+    input = 'c:/one/two'
+    output = Solargraph::Diagnostics::RubocopHelpers.fix_drive_letter(input)
+    expect(output).to eq('C:/one/two')
+  end
+
+  it "ignores paths without drive letters" do
+    input = 'one/two'
+    output = Solargraph::Diagnostics::RubocopHelpers.fix_drive_letter(input)
+    expect(output).to eq('one/two')
+  end
 end
