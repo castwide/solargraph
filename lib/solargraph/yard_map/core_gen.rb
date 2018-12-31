d32553c7249f3b92442ee8ba8e3047c6df401901
Fred Snyder <fsnyder@castwide.com>
2018-12-31 17:54:58 -0500

    Fixed CoreGen path handling.

--- lib/solargraph/yard_map/core_gen.rb
+++ lib/solargraph/yard_map/core_gen.rb
@@ -13,15 +13,16 @@ module Solargraph
         # @param ruby_dir [String] The Ruby source directory
         # @param dest_dir [String] The destination directory for the yardocs
         # @return [void]
-        def generate_core ruby_dir, dest_dir
-          FileUtils.mkdir_p dest_dir
+        def generate_docs ruby_dir, dest_dir
+          path_name = Pathname.new(Dir.pwd).join(dest_dir).to_s
+          FileUtils.mkdir_p path_name
           Dir.chdir(ruby_dir) do
-            `yardoc --plugin coregen -b #{File.join(dest_dir, 'yardoc')} -n *.c`
+            `yardoc --plugin coregen -b #{File.join(path_name, 'yardoc')} -n *.c`
             raise 'An error occurred generating the core yardoc.' unless $?.success?
-            `yardoc -b #{File.join(dest_dir, 'yardoc-stdlib')} -n lib ext`
+            `yardoc -b #{File.join(path_name, 'yardoc-stdlib')} -n lib ext`
             raise 'An error occurred generating the stdlib yardoc.' unless $?.success?
           end
-        end
+        endgene
 
         # Generate a gzip of documentation from the specified Ruby source directory.
         #
@@ -33,7 +34,7 @@ module Solargraph
             gzip_name += '.tar.gz' unless gzip_name.end_with?('.tar.gz')
             base_name = gzip_name[0..-8]
             path_name = Pathname.new(Dir.pwd).join(base_name).to_s
-            generate_core ruby_dir, tmp
+            generate_docs ruby_dir, tmp
             `cd #{tmp} && tar -cf #{path_name}.tar *`
             raise 'An error occurred generating the documentation tar.' unless $?.success?
             `gzip #{path_name}.tar`
