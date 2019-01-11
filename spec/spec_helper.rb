c4db3f2662a6578b99aebaa688322194b821a792
Fred Snyder <fsnyder@castwide.com>
2019-01-11 03:27:45 -0500

    Suppress logger output in specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ $LOAD_PATH.unshift File.realpath(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'simplecov'
 SimpleCov.start
 require 'solargraph'
+# Suppress logger output in specs (if possible)
 Solargraph::Logging.logger.reopen(File::NULL) if Solargraph::Logging.logger.respond_to?(:reopen)
