a2298425749c2541da524acb77b0b515211f2f68
hima2 <himawari2539@gmail.com>
2019-03-31 01:27:41 +0900

    Add Object#tap to core documentation. (#172)

--- spec/pin/block_parameter_spec.rb
+++ spec/pin/block_parameter_spec.rb
@@ -25,6 +25,17 @@ describe Solargraph::Pin::BlockParameter do
     expect(clip.define.first.return_complex_type.namespace).to eq('String')
   end
 
+  it "detects block parameter return self from core methods" do
+    api_map = Solargraph::ApiMap.new
+    source = Solargraph::Source.load_string(%(
+      String.new.tap do |str|
+        str
+      end
+    ), 'file.rb')
+    api_map.map source
+    clip = api_map.clip_at('file.rb', Solargraph::Position.new(2, 8))
+    expect(clip.define.first.return_complex_type.namespace).to eq('String')
+  end
   it "gets return types from param type tags" do
     map = Solargraph::SourceMap.load_string(%(
       require 'set'
