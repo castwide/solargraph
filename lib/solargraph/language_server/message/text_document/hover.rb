a613d9d9d8a118c0e17a1a85f74f6693cc52758a
Fred Snyder <fsnyder@castwide.com>
2019-05-30 18:02:50 -0400

    Refactored hover.

--- lib/solargraph/language_server/message/text_document/hover.rb
+++ lib/solargraph/language_server/message/text_document/hover.rb
@@ -1,35 +1,41 @@
 require 'uri'
 require 'htmlentities'
 
-module Solargraph::LanguageServer::Message::TextDocument
-  class Hover < Base
-    def process
-      line = params['position']['line']
-      col = params['position']['character']
-      contents = []
-      suggestions = host.definitions_at(params['textDocument']['uri'], line, col)
-      last_link = nil
-      suggestions.each do |pin|
-        parts = []
-        this_link = pin.link_documentation
-        if !this_link.nil? and this_link != last_link
-          parts.push this_link
+module Solargraph
+  module LanguageServer
+    module Message
+      module TextDocument
+        class Hover < Base
+          def process
+            line = params['position']['line']
+            col = params['position']['character']
+            contents = []
+            suggestions = host.definitions_at(params['textDocument']['uri'], line, col)
+            last_link = nil
+            suggestions.each do |pin|
+              parts = []
+              this_link = pin.link_documentation
+              if !this_link.nil? and this_link != last_link
+                parts.push this_link
+              end
+              parts.push HTMLEntities.new.encode(pin.detail) unless pin.kind == Solargraph::Pin::NAMESPACE or pin.detail.nil?
+              parts.push pin.documentation unless pin.documentation.nil? or pin.documentation.empty?
+              contents.push parts.join("\n\n") unless parts.empty?
+              last_link = this_link unless this_link.nil?
+            end
+            set_result(
+              contents: {
+                kind: 'markdown',
+                value: contents.join("\n\n")
+              }
+            )
+          rescue FileNotFoundError => e
+            Logging.logger.warn "[#{e.class}] #{e.message}"
+            Logging.logger.warn e.backtrace
+            set_result nil
+          end
         end
-        parts.push HTMLEntities.new.encode(pin.detail) unless pin.kind == Solargraph::Pin::NAMESPACE or pin.detail.nil?
-        parts.push pin.documentation unless pin.documentation.nil? or pin.documentation.empty?
-        contents.push parts.join("\n\n") unless parts.empty?
-        last_link = this_link unless this_link.nil?
       end
-      set_result(
-        contents: {
-          kind: 'markdown',
-          value: contents.join("\n\n")
-        }
-      )
-    rescue FileNotFoundError => e
-      Logging.logger.warn "[#{e.class}] #{e.message}"
-      Logging.logger.warn e.backtrace
-      set_result empty_result
     end
   end
 end
--- lib/solargraph/version.rb
+++ lib/solargraph/version.rb
@@ -1,3 +1,3 @@
 module Solargraph
-  VERSION = '0.32.4'
+  VERSION = '0.32.5'
 end
