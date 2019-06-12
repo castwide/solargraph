56ce48d8338d00c76c4bd2a36907e2d6195e1ce2
Fred Snyder <fsnyder@castwide.com>
2019-06-12 01:37:27 -0400

    Enable textDocument/codeAction.

---
+++ lib/solargraph/language_server/message/text_document/code_action.rb
@@ -0,0 +1,15 @@
+require 'open3'
+
+module Solargraph
+  module LanguageServer
+    module Message
+      module TextDocument
+        class CodeAction < Base
+          def process
+            # @todo Find code actions
+          end
+        end
+      end
+    end
+  end
+end
