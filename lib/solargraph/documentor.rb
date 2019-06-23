f487a79bdffa987baa6c81a10ca8120a2c5aebb6
Fred Snyder <fsnyder@castwide.com>
2019-06-23 18:51:08 -0400

    Documentor uses Bundler to resolve lockfile specs.

--- lib/solargraph/documentor.rb
+++ lib/solargraph/documentor.rb
@@ -14,18 +14,27 @@ module Solargraph
 
     def document
       Dir.chdir @directory do
-        lockfile = Bundler::LockfileParser.new(Bundler.read_file(Bundler.default_lockfile))
-        # @param spec [Gem::Specification]
-        lockfile.specs.each do |spec|
-          spec = spec.__materialize__
-          puts "Documenting #{spec.name} #{spec.version}"
-          `yard gems #{spec.name} #{spec.version} #{@rebuild ? '--rebuild' : ''}`
-          if RDOC_GEMS.include?(spec.name)
-            puts "  Caching custom documentation"
-            Solargraph::YardMap::RdocToYard.run(spec)
+        Bundler.with_clean_env do
+          Bundler.reset!
+          lockfile = Bundler::LockfileParser.new(Bundler.read_file(Bundler.default_lockfile))
+          # @param spec [Gem::Specification]
+          lockfile.specs.each do |spec|
+            spec = spec.__materialize__
+            yd = YARD::Registry.yardoc_file_for_gem(spec.name, spec.version)
+            if !yd || @rebuild
+              puts "Documenting #{spec.name} #{spec.version}"
+              `yard gems #{spec.name} #{spec.version} #{@rebuild ? '--rebuild' : ''}`
+            end
+            if RDOC_GEMS.include?(spec.name)
+              cache = File.join(Solargraph::YardMap::CoreDocs.cache_dir, 'gems', "#{spec.name}-#{spec.version}", 'yardoc')
+              next if File.exist?(cache) && !@rebuild
+              puts "Caching custom documentation for #{spec.name} #{spec.version}"
+              Solargraph::YardMap::RdocToYard.run(spec)
+            end
           end
         end
       end
+      Bundler.reset!
     end
   end
 end
