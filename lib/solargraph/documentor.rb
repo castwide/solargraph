c21ec40ae92aa67244605bf288dc8741fbdaf759
Fred Snyder <fsnyder@castwide.com>
2019-06-25 16:55:56 -0400

    Documentor warns for unmaterialized specs.

--- lib/solargraph/documentor.rb
+++ lib/solargraph/documentor.rb
@@ -14,29 +14,40 @@ module Solargraph
       @rebuild = rebuild
     end
 
+    # @return [Boolean] True if all specs were found and documented.
     def document
+      failures = 0
       Dir.chdir @directory do
         Bundler.with_clean_env do
           Bundler.reset!
           lockfile = Bundler::LockfileParser.new(Bundler.read_file(Bundler.default_lockfile))
           # @param spec [Gem::Specification]
           lockfile.specs.each do |spec|
-            spec = spec.__materialize__
-            yd = YARD::Registry.yardoc_file_for_gem(spec.name, spec.version)
+            real = spec.__materialize__
+            if real.nil?
+              puts "WARNING: #{spec.name} #{spec.version} not found"
+              failures += 1
+              next
+            end
+            yd = YARD::Registry.yardoc_file_for_gem(real.name, real.version)
             if !yd || @rebuild
-              puts "Documenting #{spec.name} #{spec.version}"
-              `yard gems #{spec.name} #{spec.version} #{@rebuild ? '--rebuild' : ''}`
+              puts "Documenting #{real.name} #{real.version}"
+              `yard gems #{real.name} #{real.version} #{@rebuild ? '--rebuild' : ''}`
             end
             if RDOC_GEMS.include?(spec.name)
-              cache = File.join(Solargraph::YardMap::CoreDocs.cache_dir, 'gems', "#{spec.name}-#{spec.version}", 'yardoc')
+              cache = File.join(Solargraph::YardMap::CoreDocs.cache_dir, 'gems', "#{real.name}-#{real.version}", 'yardoc')
               next if File.exist?(cache) && !@rebuild
-              puts "Caching custom documentation for #{spec.name} #{spec.version}"
-              Solargraph::YardMap::RdocToYard.run(spec)
+              puts "Caching custom documentation for #{real.name} #{real.version}"
+              Solargraph::YardMap::RdocToYard.run(real)
             end
           end
         end
       end
       Bundler.reset!
+      if failures > 0
+        puts "#{failures} spec#{failures == 1 ? '' : 's'} could not be found. You might need to run `bundle install` first."
+      end
+      failures == 0
     end
   end
 end
