fc66af693867175af0b67aa38ae75e2de28e5558
Fred Snyder <fsnyder@castwide.com>
2019-06-26 21:34:24 -0400

    Bundler and Travis changes (#203)
    * Travis uses Bundler 2.
    
    * Update bundler.
    
    * Partially reverting bundle setup.
    
    * BundlerMethods specs.
    
    * require_from_bundle skips directories without lockfiles.
    
    * Debugging bundler.
    
    * BundlerMethods spec bundles fixture with clean env.
    
    * Get bundler/require paths from clean env.
    
    * Switch bundler version.
    
    * Erroneous expectation.
    
    * Removed jruby-9.1.

--- lib/solargraph/api_map/bundler_methods.rb
+++ lib/solargraph/api_map/bundler_methods.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'shellwords'
 
 module Solargraph
   class ApiMap
@@ -6,21 +7,21 @@ module Solargraph
       module_function
 
       def require_from_bundle directory
-        return [] unless File.directory?(directory)
-        result = []
-        begin
-          Solargraph.logger.info "Using bundler/require"
-          Dir.chdir directory do
-            result.concat(Bundler.with_clean_env do
-              Bundler.definition.specs_for([:default]).map(&:name)
-            end)
-            Bundler.reset!
+        @require_from_bundle ||= begin
+          Solargraph.logger.info "Loading gems for bundler/require"
+          Bundler.with_clean_env do
+            specs = `cd #{Shellwords.escape(directory)} && bundle exec ruby -e "require 'bundler'; puts Bundler.definition.specs_for([:default]).map(&:name)"`
+            if specs
+              specs.lines.map(&:strip).reject(&:nil?)
+            else
+              []
+            end
           end
-          result
-        rescue Bundler::GemfileNotFound => e
-          Solargraph.logger.info "[#{e.class}] #{e.message}"
-          result
         end
+      end
+
+      def reset_require_from_bundle
+        @require_from_bundle = nil
       end
     end
   end
--- spec/api_map/bundler_methods_spec.rb
+++ spec/api_map/bundler_methods_spec.rb
@@ -1,27 +1,37 @@
 require 'tmpdir'
+require 'fileutils'
 
 describe Solargraph::ApiMap::BundlerMethods do
-  it 'find default gems from bundler/require' do
-    result = Solargraph::ApiMap::BundlerMethods.require_from_bundle('spec/fixtures/workspace')
-    expect(result).to eq(['backport', 'bundler'])
-    expect(Bundler.environment.specs.map(&:name)).to include('solargraph')
+  after :each do
+    Solargraph::ApiMap::BundlerMethods.reset_require_from_bundle
   end
 
-  it 'does not raise an error processing bundler/require without a Gemfile' do
-    Dir.mktmpdir do |tmp|
-      expect {
-        Solargraph::ApiMap::BundlerMethods.require_from_bundle(tmp)
-      }.not_to raise_error
+  describe 'with Gemfile.lock' do
+    before :all do
+      Bundler.with_clean_env do
+        `cd spec/fixtures/workspace && bundle install`
+      end
+    end
+
+    after :all do
+      File.unlink 'spec/fixtures/workspace/Gemfile.lock'
+    end
+
+    it 'finds default gems from bundler/require' do
+      result = Solargraph::ApiMap::BundlerMethods.require_from_bundle('spec/fixtures/workspace')
+      expect(result).to eq(['backport', 'bundler'])
     end
   end
 
-  it 'does not raise an error processing bundler/require without a base Bundler environment' do
-    Dir.mktmpdir do |tmp|
-      Dir.chdir tmp do
-        expect {
-          Solargraph::ApiMap::BundlerMethods.require_from_bundle(tmp)
-        }.not_to raise_error
-      end
+  describe 'without Gemfile.lock' do
+    it 'does not raise an error without a bundle' do
+      expect {
+        Dir.mktmpdir do |dir|
+          Bundler.with_clean_env do
+            Solargraph::ApiMap::BundlerMethods.require_from_bundle(dir)
+          end
+        end
+      }.not_to raise_error
     end
   end
 end
