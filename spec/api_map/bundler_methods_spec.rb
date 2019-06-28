d463297db2b1f6fc3e582febcdfeaa02811760d9
Fred Snyder <fsnyder@castwide.com>
2019-06-28 15:08:48 -0400

    Build Gemfile.lock per job.

--- spec/api_map/bundler_methods_spec.rb
+++ spec/api_map/bundler_methods_spec.rb
@@ -2,6 +2,18 @@ require 'tmpdir'
 require 'fileutils'
 
 describe Solargraph::ApiMap::BundlerMethods do
+  # Build the Gemfile.lock in specs so Travis jobs use the correct version of
+  # Bundler (e.g., Ruby 2.1 uses Bundler 1)
+  before :all do
+    Dir.chdir 'spec/fixtures/workspace' do
+      `bundle install`
+    end
+  end
+
+  after :all do
+    File.unlink 'spec/fixtures/workspace/Gemfile.lock'
+  end
+
   after :each do
     Solargraph::ApiMap::BundlerMethods.reset_require_from_bundle
   end
--- spec/documentor_spec.rb
+++ spec/documentor_spec.rb
@@ -1,6 +1,18 @@
 require 'tmpdir'
 
 describe Solargraph::Documentor do
+  # Build the Gemfile.lock in specs so Travis jobs use the correct version of
+  # Bundler (e.g., Ruby 2.1 uses Bundler 1)
+  before :all do
+    Dir.chdir 'spec/fixtures/workspace' do
+      `bundle install`
+    end
+  end
+
+  after :all do
+    File.unlink 'spec/fixtures/workspace/Gemfile.lock'
+  end
+
   it 'returns gemsets for directories with bundles' do
     gemset = Solargraph::Documentor.specs_from_bundle('spec/fixtures/workspace')
     expect(gemset.keys).to eq(['backport', 'bundler'])
