ed3e68eb596259bc913a7ae645f5403298c3943f
Fred Snyder <fsnyder@castwide.com>
2019-07-16 16:22:22 -0400

    Use webmock in specs (WIP)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,5 @@ SimpleCov.start
 require 'solargraph'
 # Suppress logger output in specs (if possible)
 Solargraph::Logging.logger.reopen(File::NULL) if Solargraph::Logging.logger.respond_to?(:reopen)
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
--- spec/yard_map/core_docs_spec.rb
+++ spec/yard_map/core_docs_spec.rb
@@ -2,14 +2,49 @@ require 'fileutils'
 require 'tmpdir'
 
 describe Solargraph::YardMap::CoreDocs do
-  before :all do
+  before :each do
     # Override the cache for testing
     @tmp_dir = Dir.mktmpdir
     @orig_env = ENV['SOLARGRAPH_CACHE']
     ENV['SOLARGRAPH_CACHE'] = @tmp_dir
+
+    stub_request(:get, "https://solargraph.org/download/versions.json").
+      with(
+        headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Host'=>'solargraph.org',
+      'User-Agent'=>'Ruby'
+      }).
+      to_return(status: 200, body: {
+        status: 'ok',
+        cores: ['2.6.0', '2.5.0', '2.4.0', '2.3.0', '2.2.0', '2.1.0']
+      }.to_json, headers: {})
+
+    @download_version = '2.2.2'
+
+    stub_request(:get, "https://solargraph.org/download/#{@download_version}.tar.gz").
+      with(
+        headers: {
+       'Accept'=>'*/*',
+       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       'Host'=>'solargraph.org',
+       'User-Agent'=>'Ruby'
+        }).
+      to_return(status: 200, body: File.read_binary("yardoc/#{@download_version}.tar.gz"), headers: {})
+
+    stub_request(:get, "https://solargraph.org/download/99.99.99.tar.gz").
+      with(
+        headers: {
+       'Accept'=>'*/*',
+       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       'Host'=>'solargraph.org',
+       'User-Agent'=>'Ruby'
+        }).
+      to_return(status: 404, body: "", headers: {})
   end
 
-  after :all do
+  after :each do
     # Delete the temp cache and reset
     FileUtils.rm_rf @tmp_dir, secure: true
     ENV['SOLARGRAPH_CACHE'] = @orig_env
@@ -42,10 +77,9 @@ describe Solargraph::YardMap::CoreDocs do
     expect(best_match).not_to be_nil
   end
 
-  it "downloads the best match" do
-    best_match = Solargraph::YardMap::CoreDocs.best_download
-    Solargraph::YardMap::CoreDocs.download best_match
-    expect(Solargraph::YardMap::CoreDocs.valid?(best_match)).to be(true)
+  it "downloads requested versions" do
+    Solargraph::YardMap::CoreDocs.download @download_version
+    expect(Solargraph::YardMap::CoreDocs.valid?(@download_version)).to be(true)
   end
 
   it 'reverts to earliest match for legacy versions' do
