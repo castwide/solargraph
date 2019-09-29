895573bdcf99e98b9c3204f416f9af12e6bbf4fe
Fred Snyder <fsnyder@castwide.com>
2019-09-29 13:48:32 -0400

    Hover escapes colons in return types.

--- lib/solargraph/language_server/message/text_document/hover.rb
+++ lib/solargraph/language_server/message/text_document/hover.rb
@@ -19,7 +19,7 @@ module Solargraph
               if !this_link.nil? && this_link != last_link
                 parts.push this_link
               end
-              parts.push pin.detail unless pin.is_a?(Pin::Namespace) || pin.detail.nil?
+              parts.push pin.detail.gsub(':', '\\:') unless pin.is_a?(Pin::Namespace) || pin.detail.nil?
               parts.push pin.documentation unless pin.documentation.nil? or pin.documentation.empty?
               contents.push parts.join("\n\n") unless parts.empty?
               last_link = this_link unless this_link.nil?
