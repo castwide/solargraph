cfa71a9808280e207cde53ee26f48bfa329ea673
Fred Snyder <fsnyder@castwide.com>
2019-10-25 02:55:10 -0400

    Minor formatting.

--- lib/solargraph/language_server/message.rb
+++ lib/solargraph/language_server/message.rb
@@ -56,38 +56,38 @@ module Solargraph
         end
       end
 
-      register 'initialize',                       Initialize
-      register 'initialized',                      Initialized
-      register 'textDocument/completion',          TextDocument::Completion
-      register 'completionItem/resolve',           CompletionItem::Resolve
-      register 'textDocument/signatureHelp',       TextDocument::SignatureHelp
-      register 'textDocument/didOpen',             TextDocument::DidOpen
-      register 'textDocument/didChange',           TextDocument::DidChange
-      register 'textDocument/didSave',             TextDocument::DidSave
-      register 'textDocument/didClose',            TextDocument::DidClose
-      register 'textDocument/hover',               TextDocument::Hover
-      register 'textDocument/definition',          TextDocument::Definition
-      register 'textDocument/formatting',          TextDocument::Formatting
-      register 'textDocument/onTypeFormatting',    TextDocument::OnTypeFormatting
-      register 'textDocument/documentSymbol',      TextDocument::DocumentSymbol
-      register 'textDocument/references',          TextDocument::References
-      register 'textDocument/rename',              TextDocument::Rename
-      register 'textDocument/prepareRename',       TextDocument::PrepareRename
-      register 'textDocument/foldingRange',        TextDocument::FoldingRange
-      register 'textDocument/codeAction',          TextDocument::CodeAction
-      register 'workspace/didChangeWatchedFiles',  Workspace::DidChangeWatchedFiles
-      register 'workspace/didChangeConfiguration', Workspace::DidChangeConfiguration
+      register 'initialize',                          Initialize
+      register 'initialized',                         Initialized
+      register 'textDocument/completion',             TextDocument::Completion
+      register 'completionItem/resolve',              CompletionItem::Resolve
+      register 'textDocument/signatureHelp',          TextDocument::SignatureHelp
+      register 'textDocument/didOpen',                TextDocument::DidOpen
+      register 'textDocument/didChange',              TextDocument::DidChange
+      register 'textDocument/didSave',                TextDocument::DidSave
+      register 'textDocument/didClose',               TextDocument::DidClose
+      register 'textDocument/hover',                  TextDocument::Hover
+      register 'textDocument/definition',             TextDocument::Definition
+      register 'textDocument/formatting',             TextDocument::Formatting
+      register 'textDocument/onTypeFormatting',       TextDocument::OnTypeFormatting
+      register 'textDocument/documentSymbol',         TextDocument::DocumentSymbol
+      register 'textDocument/references',             TextDocument::References
+      register 'textDocument/rename',                 TextDocument::Rename
+      register 'textDocument/prepareRename',          TextDocument::PrepareRename
+      register 'textDocument/foldingRange',           TextDocument::FoldingRange
+      register 'textDocument/codeAction',             TextDocument::CodeAction
+      register 'workspace/didChangeWatchedFiles',     Workspace::DidChangeWatchedFiles
+      register 'workspace/didChangeConfiguration',    Workspace::DidChangeConfiguration
       register 'workspace/didChangeWorkspaceFolders', Workspace::DidChangeWorkspaceFolders
-      register 'workspace/symbol',                 Workspace::WorkspaceSymbol
-      register '$/cancelRequest',                  CancelRequest
-      register '$/solargraph/document',            Extended::Document
-      register '$/solargraph/search',              Extended::Search
-      register '$/solargraph/checkGemVersion',     Extended::CheckGemVersion
-      register '$/solargraph/documentGems',        Extended::DocumentGems
-      register '$/solargraph/downloadCore',        Extended::DownloadCore
-      register '$/solargraph/environment',         Extended::Environment
-      register 'shutdown',                         Shutdown
-      register 'exit',                             ExitNotification
+      register 'workspace/symbol',                    Workspace::WorkspaceSymbol
+      register '$/cancelRequest',                     CancelRequest
+      register '$/solargraph/document',               Extended::Document
+      register '$/solargraph/search',                 Extended::Search
+      register '$/solargraph/checkGemVersion',        Extended::CheckGemVersion
+      register '$/solargraph/documentGems',           Extended::DocumentGems
+      register '$/solargraph/downloadCore',           Extended::DownloadCore
+      register '$/solargraph/environment',            Extended::Environment
+      register 'shutdown',                            Shutdown
+      register 'exit',                                ExitNotification
     end
   end
 end
