16f63547f33b371ae532b1c839cb078ee057dad1
Fred Snyder <fsnyder@castwide.com>
2019-11-22 16:25:29 -0500

    CommentRipper ignores inline comments.

--- lib/solargraph/parser/comment_ripper.rb
+++ lib/solargraph/parser/comment_ripper.rb
@@ -3,10 +3,14 @@ require 'ripper'
 module Solargraph
   module Parser
     class CommentRipper < Ripper::SexpBuilder
+      def initialize src, filename = '(ripper)', lineno = 0
+        super
+        @buffer = src
+      end
+
       def on_comment *args
         result = super
-        # @comments[result[2][0]] = result[1] if result[1][0..result[2][1]].strip =~ /$#?/
-        if result[1][0..result[2][1]].strip =~ /$#?/
+        if @buffer.lines[result[2][0]][0..result[2][1]].strip =~ /^#/
           chomped = result[1].chomp
           @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0], result[2][1], result[2][0], result[2][1] + chomped.length), chomped)
         end
