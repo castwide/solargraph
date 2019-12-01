3fe9da18da5ee84214d0029156a9e5a4ea1e1eb1
Fred Snyder <fsnyder@castwide.com>
2019-12-01 00:02:43 -0500

    Parser-independent test.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,6 +1,6 @@
 describe Solargraph::Parser do
   it "parses nodes" do
     node = Solargraph::Parser.parse('class Foo; end', 'test.rb')
-    expect(node).to be_a(Parser::AST::Node)
+    expect(Solargraph::Parser.is_ast_node?(node)).to be(true)
   end
 end
