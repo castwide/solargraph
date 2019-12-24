06a216753465fecf5b773e8e298ec0debb8b0173
Fred Snyder <fsnyder@castwide.com>
2019-12-23 21:33:12 -0500

    Static/dynamic documentHighlight.

--- lib/solargraph/language_server/message/initialize.rb
+++ lib/solargraph/language_server/message/initialize.rb
@@ -40,7 +40,6 @@ module Solargraph
             result[:capabilities].merge! static_workspace_symbols unless dynamic_registration_for?('workspace', 'symbol')
             result[:capabilities].merge! static_folding_range unless dynamic_registration_for?('textDocument', 'foldingRange')
             result[:capabilities].merge! static_highlights unless dynamic_registration_for?('textDocument', 'documentHighlight')
-            dynamic_registration_for?('textDocument', 'documentHighlight')
             # @todo Temporarily disabled
             # result[:capabilities].merge! static_code_action unless dynamic_registration_for?('textDocument', 'codeAction')
             set_result result
