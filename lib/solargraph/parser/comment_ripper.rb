3927ab4c6e8f7696b9ccb0d854dfd8f98474f981
Fred Snyder <fsnyder@castwide.com>
2020-01-16 15:04:45 -0500

    CommentRipper caches buffer lines array.

--- lib/solargraph/parser/comment_ripper.rb
+++ lib/solargraph/parser/comment_ripper.rb
@@ -6,11 +6,12 @@ module Solargraph
       def initialize src, filename = '(ripper)', lineno = 0
         super
         @buffer = src
+        @buffer_lines = @buffer.lines
       end
 
       def on_comment *args
         result = super
-        if @buffer.lines[result[2][0]][0..result[2][1]].strip =~ /^#/
+        if @buffer_lines[result[2][0]][0..result[2][1]].strip =~ /^#/
           chomped = result[1].chomp
           @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0], result[2][1], result[2][0], result[2][1] + chomped.length), chomped)
         end
