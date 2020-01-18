0c1a198c5d719d9e08fad53a6c30124888ddcfe2
Fred Snyder <fsnyder@castwide.com>
2020-01-18 06:42:51 -0500

    CommentRipper ignores frozen_string_literal.

--- lib/solargraph/parser/comment_ripper.rb
+++ lib/solargraph/parser/comment_ripper.rb
@@ -13,6 +13,7 @@ module Solargraph
         result = super
         if @buffer_lines[result[2][0]][0..result[2][1]].strip =~ /^#/
           chomped = result[1].chomp
+          chomped = '#' if result[2][0] == 0 && chomped =~ /^#\s*frozen_string_literal:/
           @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0], result[2][1], result[2][0], result[2][1] + chomped.length), chomped)
         end
         result
