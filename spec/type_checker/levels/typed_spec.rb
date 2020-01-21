52feca27c7623f0f8f67d915b82eb825ebd38da6
Fred Snyder <fsnyder@castwide.com>
2020-01-21 02:32:04 -0500

    Remove duplicate test.

--- spec/type_checker/levels/typed_spec.rb
+++ spec/type_checker/levels/typed_spec.rb
@@ -4,18 +4,6 @@ describe Solargraph::TypeChecker do
       Solargraph::TypeChecker.load_string(code, 'test.rb', :typed)
     end
 
-    it 'reports methods without either return tags or inferred types' do
-      checker = type_checker(%(
-        class Foo
-          def bar
-            unknown_method
-          end
-        end
-      ))
-      expect(checker.problems).to be_one
-      expect(checker.problems.first.message).to include('could not be inferred')
-    end
-
     it 'reports untyped methods without inferred types' do
       checker = type_checker(%(
         class Foo
