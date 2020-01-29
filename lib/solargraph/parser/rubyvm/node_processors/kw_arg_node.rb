45c8dffec6bd427835c011f384431900bf8817fb
Fred Snyder <fsnyder@castwide.com>
2020-01-29 04:57:30 -0500

    KW_ARG node changed in Ruby 2.7.

--- lib/solargraph/parser/rubyvm/node_processors/kw_arg_node.rb
+++ lib/solargraph/parser/rubyvm/node_processors/kw_arg_node.rb
@@ -11,12 +11,20 @@ module Solargraph
               closure: region.closure,
               comments: comments_for(node),
               name: node.children[0].children[0].to_s,
-              assignment: node.children[0].children[1],
+              assignment: require_keyword?(node) ? nil : node.children[0].children[1],
               presence: region.closure.location.range,
-              decl: node.children[0].children[1].nil? ? :kwarg : :kwoptarg
+              decl: require_keyword?(node) ? :kwarg : :kwoptarg
             )
             region.closure.parameters.push locals.last
             node.children[1] && NodeProcessor.process(node.children[1], region, pins, locals)
+          end
+
+          private
+
+          def require_keyword? node
+            # Ruby 2.7 changed required keywords to use a magic symbol instead
+            # of nil in the assignment node
+            node.children[0].children[1].nil? || node.children[0].children[1] == :NODE_SPECIAL_REQUIRED_KEYWORD
           end
         end
       end
