50d9bceb3b6a95f0966a2601b0ae575855198562
Fred Snyder <fsnyder@castwide.com>
2020-01-29 02:39:18 -0500

    Validate optional hash args as kwrestargs.

--- lib/solargraph/type_checker.rb
+++ lib/solargraph/type_checker.rb
@@ -270,9 +270,10 @@ module Solargraph
     def kwarg_problems_for argchain, api_map, block_pin, locals, location, pin, params, first
       result = []
       kwargs = convert_hash(argchain.node)
-      pin.parameters[first..-1].each do |par|
+      pin.parameters[first..-1].each_with_index do |par, cur|
+        idx = first + cur
         argchain = kwargs[par.name.to_sym]
-        if par.decl == :kwrestarg
+        if par.decl == :kwrestarg || (par.decl == :optarg && idx == pin.parameters.length - 1 && par.asgn_code == '{}')
           result.concat kwrestarg_problems_for(api_map, block_pin, locals, location, pin, params, kwargs)
         else
           if argchain
