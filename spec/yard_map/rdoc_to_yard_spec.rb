9273a781607a392ba2cdded7a4a552fa42aaa5ea
Fred Snyder <fsnyder@castwide.com>
2020-06-20 15:36:27 -0400

    Stale code.

--- spec/yard_map/rdoc_to_yard_spec.rb
+++ spec/yard_map/rdoc_to_yard_spec.rb
@@ -17,16 +17,6 @@ describe Solargraph::YardMap::RdocToYard do
   end
 
   it 'converts rdoc to yard' do
-    # Dir.mktmpdir do |tmpdir|
-    #   file = File.join('spec', 'fixtures', 'rdoc-lib', 'rdoc-lib.gemspec')
-    #   # @type [Gem::Specification]
-    #   spec = eval(File.read(file), binding, file)
-    #   spec.full_gem_path = File.join('spec', 'fixtures', 'rdoc-lib')
-    #   Solargraph::YardMap::RdocToYard.run(spec, cache_dir: tmpdir)
-    #   YARD::Registry.load tmpdir
-    #   obj = YARD::Registry.at('Example#example')
-    #   expect(obj).to be_a(YARD::CodeObjects::MethodObject)
-    # end
     obj = YARD::Registry.at('Example#example')
     expect(obj).to be_a(YARD::CodeObjects::MethodObject)
   end
