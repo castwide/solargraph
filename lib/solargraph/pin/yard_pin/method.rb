68aef0534427195a4915d84ee9019d0ea05db21e
Fred Snyder <fsnyder@castwide.com>
2020-07-25 10:44:45 -0400

    YardPin::Method skips invalid parameters (#345)

--- lib/solargraph/pin/yard_pin/method.rb
+++ lib/solargraph/pin/yard_pin/method.rb
@@ -6,6 +6,12 @@ module Solargraph
       class Method < Pin::Method
         include YardMixin
 
+        # @param code_object [YARD::CodeObjects::Base]
+        # @param name [String, nil]
+        # @param scope [Symbol, nil]
+        # @param visibility [Symbol, nil]
+        # @param closure [Solargraph::Pin::Closure, nil]
+        # @param spec [Gem::Specification]
         def initialize code_object, name = nil, scope = nil, visibility = nil, closure = nil, spec = nil
           closure ||= Solargraph::Pin::Namespace.new(
             name: code_object.namespace.to_s,
@@ -25,9 +31,14 @@ module Solargraph
 
         private
 
+        # @param code_object [YARD::CodeObjects::Base]
+        # @return [Array<Solargraph::Pin::Parameter>]
         def get_parameters code_object
           return [] unless code_object.is_a?(YARD::CodeObjects::MethodObject)
-          code_object.parameters.map do |a|
+          # HACK: Skip `nil` and `self` parameters that are sometimes emitted
+          # for methods defined in C
+          # See https://github.com/castwide/solargraph/issues/345
+          code_object.parameters.select { |a| a[0] && a[0] != 'self' }.map do |a|
             Solargraph::Pin::Parameter.new(
               location: location,
               closure: self,
@@ -40,10 +51,14 @@ module Solargraph
           end
         end
 
+        # @param a [Array]
+        # @return [String]
         def arg_name a
           a[0].match(/[A-Za-z0-9_]*/)[0]
         end
 
+        # @param a [Array]
+        # @return [Symbol]
         def arg_type a
           if a[0].start_with?('**')
             :kwrestarg
