16e359ba633e5470b46fe87ce0f63c6718fdbc40
Fred Snyder <fsnyder@castwide.com>
2020-09-13 16:32:46 -0400

    Minor linting.

--- lib/solargraph/yard_map/mapper.rb
+++ lib/solargraph/yard_map/mapper.rb
@@ -35,10 +35,10 @@ module Solargraph
             # This method of superclass detection is a bit of a hack. If
             # the superclass is a Proxy, it is assumed to be undefined in its
             # yardoc and converted to a fully qualified namespace.
-            if code_object.superclass.is_a?(YARD::CodeObjects::Proxy)
-              superclass = "::#{code_object.superclass}"
+            superclass = if code_object.superclass.is_a?(YARD::CodeObjects::Proxy)
+              "::#{code_object.superclass}"
             else
-              superclass = code_object.superclass.to_s
+              code_object.superclass.to_s
             end
             result.push Solargraph::Pin::Reference::Superclass.new(name: superclass, closure: nspin)
           end
