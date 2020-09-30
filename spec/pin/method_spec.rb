95b7235ee90496afc47746b3a067acf84acc1c83
Fred Snyder <fsnyder@castwide.com>
2020-09-29 23:27:18 -0400

    Attribute specs.

--- spec/pin/method_spec.rb
+++ spec/pin/method_spec.rb
@@ -262,4 +262,62 @@ describe Solargraph::Pin::Method do
     )
     expect(pin.documentation).to include('the foo text string')
   end
+
+  context 'as attribute' do
+    it "is a kind of attribute/property" do
+      source = Solargraph::Source.load_string(%(
+        class Foo
+          attr_reader :bar
+        end
+      ))
+      map = Solargraph::SourceMap.map(source)
+      pin = map.pins.select{|p| p.is_a?(Solargraph::Pin::Method)}.first
+      expect(pin).to be_attribute
+      expect(pin.completion_item_kind).to eq(Solargraph::LanguageServer::CompletionItemKinds::PROPERTY)
+      expect(pin.symbol_kind).to eq(Solargraph::LanguageServer::SymbolKinds::PROPERTY)
+    end
+
+    it "uses return type tags" do
+      pin = Solargraph::Pin::Method.new(name: 'bar', comments: '@return [File]', attribute: true)
+      expect(pin.return_type.tag).to eq('File')
+    end
+
+    it "detects undefined types" do
+      pin = Solargraph::Pin::Method.new(name: 'bar', attribute: true)
+      expect(pin.return_type).to be_undefined
+    end
+
+    it "generates paths" do
+      npin = Solargraph::Pin::Namespace.new(name: 'Foo', type: :class)
+      ipin = Solargraph::Pin::Method.new(closure: npin, name: 'bar', attribute: true, scope: :instance)
+      expect(ipin.path).to eq('Foo#bar')
+      cpin = Solargraph::Pin::Method.new(closure: npin, name: 'bar', attribute: true, scope: :class)
+      expect(cpin.path).to eq('Foo.bar')
+    end
+
+    it "handles invalid return type tags" do
+      pin = Solargraph::Pin::Method.new(name: 'bar', comments: '@return [Array<]', attribute: true)
+      expect(pin.return_type).to be_undefined
+    end
+
+    it 'infers untagged types from instance variables' do
+      source = Solargraph::Source.load_string(%(
+        class Foo
+          attr_reader :bar
+          attr_writer :bar
+          def initialize
+            @bar = String.new
+          end
+        end
+      ))
+      api_map = Solargraph::ApiMap.new
+      api_map.map source
+      pin = api_map.get_path_pins('Foo#bar').first
+      expect(pin.typify(api_map)).to be_undefined
+      expect(pin.probe(api_map).tag).to eq('String')
+      pin = api_map.get_path_pins('Foo#bar=').first
+      expect(pin.typify(api_map)).to be_undefined
+      expect(pin.probe(api_map).tag).to eq('String')
+    end
+  end
 end
