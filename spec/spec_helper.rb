7acecff281108a9388f46e2d40f26620daa4a770
Fred Snyder <fsnyder@castwide.com>
2020-12-05 17:29:45 -0500

    Remove legacy Ruby warts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
-if RUBY_VERSION =~ /^2\.(1|2)\./
-  # HACK: Avoid using stdgems in Ruby < 2.3
-  $LOAD_PATH.unshift File.realpath(File.join(File.dirname(__FILE__), '..', 'lib'))
-else
-  require 'bundler/setup'
-end
+require 'bundler/setup'
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
 require 'simplecov'
