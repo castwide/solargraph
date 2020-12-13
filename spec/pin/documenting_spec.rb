f9a4e294d03254151d5df896be87666999a0a559
Fred Snyder <fsnyder@castwide.com>
2020-12-13 16:03:44 -0500

    Documenting specs

---
+++ spec/pin/documenting_spec.rb
@@ -0,0 +1,23 @@
+describe Solargraph::Pin::Documenting do
+  let(:object) {
+    Class.new do |cls|
+      cls.include Solargraph::Pin::Documenting
+      cls.attr_accessor :docstring
+    end.new
+  }
+
+  it 'parses indented code blocks' do
+    object.docstring = YARD::Docstring.new(%(Method overview
+
+Example code:
+
+  class Foo; end
+))
+    expect(object.documentation).to include("```ruby\nclass Foo; end\n```")
+  end
+
+  it 'allows unclosed tags' do
+    object.docstring = YARD::Docstring.new('comment <tt>code')
+    expect(object.documentation).to include('comment `code`')
+  end
+end
