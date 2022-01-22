db4caa0d822c3e59c63933875017778169ae86f6
Fred Snyder <fsnyder@castwide.com>
2022-01-22 11:43:12 -0500

    Basic support for RSpec #describe and #it

--- lib/solargraph/convention/rspec.rb
+++ lib/solargraph/convention/rspec.rb
@@ -8,13 +8,22 @@ module Solargraph
         @environ ||= Environ.new(
           requires: ['rspec'],
           domains: ['RSpec::Matchers', 'RSpec::ExpectationGroups'],
-          # This override is necessary due to an erroneous @return tag in
-          # rspec's YARD documentation.
-          # @todo The return types have been fixed (https://github.com/rspec/rspec-expectations/pull/1121)
           pins: [
+            # This override is necessary due to an erroneous @return tag in
+            # rspec's YARD documentation.
+            # @todo The return types have been fixed (https://github.com/rspec/rspec-expectations/pull/1121)
             Solargraph::Pin::Reference::Override.method_return('RSpec::Matchers#expect', 'RSpec::Expectations::ExpectationTarget')
-          ]
+          ].concat(extras)
         )
+      end
+
+      private
+
+      def extras
+        @@extras ||= SourceMap.load_string(%(
+          def describe(*args); end
+          def it(*args); end
+        )).pins
       end
     end
   end
