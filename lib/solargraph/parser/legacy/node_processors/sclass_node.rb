0a520aeec1c855add1b4b0c2d2846b0787866c1a
Fred Snyder <fsnyder@castwide.com>
2022-12-08 04:20:54 -0500

    Process non-self singleton classes in Ruby < 2.6

--- lib/solargraph/parser/legacy/node_processors/sclass_node.rb
+++ lib/solargraph/parser/legacy/node_processors/sclass_node.rb
@@ -6,11 +6,32 @@ module Solargraph
       module NodeProcessors
         class SclassNode < Parser::NodeProcessor::Base
           def process
-            # @todo Temporarily skipping remote metaclasses
-            return unless node.children[0].is_a?(AST::Node) && node.children[0].type == :self
+            sclass = node.children[0]
+            if sclass.is_a?(AST::Node) && sclass.type == :self
+              closure = region.closure
+            elsif sclass.is_a?(AST::Node) && sclass.type == :casgn
+              names = [region.closure.namespace, region.closure.name]
+              if sclass.children[0].nil? && names.last != sclass.children[1].to_s
+                names << sclass.children[1].to_s
+              else
+                names.concat [NodeMethods.unpack_name(sclass.children[0]), sclass.children[1].to_s]
+              end
+              name = names.reject(&:empty?).join('::')
+              closure = Solargraph::Pin::Namespace.new(name: name, location: region.closure.location)
+            elsif sclass.is_a?(AST::Node) && sclass.type == :const
+              names = [region.closure.namespace, region.closure.name]
+              also = NodeMethods.unpack_name(sclass)
+              if also != region.closure.name
+                names << also
+              end
+              name = names.reject(&:empty?).join('::')
+              closure = Solargraph::Pin::Namespace.new(name: name, location: region.closure.location)
+            else
+              return
+            end
             pins.push Solargraph::Pin::Singleton.new(
               location: get_node_location(node),
-              closure: region.closure
+              closure: closure
             )
             process_children region.update(visibility: :public, scope: :class, closure: pins.last)
           end
