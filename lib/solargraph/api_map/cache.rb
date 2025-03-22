ee8d96a98cc7d189a67fd9700a563d029b872d4d
Fred Snyder <fsnyder@castwide.com>
2025-03-22 05:13:41 -0400

    Fix ApiMap::Cache

--- lib/solargraph/api_map/cache.rb
+++ lib/solargraph/api_map/cache.rb
@@ -12,6 +12,7 @@ module Solargraph
         @qualified_namespaces = {}
         # @type [Hash{String => Pin::Method}]
         @receiver_definitions = {}
+        @clips = {}
       end
 
       # @param fqns [String]
@@ -20,7 +21,7 @@ module Solargraph
       # @param deep [Boolean]
       # @return [Array<Pin::Method>]
       def get_methods fqns, scope, visibility, deep
-        @methods[[fqns, scope, visibility.sort, deep]]
+        @methods["#{fqns}|#{scope}|#{visibility}|#{deep}"]
       end
 
       # @param fqns [String]
@@ -30,14 +31,14 @@ module Solargraph
       # @param value [Array<Pin::Method>]
       # @return [void]
       def set_methods fqns, scope, visibility, deep, value
-        @methods[[fqns, scope, visibility.sort, deep]] = value
+        @methods["#{fqns}|#{scope}|#{visibility}|#{deep}"] = value
       end
 
       # @param namespace [String]
       # @param contexts [Array<String>]
       # @return [Array<Pin::Base>]
       def get_constants namespace, contexts
-        @constants[[namespace, contexts]]
+        @constants["#{namespace}|#{contexts}"]
       end
 
       # @param namespace [String]
@@ -45,14 +46,14 @@ module Solargraph
       # @param value [Array<Pin::Base>]
       # @return [void]
       def set_constants namespace, contexts, value
-        @constants[[namespace, contexts]] = value
+        @constants["#{namespace}|#{contexts}"] = value
       end
 
       # @param name [String]
       # @param context [String]
       # @return [String]
       def get_qualified_namespace name, context
-        @qualified_namespaces[[name, context]]
+        @qualified_namespaces["#{name}|#{context}"]
       end
 
       # @param name [String]
@@ -60,7 +61,7 @@ module Solargraph
       # @param value [String]
       # @return [void]
       def set_qualified_namespace name, context, value
-        @qualified_namespaces[[name, context]] = value
+        @qualified_namespaces["#{name}|#{context}"] = value
       end
 
       # @param path [String]
@@ -81,12 +82,25 @@ module Solargraph
         @receiver_definitions[path] = pin
       end
 
+      # @param cursor [Source::Cursor]
+      # @return [SourceMap::Clip, nil]
+      def get_clip(cursor)
+        @clips["#{cursor.filename}|#{cursor.range.inspect}"]
+      end
+
+      # @param cursor [Source::Cursor]
+      # @param clip [SourceMap::Clip]
+      def set_clip(cursor, clip)
+        @clips["#{cursor.filename}|#{cursor.range.inspect}"] = clip
+      end
+
       # @return [void]
       def clear
         @methods.clear
         @constants.clear
         @qualified_namespaces.clear
         @receiver_definitions.clear
+        @clips.clear
       end
 
       # @return [Boolean]
@@ -94,7 +108,8 @@ module Solargraph
         @methods.empty? &&
           @constants.empty? &&
           @qualified_namespaces.empty? &&
-          @receiver_definitions.empty?
+          @receiver_definitions.empty? &&
+          @clips.empty?
       end
     end
   end
