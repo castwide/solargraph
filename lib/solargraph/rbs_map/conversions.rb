368be9f95f78cf74add86db7e7237bf6f2907c14
Fred Snyder <fsnyder@castwide.com>
2025-03-23 11:08:11 -0400

    DRY mixin processing

--- lib/solargraph/rbs_map/conversions.rb
+++ lib/solargraph/rbs_map/conversions.rb
@@ -173,15 +173,7 @@ module Solargraph
             name: decl.super_class.name.relative!.to_s
           )
         end
-
-        decl.each_mixin do |mixin|
-          klass = mixin.is_a?(RBS::AST::Members::Include) ? Pin::Reference::Include : Pin::Reference::Extend
-          pins.push klass.new(
-            name: mixin.name.relative!.to_s,
-            location: rbs_location_to_location(mixin.location),
-            closure: class_pin.closure
-          )
-        end
+        add_mixins decl, class_pin.closure
         convert_members_to_pins decl, class_pin
       end
 
@@ -218,14 +210,7 @@ module Solargraph
         convert_self_types_to_pins decl, module_pin
         convert_members_to_pins decl, module_pin
 
-        decl.each_mixin do |mixin|
-          klass = mixin.is_a?(RBS::AST::Members::Include) ? Pin::Reference::Include : Pin::Reference::Extend
-          pins.push klass.new(
-            name: mixin.name.relative!.to_s,
-            location: rbs_location_to_location(mixin.location),
-            closure: module_pin.closure
-          )
-        end
+        add_mixins decl, module_pin.closure
       end
 
       # @param name [String]
@@ -641,6 +626,19 @@ module Solargraph
         else
           Solargraph.logger.warn "Unrecognized RBS type: #{type.class} at #{type.location}"
           'undefined'
+        end
+      end
+
+      # @param decl [RBS::AST::Declarations::Class, RBS::AST::Declarations::Module]
+      # @param closure [Pin::Closure]
+      def add_mixins decl, closure
+        decl.each_mixin do |mixin|
+          klass = mixin.is_a?(RBS::AST::Members::Include) ? Pin::Reference::Include : Pin::Reference::Extend
+          pins.push klass.new(
+            name: mixin.name.relative!.to_s,
+            location: rbs_location_to_location(mixin.location),
+            closure: closure
+          )
         end
       end
     end
