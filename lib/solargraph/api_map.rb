35eb3826e12a99981da07e0ccc4704e7047416db
Fred Snyder <fsnyder@castwide.com>
2025-03-24 05:09:24 -0400

    Disable ApiMap clip cache

--- lib/solargraph/api_map.rb
+++ lib/solargraph/api_map.rb
@@ -460,8 +460,10 @@ module Solargraph
     def clip cursor
       raise FileNotFoundError, "ApiMap did not catalog #{cursor.filename}" unless source_map_hash.key?(cursor.filename)
 
-      cache.get_clip(cursor) ||
-        SourceMap::Clip.new(self, cursor).tap { |clip| cache.set_clip(cursor, clip) }
+      # @todo Clip caches are disabled pending resolution of a stale cache bug
+      # cache.get_clip(cursor) ||
+      #   SourceMap::Clip.new(self, cursor).tap { |clip| cache.set_clip(cursor, clip) }
+      SourceMap::Clip.new(self, cursor)
     end
 
     # Get an array of document symbols from a file.
