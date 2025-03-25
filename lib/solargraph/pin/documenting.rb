a76030ec548f6fa954c8b0fcffa5714616bedce4
Fred Snyder <fsnyder@castwide.com>
2025-03-24 20:07:58 -0400

    Revise documentation formatting

--- lib/solargraph/pin/documenting.rb
+++ lib/solargraph/pin/documenting.rb
@@ -38,23 +38,18 @@ module Solargraph
         end
 
         def to_s
-          return "\n```ruby\n#{@plaintext}#{@plaintext.end_with?("\n") ? '' : "\n"}```\n\n" if code?
-          ReverseMarkdown.convert unescape_brackets(Kramdown::Document.new(escape_brackets(@plaintext), input: 'GFM').to_html)
+          return to_code if code?
+          to_markdown
         end
 
         private
 
-        # @param text [String]
-        # @return [String]
-        def escape_brackets text
-          # text.gsub(/(\[[^\]]*\])([^\(]|\z)/, '!!!^\1^!!!\2')
-          text.gsub('[', '!!!!b').gsub(']', 'e!!!!')
+        def to_code
+          "\n```ruby\n#{Documenting.normalize_indentation(@plaintext)}#{@plaintext.end_with?("\n") ? '' : "\n"}```\n\n"
         end
 
-        # @param text [String]
-        # @return [String]
-        def unescape_brackets text
-          text.gsub('!!!!b', '[').gsub('e!!!!', ']')
+        def to_markdown
+          ReverseMarkdown.convert Kramdown::Document.new(@plaintext, input: 'GFM').to_html
         end
       end
 
@@ -65,40 +60,30 @@ module Solargraph
           # line and at least two spaces of indentation. This is a common
           # convention in Ruby core documentation, e.g., String#split.
           sections = [DocSection.new(false)]
-          normalize_indentation(docstring.to_s).gsub(/\t/, '  ').lines.each do |l|
-            if l.strip.empty?
-              sections.last.concat l
-            else
-              if (l =~ /^  [^\s]/ && sections.last.plaintext =~ /(\r?\n[ \t]*?){2,}$/) || (l.start_with?('  ') && sections.last.code?)
-                # Code block
-                sections.push DocSection.new(true) unless sections.last.code?
-                sections.last.concat l[2..-1]
-              else
-                # Regular documentation
-                sections.push DocSection.new(false) if sections.last.code?
-                sections.last.concat l
-              end
+          Documenting.normalize_indentation(Documenting.strip_html_comments(docstring.to_s.gsub("\t", '  '))).lines.each do |l|
+            if l.start_with?('  ')
+              # Code block
+              sections.push DocSection.new(true) unless sections.last.code?
+            elsif sections.last.code?
+              # Regular documentation
+              sections.push DocSection.new(false)
             end
+            sections.last.concat l
           end
           sections.map(&:to_s).join.strip
         end
       end
 
-      private
+      def self.strip_html_comments text
+        text.gsub(/<!--([\s\S]*?)-->/, '').strip
+      end
 
       # @param text [String]
       # @return [String]
-      def normalize_indentation text
-        text.lines.map { |l| remove_odd_spaces(l) }.join
-      end
-
-      # @param line [String]
-      # @return [String]
-      def remove_odd_spaces line
-        return line unless line.start_with?(' ')
-        spaces = line.match(/^ +/)[0].length
-        return line unless spaces.odd?
-        line[1..-1]
+      def self.normalize_indentation text
+        left = text.lines.map { |line| line.match(/^ +/)&.match(0)&.length || 0 }.min
+        return text if left.nil? || left.zero?
+        text.lines.map { |line| line[left..] }.join
       end
     end
   end
