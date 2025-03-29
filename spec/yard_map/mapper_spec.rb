98a3cbecee19a44f23ff88ec55932415b83f810f
Vince Broz <vince@broz.cc>
2025-03-29 16:14:35 -0400

    [regression] Ensure YardMap gems have return type for Class<T>.new (#850)
    * [regression] Ensure YardMap gems have return type for Class<T>.new
    
    * Handle self return-type-setting at lower level
    
    * Fix bug where signature return type was based only on annotation

--- spec/yard_map/mapper_spec.rb
+++ spec/yard_map/mapper_spec.rb
@@ -22,6 +22,27 @@ describe Solargraph::YardMap::Mapper do
     expect(pin.explicit?).to be(true)
   end
 
+  it 'marks correct return type from StringIO.new' do
+    # Using stringio because it's a known dependency
+    stringio = Gem::Specification.find_by_name('stringio')
+    Solargraph::Yardoc.cache(stringio)
+    Solargraph::Yardoc.load!(stringio)
+    pins = Solargraph::YardMap::Mapper.new(YARD::Registry.all).map
+    pins = pins.select { |pin| pin.path == 'StringIO.new' }
+    expect(pins.map(&:return_type).uniq.map(&:to_s)).to eq(['self'])
+  end
+
+  it 'marks correct return type from RuboCop::Options.new' do
+    # Using rubocop because it's a known dependency
+    rubocop = Gem::Specification.find_by_name('rubocop')
+    Solargraph::Yardoc.cache(rubocop)
+    Solargraph::Yardoc.load!(rubocop)
+    pins = Solargraph::YardMap::Mapper.new(YARD::Registry.all).map
+    pins = pins.select { |pin| pin.path == 'RuboCop::Options.new' }
+    expect(pins.map(&:return_type).uniq.map(&:to_s)).to eq(['self'])
+    expect(pins.flat_map(&:signatures).map(&:return_type).uniq.map(&:to_s)).to eq(['self'])
+  end
+
   it 'marks non-explicit methods' do
     # Using rspec-expectations because it's a known dependency
     rspec = Gem::Specification.find_by_name('rspec-expectations')
