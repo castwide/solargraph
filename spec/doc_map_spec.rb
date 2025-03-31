95d125c297276cd1e68d571aecf417483786253a
Fred Snyder <fsnyder@castwide.com>
2025-03-31 16:41:27 -0400

    Enable passing tests (#854)

--- spec/doc_map_spec.rb
+++ spec/doc_map_spec.rb
@@ -29,8 +29,7 @@ describe Solargraph::DocMap do
     expect(doc_map.uncached_gemspecs).to eq([gemspec])
   end
 
-  # @todo This test fails on CI for some reason
-  xit 'does not warn for redundant requires' do
+  it 'does not warn for redundant requires' do
     # Requiring 'set' is unnecessary because it's already included in core. It
     # might make sense to log redundant requires, but a warning is overkill.
     expect(Solargraph.logger).not_to receive(:warn)
