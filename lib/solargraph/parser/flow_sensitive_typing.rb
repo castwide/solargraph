1736a8fd1980d08e98466136f5fa619b5a34108e
Vince Broz <vince@broz.cc>
2025-04-02 16:09:57 -0400

    Extract to new file

--- lib/solargraph/parser/flow_sensitive_typing.rb
+++ lib/solargraph/parser/flow_sensitive_typing.rb
@@ -12,26 +12,31 @@ module Solargraph
 
       # @return [void]
       def run
-        conditional_node = node.children[0]
-        then_clause = node.children[1]
-        #
-        # See if we can refine a type based on the result of 'if foo.nil?'
-        #
-        # [3] pry(main)> require 'parser/current'; Parser::CurrentRuby.parse("if foo.is_a? Baz; then foo; else bar; end")
-        # => s(:if,
-        #   s(:send,
-        #     s(:send, nil, :foo), :is_a?,
-        #     s(:const, nil, :Baz)),
-        #   s(:send, nil, :foo),
-        #   s(:send, nil, :bar))
-        # [4] pry(main)>
-        return unless conditional_node.type == :send && conditional_node.children[1] == :is_a?
+        process_node(node)
+      end
+
+      def process_node(node)
+        return process_if(node) if node.type == :if
+        return process_or(node) if node.type == :or
+      end
+
+      def process_or(node)
+        lhs = node.children[0]
+        rhs = node.children[1]
+        if_true = {}
+        if_false = {}
+        gather_facts(conditional_node, if_true, if_false)
+
+      end
+
+      def process_conditional(node, if_true, if_false)
+        return unless node.type == :send && node.children[1] == :is_a?
         # Check if conditional node follows this pattern:
         #   s(:send,
         #     s(:send, nil, :foo), :is_a?,
         #     s(:const, nil, :Baz)),
-        isa_receiver = conditional_node.children[0]
-        isa_class = conditional_node.children[2]
+        isa_receiver = node.children[0]
+        isa_class = node.children[2]
         return unless isa_class.type == :const
         # pay attention to above parse tree while writing code
         isa_module = isa_class.children[0]
@@ -56,24 +61,53 @@ module Solargraph
         return if variable_name.nil? || variable_name.empty?
         pins = locals.select { |pin| pin.name == variable_name }
         return unless pins.length == 1
+        if_true[pins.first] ||= []
+        if_true[pins.first] << { type: isa_type_name }
+      end
+
+      def process_if(node)
+        conditional_node = node.children[0]
+        then_clause = node.children[1]
+        #
+        # See if we can refine a type based on the result of 'if foo.nil?'
+        #
+        # [3] pry(main)> require 'parser/current'; Parser::CurrentRuby.parse("if foo.is_a? Baz; then foo; else bar; end")
+        # => s(:if,
+        #   s(:send,
+        #     s(:send, nil, :foo), :is_a?,
+        #     s(:const, nil, :Baz)),
+        #   s(:send, nil, :foo),
+        #   s(:send, nil, :bar))
+        # [4] pry(main)>
+        if_true = {}
+        if_false = {}
+        process_conditional(conditional_node, if_true, if_false)
+        return if if_true.empty? && if_false.empty?
+
         return if then_clause.nil?
+
         before_then_clause_loc = then_clause.location.expression.adjust(begin_pos: -1)
         then_presence = Range.new(Position.new(before_then_clause_loc.line, before_then_clause_loc.column),
                                   get_node_end_position(then_clause))
+        if_true.each_pair do |pin, facts|
+          facts.each do |fact|
+            isa_type_name = fact.fetch(:type)
+            # @todo Create pin#update method
+            then_pin = Solargraph::Pin::LocalVariable.new(
+              location: pin.location,
+              closure: pin.closure,
+              name: pin.name,
+              assignment: pin.assignment,
+              comments: pin.comments,
+              presence: then_presence,
+              return_type: ComplexType.try_parse(isa_type_name),
+              declaration: true
+            )
+            locals.push(then_pin)
+          end
+        end
+
         # else_presence = Range.new(get_node_start_position(else_clause), get_node_end_position(else_clause))
-        pin = pins.first
-        # @todo Create pin#update method
-        then_pin = Solargraph::Pin::LocalVariable.new(
-          location: pin.location,
-          closure: pin.closure,
-          name: pin.name,
-          assignment: pin.assignment,
-          comments: pin.comments,
-          presence: then_presence,
-          return_type: ComplexType.try_parse(isa_type_name),
-          declaration: true
-        )
-        locals.push(then_pin)
       end
 
       private
