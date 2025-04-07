4b5a756f68cf63470caafd2621df1765f2eb9437
Vince Broz <vince@broz.cc>
2025-04-06 22:06:09 -0400

    Mark methods as private

--- lib/solargraph/parser/flow_sensitive_typing.rb
+++ lib/solargraph/parser/flow_sensitive_typing.rb
@@ -4,20 +4,9 @@ module Solargraph
       include Solargraph::Parser::NodeMethods
 
       # @param locals [Array<Solargraph::Pin::BaseVariable>]
-      def initialize(locals, enclosing_block_pin = nil)
+      def initialize(locals, enclosing_breakable_pin = nil)
         @locals = locals
-        @enclosing_block_pin = enclosing_block_pin
-      end
-
-      # @param node [Parser::AST::Node]
-      # @return [void]
-      def run(node)
-        process_node(node)
-      end
-
-      def process_node(node)
-        return process_if(node) if node.type == :if
-        # process_or(node) if node.type == :or
+        @enclosing_breakable_pin = enclosing_breakable_pin
       end
 
       # def process_or(node)
@@ -27,50 +16,6 @@ module Solargraph
       #   if_false = {}
       #   gather_facts(conditional_node, if_true, if_false)
       # end
-
-      # @param node [Parser::AST::Node]
-      def type_name(node)
-        # e.g.,
-        #  s(:const, nil, :Baz)
-        return unless node.type == :const
-        module_node = node.children[0]
-        class_node = node.children[1]
-
-        return class_node.to_s if module_node.nil?
-
-        module_type_name = type_name(module_node)
-        return unless module_type_name
-
-        "#{module_type_name}::#{class_node}"
-      end
-
-      # @param conditional_node [Parser::AST::Node]
-      def process_conditional(conditional_node, if_true, if_false)
-        return unless conditional_node.type == :send && conditional_node.children[1] == :is_a?
-        # Check if conditional node follows this pattern:
-        #   s(:send,
-        #     s(:send, nil, :foo), :is_a?,
-        #     s(:const, nil, :Baz)),
-        isa_receiver = conditional_node.children[0]
-        isa_type_name = type_name(conditional_node.children[2])
-        return unless isa_type_name
-        # check if isa_receiver looks like this:
-        #  s(:send, nil, :foo)
-        # and set variable_name to :foo
-        if isa_receiver.type == :send && isa_receiver.children[0].nil? && isa_receiver.children[1].is_a?(Symbol)
-          variable_name = isa_receiver.children[1].to_s
-        end
-        # or like this:
-        # (lvar :repr)
-        variable_name = isa_receiver.children[0].to_s if isa_receiver.type == :lvar
-        return if variable_name.nil? || variable_name.empty?
-        conditional_range = Range.from_node(conditional_node)
-        pins = locals.select { |pin| pin.name == variable_name && pin.presence.include?(conditional_range.start) }
-        return unless pins.length == 1
-
-        if_true[pins.first] ||= []
-        if_true[pins.first] << { type: isa_type_name }
-      end
 
       # @param if_node [Parser::AST::Node]
       def process_if(if_node)
@@ -121,7 +66,7 @@ module Solargraph
         end
 
         if always_breaks?(else_clause)
-          unless enclosing_block_pin.nil? # TODO is break correct?
+          unless enclosing_breakable_pin.nil?
             #
             # Add specialized locals for the rest of the block
             #
@@ -129,7 +74,7 @@ module Solargraph
               facts.each do |fact|
                 isa_type_name = fact.fetch(:type)
                 remaining_block_presence = Range.new(get_node_end_position(if_node),
-                                                     get_node_end_position(enclosing_block_pin.node))
+                                                     get_node_end_position(enclosing_breakable_pin.node))
                 # @todo Create pin#update method
                 remaining_loop_pin = Solargraph::Pin::LocalVariable.new(
                   location: pin.location,
@@ -150,6 +95,50 @@ module Solargraph
 
       private
 
+      # @param conditional_node [Parser::AST::Node]
+      def process_conditional(conditional_node, if_true, if_false)
+        return unless conditional_node.type == :send && conditional_node.children[1] == :is_a?
+        # Check if conditional node follows this pattern:
+        #   s(:send,
+        #     s(:send, nil, :foo), :is_a?,
+        #     s(:const, nil, :Baz)),
+        isa_receiver = conditional_node.children[0]
+        isa_type_name = type_name(conditional_node.children[2])
+        return unless isa_type_name
+        # check if isa_receiver looks like this:
+        #  s(:send, nil, :foo)
+        # and set variable_name to :foo
+        if isa_receiver.type == :send && isa_receiver.children[0].nil? && isa_receiver.children[1].is_a?(Symbol)
+          variable_name = isa_receiver.children[1].to_s
+        end
+        # or like this:
+        # (lvar :repr)
+        variable_name = isa_receiver.children[0].to_s if isa_receiver.type == :lvar
+        return if variable_name.nil? || variable_name.empty?
+        conditional_range = Range.from_node(conditional_node)
+        pins = locals.select { |pin| pin.name == variable_name && pin.presence.include?(conditional_range.start) }
+        return unless pins.length == 1
+
+        if_true[pins.first] ||= []
+        if_true[pins.first] << { type: isa_type_name }
+      end
+
+      # @param node [Parser::AST::Node]
+      def type_name(node)
+        # e.g.,
+        #  s(:const, nil, :Baz)
+        return unless node.type == :const
+        module_node = node.children[0]
+        class_node = node.children[1]
+
+        return class_node.to_s if module_node.nil?
+
+        module_type_name = type_name(module_node)
+        return unless module_type_name
+
+        "#{module_type_name}::#{class_node}"
+      end
+
       # @todo "return type could not be inferred" should not trigger here
       # @sg-ignore
       def always_breaks?(clause_node)
@@ -158,7 +147,7 @@ module Solargraph
 
       attr_reader :locals
 
-      attr_reader :enclosing_block_pin
+      attr_reader :enclosing_breakable_pin
     end
   end
 end
