aa801af3cc58c3703aafd18c05b1bbc0bd959703
Vince Broz <vince@broz.cc>
2025-04-24 23:09:22 -0400

    Understand mass assignment into instance variables (#893)

--- spec/source_map/clip_spec.rb
+++ spec/source_map/clip_spec.rb
@@ -2682,6 +2682,31 @@ describe Solargraph::SourceMap::Clip do
     expect(clip.infer.to_s).to eq('nil')
   end
 
+  it 'handles mass assignment into instance variables' do
+    source = Solargraph::Source.load_string(%(
+      class Blah
+        def initialize
+          @foo, @bar = baz
+          @foo
+          @bar
+        end
+
+        # @return [Array(Integer, String)]
+        def baz
+          [1, 'foo']
+        end
+      end
+    ), 'test.rb')
+
+    api_map = Solargraph::ApiMap.new.map(source)
+
+    clip = api_map.clip_at('test.rb', [4, 10])
+    expect(clip.infer.to_s).to eq('Integer')
+
+    clip = api_map.clip_at('test.rb', [5, 10])
+    expect(clip.infer.to_s).to eq('String')
+  end
+
   it 'handles a complex yieldreturn type resolution situation' do
     source = Solargraph::Source.load_string(%(
       module A
