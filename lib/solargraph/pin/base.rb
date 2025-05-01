4f7b79156dc546dfc046cdf65994c0b598c5523a
Vince Broz <vince@broz.cc>
2025-05-01 06:23:07 -0400

    Drop source from Pin::Base#equality_fields

--- lib/solargraph/pin/base.rb
+++ lib/solargraph/pin/base.rb
@@ -43,7 +43,8 @@ module Solargraph
 
       # @sg-ignore Fix "Not enough arguments to Module#protected"
       protected def equality_fields
-        [self.class, identity, code_object, location, type_location, name, path, source, comments, closure]
+        # 'source' not included so that top level namespaces are comparable, whether from RBS, code or a constant
+        [self.class, identity, code_object, location, type_location, name, path, comments, closure, presence_certain?, return_type]
       end
 
       # specialize some things from Equality mix-in
