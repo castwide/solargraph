f12f8a131b3029933cf6fe8728c2c1c23bf137f4
Fred Snyder <fsnyder@castwide.com>
2025-05-14 17:56:02 -0400

    Clip#define returns variable pins (#934)

--- lib/solargraph/library.rb
+++ lib/solargraph/library.rb
@@ -193,11 +193,7 @@ module Solargraph
       else
         mutex.synchronize do
           clip = api_map.clip(cursor)
-          if cursor.assign?
-            [Pin::ProxyType.new(name: cursor.word, return_type: clip.infer)]
-          else
-            clip.define.map { |pin| pin.realize(api_map) }
-          end
+          clip.define.map { |pin| pin.realize(api_map) }
         end
       end
     rescue FileNotFoundError => e
