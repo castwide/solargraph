0108ec04f6e95542cafe1eadd76509a4b66f2955
Vince Broz <vince@broz.cc>
2025-06-01 11:23:44 -0400

    Add type annotations to comment_ripper.rb for strict typechecking (#924)

--- lib/solargraph/parser/comment_ripper.rb
+++ lib/solargraph/parser/comment_ripper.rb
@@ -13,6 +13,7 @@ module Solargraph
       end
 
       def on_comment *args
+        # @type [Array(Symbol, String, Array([Integer, nil], [Integer, nil]))]
         result = super
         if @buffer_lines[result[2][0]][0..result[2][1]].strip =~ /^#/
           chomped = result[1].chomp
@@ -24,24 +25,28 @@ module Solargraph
         result
       end
 
+      # @param result [Array(Symbol, String, Array([Integer, nil], [Integer, nil]))]
+      # @return [void]
+      def create_snippet(result)
+        chomped = result[1].chomp
+        @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0] || 0, result[2][1] || 0, result[2][0] || 0, (result[2][1] || 0) + chomped.length), chomped)
+      end
+
       def on_embdoc_beg *args
         result = super
-        chomped = result[1].chomp
-        @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0], result[2][1], result[2][0], result[2][1] + chomped.length), chomped)
+        create_snippet(result)
         result
       end
 
       def on_embdoc *args
         result = super
-        chomped = result[1].chomp
-        @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0], result[2][1], result[2][0], result[2][1] + chomped.length), chomped)
+        create_snippet(result)
         result
       end
 
       def on_embdoc_end *args
         result = super
-        chomped = result[1].chomp
-        @comments[result[2][0]] = Snippet.new(Range.from_to(result[2][0], result[2][1], result[2][0], result[2][1] + chomped.length), chomped)
+        create_snippet(result)
         result
       end
 
