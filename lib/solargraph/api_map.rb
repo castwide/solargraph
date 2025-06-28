9ddbb716537ebb2854fca376a367f968e5e22ea9
Vince Broz <vince@broz.cc>
2025-06-28 08:52:32 -0400

    GemPins pin merging improvements (#946)
    * GemPins pin merging improvements
    
    Put responsibility for intelligently merging multiple pins for the
    same underlying item (e.g. from different sources like YARD and RBS)
    into the Pin class hierarchy.
    
    * GemPins pin merging improvements
    
    * GemPins pin merging improvements
    
    * GemPins pin merging improvements
    
    * GemPins pin merging improvements
    
    * GemPins pin merging improvements
    
    * Improve parameter setting
    
    * GemPins pin merging improvements
    
    * GemPins pin merging improvements
    
    * Improved handling of YARD macros
    
    * Mark asserts pending different PRs
    
    * Avoid bad sources of return_type when possible
    
    * Prefer method pins wtih non-undefined signatures
    
    * Ensure overrides apply to all pins for a path, since we use all
    
    * Reset method  parameters and block if needed in reset_generated!
    
    * Drop ill-conceived backup location info
    
    * Add more to dodgy_visibility_source?
    
    * Add some type tags
    
    * Fix issue found in typechecking
    
    * Handle a nil case in Pin::Base#assert_same
    
    * Add annotations for strong typechecking on Pin::Base
    
    * Handle a block combination situation
    
    * Fix merge issues
    
    * Fix merge issue
    
    * Handle cases related to future RBS collection pins
    
    * Adjust tests to match code
    
    * Drop assert exclusions
    
    * Reenable test
    
    * Efficiency fixes
    
    * Fix broken <=> approach

--- lib/solargraph/api_map.rb
+++ lib/solargraph/api_map.rb
@@ -631,6 +631,19 @@ module Solargraph
       store.get_includes(host_ns).map { |inc_tag| ComplexType.parse(inc_tag).name }.include?(module_ns)
     end
 
+    # @param pins [Enumerable<Pin::Base>]
+    # @param visibility [Enumerable<Symbol>]
+    # @return [Array<Pin::Base>]
+    def resolve_method_aliases pins, visibility = [:public, :private, :protected]
+      with_resolved_aliases = pins.map do |pin|
+        resolved = resolve_method_alias(pin)
+        next nil if resolved.respond_to?(:visibility) && !visibility.include?(resolved.visibility)
+        resolved
+      end.compact
+      logger.debug { "ApiMap#resolve_method_aliases(pins=#{pins.map(&:name)}, visibility=#{visibility}) => #{with_resolved_aliases.map(&:name)}" }
+      GemPins.combine_method_pins_by_path(with_resolved_aliases)
+    end
+
     private
 
     # A hash of source maps with filename keys.
@@ -861,17 +874,6 @@ module Solargraph
         end
       end
       result + nil_pins
-    end
-
-    # @param pins [Enumerable<Pin::Base>]
-    # @param visibility [Enumerable<Symbol>]
-    # @return [Array<Pin::Base>]
-    def resolve_method_aliases pins, visibility = [:public, :private, :protected]
-      pins.map do |pin|
-        resolved = resolve_method_alias(pin)
-        next pin if resolved.respond_to?(:visibility) && !visibility.include?(resolved.visibility)
-        resolved
-      end.compact
     end
 
     # @param pin [Pin::MethodAlias, Pin::Base]
