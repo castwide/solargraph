780e516cb4bf3d4af4f9a693d3462397e240cea4
Vince Broz <vince@broz.cc>
2025-07-09 15:51:21 -0400

    RuboCop fixes

--- lib/solargraph/logging.rb
+++ lib/solargraph/logging.rb
@@ -11,11 +11,11 @@ module Solargraph
       'info' => Logger::INFO,
       'debug' => Logger::DEBUG
     }
-    configured_level = ENV['SOLARGRAPH_LOG']
+    configured_level = ENV.fetch('SOLARGRAPH_LOG', nil)
     level = if LOG_LEVELS.keys.include?(configured_level)
               LOG_LEVELS.fetch(configured_level)
             else
-              STDERR.puts("Invalid value for SOLARGRAPH_LOG: #{configured_level.inspect} - valid values are #{LOG_LEVELS.keys}") if configured_level
+              warn "Invalid value for SOLARGRAPH_LOG: #{configured_level.inspect} - valid values are #{LOG_LEVELS.keys}" if configured_level
               DEFAULT_LOG_LEVEL
             end
     @@logger = Logger.new(STDERR, level: level)
