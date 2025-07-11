32a2f342066b29fcbf568ce82d6546c7f77bc1e5
Vince Broz <vince@broz.cc>
2025-07-11 18:04:21 -0400

    Linting

--- lib/solargraph/pin_cache.rb
+++ lib/solargraph/pin_cache.rb
@@ -27,6 +27,8 @@ module Solargraph
         File.join(base_dir, "ruby-#{RUBY_VERSION}", "rbs-#{RBS::VERSION}", "solargraph-#{Solargraph::VERSION}")
       end
 
+      # @param gemspec [Gem::Specification]
+      # @return [String]
       def yardoc_path gemspec
         File.join(base_dir,
                   "yard-#{YARD::VERSION}",
@@ -34,94 +36,147 @@ module Solargraph
                   "#{gemspec.name}-#{gemspec.version}.yardoc")
       end
 
+      # @return [String]
       def stdlib_path
         File.join(work_dir, 'stdlib')
       end
 
+      # @param require [String]
+      # @return [String]
       def stdlib_require_path require
         File.join(stdlib_path, "#{require}.ser")
       end
 
+      # @param require [String]
+      # @return [Array<Pin::Base>]
       def deserialize_stdlib_require require
         load(stdlib_require_path(require))
       end
 
+      # @param require [String]
+      # @param pins [Array<Pin::Base>]
+      # @return [void]
       def serialize_stdlib_require require, pins
         save(stdlib_require_path(require), pins)
       end
 
+      # @return [String]
       def core_path
         File.join(work_dir, 'core.ser')
       end
 
+      # @return [Array<Pin::Base>]
       def deserialize_core
         load(core_path)
       end
 
+      # @param pins [Array<Pin::Base>]
+      # @return [void]
       def serialize_core pins
         save(core_path, pins)
       end
 
+      # @param gemspec [Gem::Specification]
+      # @return [String]
       def yard_gem_path gemspec
         File.join(work_dir, 'yard', "#{gemspec.name}-#{gemspec.version}.ser")
       end
 
+      # @param gemspec [Gem::Specification]
+      # @return [Array<Pin::Base>]
       def deserialize_yard_gem(gemspec)
         load(yard_gem_path(gemspec))
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param pins [Array<Pin::Base>]
+      # @return [void]
       def serialize_yard_gem(gemspec, pins)
         save(yard_gem_path(gemspec), pins)
       end
 
+      # @param gemspec [Gem::Specification]
+      # @return [Boolean]
       def has_yard?(gemspec)
         exist?(yard_gem_path(gemspec))
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @return [String]
       def rbs_collection_path(gemspec, hash)
         File.join(work_dir, 'rbs', "#{gemspec.name}-#{gemspec.version}-#{hash || 0}.ser")
       end
 
+      # @param gemspec [Gem::Specification]
+      # @return [String]
       def rbs_collection_path_prefix(gemspec)
         File.join(work_dir, 'rbs', "#{gemspec.name}-#{gemspec.version}-")
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @return [Array<Pin::Base>]
       def deserialize_rbs_collection_gem(gemspec, hash)
         load(rbs_collection_path(gemspec, hash))
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @param pins [Array<Pin::Base>]n
+      # @return [void]
       def serialize_rbs_collection_gem(gemspec, hash, pins)
         save(rbs_collection_path(gemspec, hash), pins)
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @return [String]
       def combined_path(gemspec, hash)
         File.join(work_dir, 'combined', "#{gemspec.name}-#{gemspec.version}-#{hash || 0}.ser")
       end
 
+      # @param gemspec [Gem::Specification]
+      # @return [String]
       def combined_path_prefix(gemspec)
         File.join(work_dir, 'combined', "#{gemspec.name}-#{gemspec.version}-")
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @param pins [Array<Pin::Base>]
+      # @return [void]
       def serialize_combined_gem(gemspec, hash, pins)
         save(combined_path(gemspec, hash), pins)
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @return [Array<Pin::Base>]
       def deserialize_combined_gem gemspec, hash
         load(combined_path(gemspec, hash))
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param hash [String, nil]
+      # @return [Boolean]
       def has_rbs_collection?(gemspec, hash)
         exist?(rbs_collection_path(gemspec, hash))
       end
 
+      # @return [void]
       def uncache_core
         uncache(core_path)
       end
 
+      # @return [void]
       def uncache_stdlib
         uncache(stdlib_path)
       end
 
+      # @param gemspec [Gem::Specification]
+      # @param out [IO, nil]
+      # @return [void]
       def uncache_gem(gemspec, out: nil)
         uncache(yardoc_path(gemspec), out: out)
         uncache_by_prefix(rbs_collection_path_prefix(gemspec), out: out)
@@ -147,11 +202,12 @@ module Solargraph
         nil
       end
 
+      # @param path [String]
       def exist? *path
-        File.file? join(*path)
+        File.file? File.join(*path)
       end
 
-      # @param path [Array<String>]
+      # @param file [String]
       # @param pins [Array<Pin::Base>]
       # @return [void]
       def save file, pins
@@ -162,8 +218,8 @@ module Solargraph
         logger.debug { "Cache#save: Saved #{pins.length} pins to #{file}" }
       end
 
-      # @return [void]
       # @param path_segments [Array<String>]
+      # @return [void]
       def uncache *path_segments, out: nil
         path = File.join(*path_segments)
         if File.exist?(path)
--- spec/shell_spec.rb
+++ spec/shell_spec.rb
@@ -2,34 +2,35 @@ require 'tmpdir'
 require 'open3'
 
 describe Solargraph::Shell do
+  let(:temp_dir) { Dir.mktmpdir }
+
   before do
-    @temp_dir = Dir.mktmpdir
-    File.open(File.join(@temp_dir, 'Gemfile'), 'w') do |file|
+    File.open(File.join(temp_dir, 'Gemfile'), 'w') do |file|
         file.puts "source 'https://rubygems.org'"
-        file.puts "gem 'solargraph', path: #{File.expand_path('../..', __FILE__)}"
+        file.puts "gem 'solargraph', path: #{File.expand_path('..', __dir__)}"
     end
-    output, status = Open3.capture2e("bundle install", chdir: @temp_dir)
-    expect(status.success?).to eq(true), ->{ "Bundle install failed: output=#{output}" }
+    output, status = Open3.capture2e("bundle install", chdir: temp_dir)
+    raise "Failure installing bundle: #{output}" unless status.success?
   end
 
   def bundle_exec(*cmd)
     # run the command in the temporary directory with bundle exec
-    output, status = Open3.capture2e("bundle exec #{cmd.join(' ')}", chdir: @temp_dir)
-    expect(status.success?).to eq(true), "Command failed: #{output}"
+    output, status = Open3.capture2e("bundle exec #{cmd.join(' ')}", chdir: temp_dir)
+    expect(status.success?).to be(true), "Command failed: #{output}"
     output
   end
 
   after do
     # remove the temporary directory after the tests
-    FileUtils.remove_entry(@temp_dir) if Dir.exist?(@temp_dir)
+    FileUtils.rm_rf(temp_dir)
   end
 
   describe "--version" do
     it "returns a version when run" do
       output = bundle_exec("solargraph", "--version")
 
-      expect(output).to_not be_empty
-      expect(output).to eq (Solargraph::VERSION + "\n")
+      expect(output).not_to be_empty
+      expect(output).to eq("#{Solargraph::VERSION}\n")
     end
   end
 
