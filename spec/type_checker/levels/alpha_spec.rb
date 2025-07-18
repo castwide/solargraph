8e9cb6f19710bb9bdaefa09dbcb350b4a90685e8
Vince Broz <vince@broz.cc>
2025-07-18 18:37:35 -0400

    Add alpha typechecking spec

---
+++ spec/type_checker/levels/alpha_spec.rb
@@ -0,0 +1,22 @@
+describe Solargraph::TypeChecker do
+  context 'alpha level' do
+    def type_checker(code)
+      Solargraph::TypeChecker.load_string(code, 'test.rb', :alpha)
+    end
+
+    it 'reports nilable type issues' do
+      checker = type_checker(%(
+        # @param a [String]
+        # @return [void]
+        def foo(a); end
+
+        # @param b [String, nil]
+        # @return [void]
+        def bar(b)
+         foo(b)
+        end
+      ))
+      expect(checker.problems.map(&:message)).to eq(["Wrong argument type for #foo: a expected String, received String, nil"])
+    end
+  end
+end
