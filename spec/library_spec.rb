581cc02321f791e16721305a7036310b71466211
Vince Broz <vince@broz.cc>
2025-07-28 15:03:36 -0400

    Ensure backport is pre-cached

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -26,20 +26,26 @@ describe Solargraph::Library do
     expect(completion.pins.map(&:name)).to include('x')
   end
 
-  it "returns a Completion from a gem" do
-    library = Solargraph::Library.new(Solargraph::Workspace.new(Dir.pwd,
-                                                                Solargraph::Workspace::Config.new))
-    library.attach Solargraph::Source.load_string(%(
-      require 'backport'
+  context 'with a require from an already-cached external gem' do
+    before do
+      Solargraph::Shell.new.gems('backport')
+    end
 
-      # @param adapter [Backport::Adapter]
-      def foo(adapter)
-        adapter.remo
-      end
-    ), 'file.rb', 0)
-    completion = library.completions_at('file.rb', 5, 19)
-    expect(completion).to be_a(Solargraph::SourceMap::Completion)
-    expect(completion.pins.map(&:name)).to include('remote')
+    it "returns a Completion" do
+      library = Solargraph::Library.new(Solargraph::Workspace.new(Dir.pwd,
+                                                                  Solargraph::Workspace::Config.new))
+      library.attach Solargraph::Source.load_string(%(
+        require 'backport'
+
+        # @param adapter [Backport::Adapter]
+        def foo(adapter)
+          adapter.remo
+        end
+      ), 'file.rb', 0)
+      completion = library.completions_at('file.rb', 5, 19)
+      expect(completion).to be_a(Solargraph::SourceMap::Completion)
+      expect(completion.pins.map(&:name)).to include('remote')
+    end
   end
 
   it "gets definitions from a file" do
