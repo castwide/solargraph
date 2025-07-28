df609a612f8f775c8892d0ce2cfde238395128b9
Vince Broz <vince@broz.cc>
2025-07-28 19:32:48 -0400

    Revert double quote changes for another PR

--- lib/solargraph/shell.rb
+++ lib/solargraph/shell.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'benchmark'
-require 'thor'
-require 'yard'
-require 'yaml'
+require "benchmark"
+require "thor"
+require "yard"
+require "yaml"
 
 module Solargraph
   class Shell < Thor
@@ -16,25 +16,25 @@ module Solargraph
 
     map %w[--version -v] => :version
 
-    desc '--version, -v', 'Print the version'
+    desc "--version, -v", "Print the version"
     # @return [void]
     def version
       puts Solargraph::VERSION
     end
 
-    desc 'socket', 'Run a Solargraph socket server'
-    option :host, type: :string, aliases: :h, desc: 'The server host', default: '127.0.0.1'
-    option :port, type: :numeric, aliases: :p, desc: 'The server port', default: 7658
+    desc "socket", "Run a Solargraph socket server"
+    option :host, type: :string, aliases: :h, desc: "The server host", default: "127.0.0.1"
+    option :port, type: :numeric, aliases: :p, desc: "The server port", default: 7658
     # @return [void]
     def socket
-      require 'backport'
+      require "backport"
       port = options[:port]
       port = available_port if port.zero?
       Backport.run do
-        Signal.trap('INT') do
+        Signal.trap("INT") do
           Backport.stop
         end
-        Signal.trap('TERM') do
+        Signal.trap("TERM") do
           Backport.stop
         end
         # @sg-ignore https://github.com/castwide/backport/pull/5
@@ -43,15 +43,15 @@ module Solargraph
       end
     end
 
-    desc 'stdio', 'Run a Solargraph stdio server'
+    desc "stdio", "Run a Solargraph stdio server"
     # @return [void]
     def stdio
-      require 'backport'
+      require "backport"
       Backport.run do
-        Signal.trap('INT') do
+        Signal.trap("INT") do
           Backport.stop
         end
-        Signal.trap('TERM') do
+        Signal.trap("TERM") do
           Backport.stop
         end
         # @sg-ignore https://github.com/castwide/backport/pull/5
@@ -60,11 +60,11 @@ module Solargraph
       end
     end
 
-    desc 'config [DIRECTORY]', 'Create or overwrite a default configuration file'
-    option :extensions, type: :boolean, aliases: :e, desc: 'Add installed extensions', default: true
+    desc "config [DIRECTORY]", "Create or overwrite a default configuration file"
+    option :extensions, type: :boolean, aliases: :e, desc: "Add installed extensions", default: true
     # @param directory [String]
     # @return [void]
-    def config directory = '.'
+    def config directory = "."
       matches = []
       if options[:extensions]
         # @sg-ignore
@@ -78,84 +78,84 @@ module Solargraph
       conf = Solargraph::Workspace::Config.new.raw_data
       unless matches.empty?
         matches.each do |m|
-          conf['extensions'].push m
+          conf["extensions"].push m
         end
       end
-      File.open(File.join(directory, '.solargraph.yml'), 'w') do |file|
+      File.open(File.join(directory, ".solargraph.yml"), "w") do |file|
         file.puts conf.to_yaml
       end
-      STDOUT.puts 'Configuration file initialized.'
+      STDOUT.puts "Configuration file initialized."
     end
 
-    desc 'clear', 'Delete all cached documentation'
+    desc "clear", "Delete all cached documentation"
     long_desc %(
       This command will delete all core and gem documentation from the cache.
     )
     # @return [void]
     def clear
-      puts 'Deleting all cached documentation (gems, core and stdlib)'
+      puts "Deleting all cached documentation (gems, core and stdlib)"
       Solargraph::PinCache.clear
     end
-    map 'clear-cache' => :clear
-    map 'clear-cores' => :clear
+    map "clear-cache" => :clear
+    map "clear-cores" => :clear
 
-    desc 'cache', 'Cache a gem', hide: true
-    option :rebuild, type: :boolean, desc: 'Rebuild existing documentation', default: false
+    desc "cache", "Cache a gem", hide: true
+    option :rebuild, type: :boolean, desc: "Rebuild existing documentation", default: false
     # @return [void]
     # @param gem [String]
     # @param version [String, nil]
     def cache gem, version = nil
-      gems(gem + (version ? "=#{version}" : ''))
+      gems(gem + (version ? "=#{version}" : ""))
       # "
     end
 
-    desc 'gems [GEM[=VERSION]]', 'Cache documentation for installed gems'
-    option :rebuild, type: :boolean, desc: 'Rebuild existing documentation', default: false
+    desc "gems [GEM[=VERSION]]", "Cache documentation for installed gems"
+    option :rebuild, type: :boolean, desc: "Rebuild existing documentation", default: false
     # @param names [Array<String>]
     # @return [void]
     def gems *names
-      api_map = ApiMap.load('.')
+      api_map = ApiMap.load(".")
       if names.empty?
         api_map.cache_all_for_workspace!($stdout, rebuild: options[:rebuild])
       else
         $stderr.puts("Caching these gems: #{names}")
         names.each do |name|
-          if name == 'core'
+          if name == "core"
             PinCache.cache_core(out: $stdout)
             next
           end
 
-          gemspec = api_map.find_gem(*name.split('='))
+          gemspec = api_map.find_gem(*name.split("="))
           if gemspec.nil?
-            warn "Gem '#{name}' not found"
+            warn "Gem "#{name}" not found"
           else
             api_map.cache_gem(gemspec, rebuild: options[:rebuild], out: $stdout)
           end
         rescue Gem::MissingSpecError
-          warn "Gem '#{name}' not found"
+          warn "Gem "#{name}" not found"
         end
         $stderr.puts "Documentation cached for #{names.count} gems."
       end
     end
 
-    desc 'uncache GEM [...GEM]', 'Delete specific cached gem documentation'
+    desc "uncache GEM [...GEM]", "Delete specific cached gem documentation"
     long_desc %(
-      Specify one or more gem names to clear. 'core' or 'stdlib' may
+      Specify one or more gem names to clear. "core" or "stdlib" may
       also be specified to clear cached system documentation.
       Documentation will be regenerated as needed.
     )
     # @param gems [Array<String>]
     # @return [void]
     def uncache *gems
-      raise ArgumentError, 'No gems specified.' if gems.empty?
-      workspace = Workspace.new('.')
+      raise ArgumentError, "No gems specified." if gems.empty?
+      workspace = Workspace.new(".")
       gems.each do |gem|
-        if gem == 'core'
+        if gem == "core"
           PinCache.uncache_core(out: $stdout)
           next
         end
 
-        if gem == 'stdlib'
+        if gem == "stdlib"
           PinCache.uncache_stdlib(out: $stdout)
           next
         end
@@ -165,21 +165,21 @@ module Solargraph
       end
     end
 
-    desc 'reporters', 'Get a list of diagnostics reporters'
+    desc "reporters", "Get a list of diagnostics reporters"
     # @return [void]
     def reporters
       puts Solargraph::Diagnostics.reporters
     end
 
-    desc 'typecheck [FILE(s)]', 'Run the type checker'
+    desc "typecheck [FILE(s)]", "Run the type checker"
     long_desc %(
       Perform type checking on one or more files in a workspace. Check the
       entire workspace if no files are specified.
 
       Type checking levels are normal, typed, strict, and strong.
     )
-    option :level, type: :string, aliases: [:mode, :m, :l], desc: 'Type checking level', default: 'normal'
-    option :directory, type: :string, aliases: :d, desc: 'The workspace directory', default: '.'
+    option :level, type: :string, aliases: [:mode, :m, :l], desc: "Type checking level", default: "normal"
+    option :directory, type: :string, aliases: :d, desc: "The workspace directory", default: "."
     # @return [void]
     def typecheck *files
       directory = File.realpath(options[:directory])
@@ -207,20 +207,20 @@ module Solargraph
         # "
       }
       puts "Typecheck finished in #{time.real} seconds."
-      puts "#{probcount} problem#{probcount != 1 ? 's' : ''} found#{files.length != 1 ? " in #{filecount} of #{files.length} files" : ''}."
+      puts "#{probcount} problem#{probcount != 1 ? "s" : ""} found#{files.length != 1 ? " in #{filecount} of #{files.length} files" : ""}."
       # "
       exit 1 if probcount > 0
     end
 
-    desc 'scan', 'Test the workspace for problems'
+    desc "scan", "Test the workspace for problems"
     long_desc %(
       A scan loads the entire workspace to make sure that the ASTs and
       maps do not raise errors during analysis. It does not perform any type
       checking or validation; it only confirms that the analysis itself is
       error-free.
     )
-    option :directory, type: :string, aliases: :d, desc: 'The workspace directory', default: '.'
-    option :verbose, type: :boolean, aliases: :v, desc: 'Verbose output', default: false
+    option :directory, type: :string, aliases: :d, desc: "The workspace directory", default: "."
+    option :verbose, type: :boolean, aliases: :v, desc: "Verbose output", default: false
     # @return [void]
     def scan
       directory = File.realpath(options[:directory])
@@ -234,7 +234,7 @@ module Solargraph
             pin.typify api_map
             pin.probe api_map
           rescue StandardError => e
-            STDERR.puts "Error testing #{pin_description(pin)} #{pin.location ? "at #{pin.location.filename}:#{pin.location.range.start.line + 1}" : ''}"
+            STDERR.puts "Error testing #{pin_description(pin)} #{pin.location ? "at #{pin.location.filename}:#{pin.location.range.start.line + 1}" : ""}"
             STDERR.puts "[#{e.class}]: #{e.message}"
             STDERR.puts e.backtrace.join("\n")
             exit 1
@@ -244,9 +244,9 @@ module Solargraph
       puts "Scanned #{directory} (#{api_map.pins.length} pins) in #{time.real} seconds."
     end
 
-    desc 'list', 'List the files in the workspace and the total count'
-    option :count, type: :boolean, aliases: :c, desc: 'Display the file count only', default: false
-    option :directory, type: :string, aliases: :d, desc: 'The directory to read', default: '.'
+    desc "list", "List the files in the workspace and the total count"
+    option :count, type: :boolean, aliases: :c, desc: "Display the file count only", default: false
+    option :directory, type: :string, aliases: :d, desc: "The directory to read", default: "."
     # @return [void]
     def list
       workspace = Solargraph::Workspace.new(options[:directory])
