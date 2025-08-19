af232049e380cd8ce1eaa22cce1f7a43a2ca64bf
Vince Broz <vince@broz.cc>
2025-08-19 12:30:09 -0400

    Fix spec

--- spec/rbs_map/conversions_spec.rb
+++ spec/rbs_map/conversions_spec.rb
@@ -1,61 +1,63 @@
 describe Solargraph::RbsMap::Conversions do
-  # create a temporary directory with the scope of the spec
-  around do |example|
-    require 'tmpdir'
-    Dir.mktmpdir("rspec-solargraph-") do |dir|
-      @temp_dir = dir
-      example.run
+  context 'with RBS to digest' do
+    # create a temporary directory with the scope of the spec
+    around do |example|
+      require 'tmpdir'
+      Dir.mktmpdir("rspec-solargraph-") do |dir|
+        @temp_dir = dir
+        example.run
+      end
     end
-  end
 
-  let(:rbs_repo) do
-    RBS::Repository.new(no_stdlib: false)
-  end
+    let(:rbs_repo) do
+      RBS::Repository.new(no_stdlib: false)
+    end
 
-  let(:loader) do
-    RBS::EnvironmentLoader.new(core_root: nil, repository: rbs_repo)
-  end
+    let(:loader) do
+      RBS::EnvironmentLoader.new(core_root: nil, repository: rbs_repo)
+    end
 
-  let(:conversions) do
-    Solargraph::RbsMap::Conversions.new(loader: loader)
-  end
+    let(:conversions) do
+      Solargraph::RbsMap::Conversions.new(loader: loader)
+    end
 
-  let(:pins) do
-    conversions.pins
-  end
+    let(:pins) do
+      conversions.pins
+    end
 
-  before do
-    rbs_file = File.join(temp_dir, 'foo.rbs')
-    File.write(rbs_file, rbs)
-    loader.add(path: Pathname(temp_dir))
-  end
+    before do
+      rbs_file = File.join(temp_dir, 'foo.rbs')
+      File.write(rbs_file, rbs)
+      loader.add(path: Pathname(temp_dir))
+    end
 
-  attr_reader :temp_dir
+    attr_reader :temp_dir
 
-  context 'with untyped response' do
-    let(:rbs) do
-      <<~RBS
+    context 'with untyped response' do
+      let(:rbs) do
+        <<~RBS
           class Foo
             def bar: () -> untyped
           end
         RBS
-    end
+      end
 
-    subject(:method_pin) { pins.find { |pin| pin.path == 'Foo#bar' } }
+      subject(:method_pin) { pins.find { |pin| pin.path == 'Foo#bar' } }
 
-    it { should_not be_nil }
+      it { should_not be_nil }
 
-    it { should be_a(Solargraph::Pin::Method) }
+      it { should be_a(Solargraph::Pin::Method) }
 
-    it 'maps untyped in RBS to undefined in Solargraph 'do
-      expect(method_pin.return_type.tag).to eq('undefined')
+      it 'maps untyped in RBS to undefined in Solargraph 'do
+        expect(method_pin.return_type.tag).to eq('undefined')
+      end
     end
   end
 
   if Gem::Version.new(RBS::VERSION) >= Gem::Version.new('3.9.1')
     context 'with method pin for Open3.capture2e' do
       it 'accepts chdir kwarg' do
-        api_map = Solargraph::ApiMap.load_with_cache('.')
+        api_map = Solargraph::ApiMap.load_with_cache('.', $stdout)
 
         method_pin = api_map.pins.find do |pin|
           pin.is_a?(Solargraph::Pin::Method) && pin.path == 'Open3.capture2e'
