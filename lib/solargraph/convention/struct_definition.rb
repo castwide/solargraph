2e6e327518a73b641680357d623e8407e8610b5c
Vince Broz <vince@broz.cc>
2025-08-19 07:20:11 -0400

    [regression] Fix crash while typechecking files with Struct use (#1031)
    * [regression] Fix crash while typechecking files with Struct use
    
    It looks like a mis-set closure field on a Parameter in is causing
    exceptions while typechecking code with Struct use inside:
    
    https://github.com/iftheshoefritz/solargraph-rails/actions/runs/17040847020/job/48303963447?pr=158
    
    * Set types in parameters and instance variables
    
    * Fix specs
    
    * Fix linting issues
    
    * Rebaseline .rubocop_todo.yml
    
    * Rebaseline .rubocop_todo.yml

--- lib/solargraph/convention/struct_definition.rb
+++ lib/solargraph/convention/struct_definition.rb
@@ -20,7 +20,8 @@ module Solargraph
               name: struct_definition_node.class_name,
               docstring: docstring,
               visibility: :public,
-              gates: region.closure.gates.freeze
+              gates: region.closure.gates.freeze,
+              source: :struct_definition
             )
             pins.push nspin
 
@@ -32,7 +33,8 @@ module Solargraph
               location: get_node_location(node),
               closure: nspin,
               visibility: :private,
-              docstring: docstring
+              docstring: docstring,
+              source: :struct_definition
             )
 
             pins.push initialize_method_pin
@@ -43,7 +45,8 @@ module Solargraph
                   name: attribute_name,
                   decl: struct_definition_node.keyword_init? ? :kwarg : :arg,
                   location: get_node_location(attribute_node),
-                  closure: initialize_method_pin
+                  closure: initialize_method_pin,
+                  source: :struct_definition
                 )
               )
             end
@@ -53,29 +56,40 @@ module Solargraph
               [attribute_name, "#{attribute_name}="].each do |name|
                 docs = docstring.tags.find { |t| t.tag_name == 'param' && t.name == attribute_name }
 
+                attribute_type = ComplexType.parse(tag_string(docs))
+                return_type_comment = attribute_comment(docs, false)
+                param_comment = attribute_comment(docs, true)
+
                 method_pin = Pin::Method.new(
                   name: name,
                   parameters: [],
                   scope: :instance,
                   location: get_node_location(attribute_node),
                   closure: nspin,
+                  docstring: YARD::Docstring.new(return_type_comment),
                   # even assignments return the value
-                  comments: attribute_comment(docs, false),
-                  visibility: :public
+                  comments: return_type_comment,
+                  return_type: attribute_type,
+                  visibility: :public,
+                  source: :struct_definition
                 )
 
                 if name.end_with?('=')
                   method_pin.parameters << Pin::Parameter.new(
                     name: attribute_name,
                     location: get_node_location(attribute_node),
-                    closure: nspin,
-                    comments: attribute_comment(docs, true)
+                    closure: method_pin,
+                    return_type: attribute_type,
+                    comments: param_comment,
+                    source: :struct_definition
                   )
 
                   pins.push Pin::InstanceVariable.new(name: "@#{attribute_name}",
                                                       closure: method_pin,
                                                       location: get_node_location(attribute_node),
-                                                      comments: attribute_comment(docs, false))
+                                                      return_type: attribute_type,
+                                                      comments: "@type [#{attribute_type.rooted_tags}]",
+                                                      source: :struct_definition)
                 end
 
                 pins.push method_pin
@@ -121,13 +135,21 @@ module Solargraph
             Solargraph::Source.parse_docstring(struct_comments).to_docstring
           end
 
+          # @param tag [YARD::Tags::Tag, nil] The param tag for this attribute.xtract_
+          #
+          # @return [String]
+          def tag_string(tag)
+            tag&.types&.join(',') || 'undefined'
+          end
+
           # @param tag [YARD::Tags::Tag, nil] The param tag for this attribute. If nil, this method is a no-op
           # @param for_setter [Boolean] If true, will return a @param tag instead of a @return tag
+          #
           # @return [String] The formatted comment for the attribute
           def attribute_comment(tag, for_setter)
             return "" if tag.nil?
 
-            suffix = "[#{tag.types&.join(',') || 'undefined'}] #{tag.text}"
+            suffix = "[#{tag_string(tag)}] #{tag.text}"
 
             if for_setter
               "@param #{tag.name} #{suffix}"
