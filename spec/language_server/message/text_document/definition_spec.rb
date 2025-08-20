cb0178b13f6570daf310d8e9655060784ad02b04
Vince Broz <vince@broz.cc>
2025-08-20 17:28:47 -0400

    Reproduce bug with spec

--- spec/language_server/message/text_document/definition_spec.rb
+++ spec/language_server/message/text_document/definition_spec.rb
@@ -1,4 +1,33 @@
 describe Solargraph::LanguageServer::Message::TextDocument::Definition do
+  it 'prepares empty directory' do
+    Dir.mktmpdir do |dir|
+      host = Solargraph::LanguageServer::Host.new
+      test_rb_path = File.join(dir, 'test.rb')
+      thing_rb_path = File.join(dir, 'thing.rb')
+      FileUtils.cp('spec/fixtures/workspace/lib/other.rb', test_rb_path)
+      FileUtils.cp('spec/fixtures/workspace/lib/thing.rb', thing_rb_path)
+      host.prepare(dir)
+      sleep 0.1 until host.libraries.all?(&:mapped?)
+      host.catalog
+      file_uri = Solargraph::LanguageServer::UriHelpers.file_to_uri(test_rb_path)
+      other_uri = Solargraph::LanguageServer::UriHelpers.file_to_uri(thing_rb_path)
+      message = Solargraph::LanguageServer::Message::TextDocument::Definition
+                .new(host, {
+                       'params' => {
+                         'textDocument' => {
+                           'uri' => file_uri
+                         },
+                         'position' => {
+                           'line' => 4,
+                           'character' => 10
+                         }
+                       }
+                     })
+      message.process
+      expect(message.result.first[:uri]).to eq(other_uri)
+    end
+  end
+
   it 'finds definitions of methods' do
     host = Solargraph::LanguageServer::Host.new
     host.prepare('spec/fixtures/workspace')
