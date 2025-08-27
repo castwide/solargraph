be46aa30a4951813535e34f3b540fde75a5fb12f
Vince Broz <vince@broz.cc>
2025-08-27 16:26:40 -0400

    Add @sg-ignores

--- lib/solargraph/shell.rb
+++ lib/solargraph/shell.rb
@@ -239,16 +239,22 @@ module Solargraph
       puts "#{workspace.filenames.length} files total."
     end
 
+    # @sg-ignore Unresolved call to desc
     desc 'method_pin [PATH]', 'Describe a method pin'
+    # @sg-ignore Unresolved call to option
     option :rbs, type: :boolean, desc: 'Output the pin as RBS', default: false
+    # @sg-ignore Unresolved call to option
     option :typify, type: :boolean, desc: 'Output the calculated return type of the pin from annotations', default: false
+    # @sg-ignore Unresolved call to option
     option :probe, type: :boolean, desc: 'Output the calculated return type of the pin from annotations and inference', default: false
+    # @sg-ignore Unresolved call to option
     option :stack, type: :boolean, desc: 'Show entire stack by including definitions in superclasses', default: false
     # @param path [String] The path to the method pin, e.g. 'Class#method' or 'Class.method'
     # @return [void]
     def method_pin path
       api_map = Solargraph::ApiMap.load_with_cache('.', $stderr)
 
+      # @sg-ignore Unresolved call to options
       # @type [Array<Pin::Base>]
       pins = if options[:stack]
                scope, ns, meth = if path.include? '#'
@@ -269,9 +275,12 @@ module Solargraph
         exit 1
       end
       pins.each do |pin|
+        # @sg-ignore Unresolved call to options
         if options[:typify] || options[:probe]
           type = ComplexType::UNDEFINED
+          # @sg-ignore Unresolved call to options
           type = pin.typify(api_map) if options[:typify]
+          # @sg-ignore Unresolved call to options
           type = pin.probe(api_map) if options[:probe] && type.undefined?
           print_type(type)
           next
@@ -311,6 +320,7 @@ module Solargraph
     # @param type [ComplexType]
     # @return [void]
     def print_type(type)
+      # @sg-ignore Unresolved call to options
       if options[:rbs]
         puts type.to_rbs
       else
@@ -321,6 +331,7 @@ module Solargraph
     # @param pin [Solargraph::Pin::Base]
     # @return [void]
     def print_pin(pin)
+      # @sg-ignore Unresolved call to options
       if options[:rbs]
         puts pin.to_rbs
       else
