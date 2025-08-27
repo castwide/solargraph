449621fa35ce3dd51b88a4a991167c22c83a3dcf
Fred Snyder <fsnyder@castwide.com>
2025-08-27 08:16:50 -0400

    Revert "Resolve class aliases via Constant pins (#1029)"

--- lib/solargraph/rbs_map/conversions.rb
+++ lib/solargraph/rbs_map/conversions.rb
@@ -240,7 +240,6 @@ module Solargraph
       #
       # @return [Solargraph::Pin::Constant]
       def create_constant(name, tag, comments, decl, base = nil)
-        tag = "#{base}<#{tag}>" if base
         parts = name.split('::')
         if parts.length > 1
           name = parts.last
@@ -256,6 +255,7 @@ module Solargraph
           comments: comments,
           source: :rbs
         )
+        tag = "#{base}<#{tag}>" if base
         rooted_tag = ComplexType.parse(tag).force_rooted.rooted_tags
         constant_pin.docstring.add_tag(YARD::Tags::Tag.new(:return, '', rooted_tag))
         constant_pin
