d16301db0bab62c9865b7411fbf9baa1797e2380
Vince Broz <vince@broz.cc>
2025-09-02 11:02:30 -0400

    Revert workaround

--- lib/solargraph/parser/parser_gem/node_processors/sclass_node.rb
+++ lib/solargraph/parser/parser_gem/node_processors/sclass_node.rb
@@ -7,18 +7,9 @@ module Solargraph
         class SclassNode < Parser::NodeProcessor::Base
           def process
             sclass = node.children[0]
-            # @todo Changing Parser::AST::Node to AST::Node below will
-            #   cause type errors at strong level because the combined
-            #   pin for AST::Node#children has return type
-            #   "Array<AST::Node>, Array".  YARD annotations in AST
-            #   provided the Array, RBS for Array<AST::Node>.  We
-            #   should probably have a rule to combine "A, A<T>""
-            #   types to "A<T>" if the "A" comes from YARD, with the
-            #   rationale that folks tend to be less formal with types in
-            #   YARD.
-            if sclass.is_a?(::Parser::AST::Node) && sclass.type == :self
+            if sclass.is_a?(::AST::Node) && sclass.type == :self
               closure = region.closure
-            elsif sclass.is_a?(::Parser::AST::Node) && sclass.type == :casgn
+            elsif sclass.is_a?(::AST::Node) && sclass.type == :casgn
               names = [region.closure.namespace, region.closure.name]
               if sclass.children[0].nil? && names.last != sclass.children[1].to_s
                 names << sclass.children[1].to_s
@@ -27,7 +18,7 @@ module Solargraph
               end
               name = names.reject(&:empty?).join('::')
               closure = Solargraph::Pin::Namespace.new(name: name, location: region.closure.location, source: :parser)
-            elsif sclass.is_a?(::Parser::AST::Node) && sclass.type == :const
+            elsif sclass.is_a?(::AST::Node) && sclass.type == :const
               names = [region.closure.namespace, region.closure.name]
               also = NodeMethods.unpack_name(sclass)
               if also != region.closure.name
