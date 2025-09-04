efbe6eddba0a48ff8426f093309e441a3f54166d
Vince Broz <vince@broz.cc>
2025-09-03 23:12:22 -0400

    Fix merge issue

--- spec/language_server/host/message_worker_spec.rb
+++ spec/language_server/host/message_worker_spec.rb
@@ -2,8 +2,8 @@ describe Solargraph::LanguageServer::Host::MessageWorker do
   it "handle requests on queue" do
     host = double(Solargraph::LanguageServer::Host)
     message = {'method' => '$/example'}
-
     allow(host).to receive(:receive).with(message).and_return(nil)
+
     worker = Solargraph::LanguageServer::Host::MessageWorker.new(host)
     worker.queue(message)
     expect(worker.messages).to eq [message]
