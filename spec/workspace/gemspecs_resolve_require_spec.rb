e08b61bf97c86b51e92da687de16a3ad7dfdb1b9
Vince Broz <vince@broz.cc>
2025-09-05 09:31:12 -0400

    Mark another new spec as pending

--- spec/workspace/gemspecs_resolve_require_spec.rb
+++ spec/workspace/gemspecs_resolve_require_spec.rb
@@ -263,6 +263,8 @@ describe Solargraph::Workspace::Gemspecs, '#resolve_require' do
           end
 
           it 'returns the gemspec we do have' do
+            pending('https://github.com/castwide/solargraph/pull/1006')
+
             gemspecs = described_class.new(dir_path, preferences: preferences)
             specs = gemspecs.resolve_require('backport')
             backport = specs.find { |spec| spec.name == 'backport' }
