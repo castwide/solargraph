7c215108324ae9d5d625793ee413937e5e439a43
Vince Broz <vince@broz.cc>
2025-09-05 10:21:54 -0400

    Clean up RSpec/MessageSpies rubocop violations

--- spec/language_server/host/diagnoser_spec.rb
+++ spec/language_server/host/diagnoser_spec.rb
@@ -1,9 +1,10 @@
 describe Solargraph::LanguageServer::Host::Diagnoser do
   it "diagnoses on ticks" do
     host = double(Solargraph::LanguageServer::Host, options: { 'diagnostics' => true }, synchronizing?: false)
+    allow(host).to receive(:diagnose)
     diagnoser = Solargraph::LanguageServer::Host::Diagnoser.new(host)
     diagnoser.schedule 'file.rb'
-    expect(host).to receive(:diagnose).with('file.rb')
     diagnoser.tick
+    expect(host).to have_received(:diagnose).with('file.rb')
   end
 end
