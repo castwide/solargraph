5afe71b7d740cff980f4af051f702c08b254ed3b
Vince Broz <vince@broz.cc>
2025-09-05 10:43:02 -0400

    Fix up allow syntax

--- spec/language_server/host/message_worker_spec.rb
+++ spec/language_server/host/message_worker_spec.rb
@@ -2,12 +2,12 @@ describe Solargraph::LanguageServer::Host::MessageWorker do
   it "handle requests on queue" do
     host = double(Solargraph::LanguageServer::Host)
     message = {'method' => '$/example'}
-    allow(host).to receive(:receive)
+    allow(host).to receive(:receive).with(message).and_return(nil)
 
     worker = Solargraph::LanguageServer::Host::MessageWorker.new(host)
     worker.queue(message)
     expect(worker.messages).to eq [message]
     worker.tick
-    expect(host).to have_received(:receive).with(message).and_return(nil)
+    expect(host).to have_received(:receive).with(message)
   end
 end
