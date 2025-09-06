839b8c1eceb8b9d620ca793474a04b273dcf4deb
Fred Snyder <fsnyder@castwide.com>
2025-09-06 19:07:14 -0400

    Superclass documentation

--- lib/solargraph/pin/reference/superclass.rb
+++ lib/solargraph/pin/reference/superclass.rb
@@ -3,6 +3,8 @@
 module Solargraph
   module Pin
     class Reference
+      # A Superclass reference pin.
+      #
       class Superclass < Reference
         def reference_gates
           @reference_gates ||= closure.gates - [closure.path]
