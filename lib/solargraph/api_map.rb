b6d86ecb49631fc9201aa02e9e0626be85147c23
Vince Broz <vince@broz.cc>
2025-09-13 17:37:08 -0400

    Fix merge failure

--- lib/solargraph/api_map.rb
+++ lib/solargraph/api_map.rb
@@ -705,21 +705,6 @@ module Solargraph
       methods
     end
 
-    # @param fq_sub_tag [String]
-    # @return [String, nil]
-    def qualify_superclass fq_sub_tag
-      fq_sub_type = ComplexType.try_parse(fq_sub_tag)
-      fq_sub_ns = fq_sub_type.name
-      sup_tag = store.get_superclass(fq_sub_tag)
-      sup_type = ComplexType.try_parse(sup_tag)
-      sup_ns = sup_type.name
-      return nil if sup_tag.nil?
-      parts = fq_sub_ns.split('::')
-      last = parts.pop
-      parts.pop if last == sup_ns
-      qualify(sup_tag, parts.join('::'))
-    end
-
     private
 
     # A hash of source maps with filename keys.
