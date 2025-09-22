85be17da9c63cb69f88320959aa0793bfd9cab05
Vince Broz <vince@broz.cc>
2025-09-22 10:05:02 -0400

    Fix bad sexpr generation in op_asgn
    I'm not sure I could come up with a case where this matters in
    practice, but this was spotted in the solargraph-rails specs when I
    turned on assertions.  It was trying to parse:
    
    ```
    Rails.application.config.filter_parameters += [
      :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
    ]
    ```
    
    from Rails' filter_parameter_logging.rb
    
    https://github.com/iftheshoefritz/solargraph-rails/actions/runs/17915995139/job/50938306870?pr=173
    
    ```
           Expected method name to be a Symbol, got Parser::AST::Node for node s(:send,
             s(:send,
               s(:send,
                 s(:const, nil, :Rails), :application), :config),
             s(:send,
               s(:send,
                 s(:send,
                   s(:send,
                     s(:const, nil, :Rails), :application), :config), :filter_parameters), :+,
               s(:array,
                 s(:sym, :passw),
                 s(:sym, :email),
                 s(:sym, :secret),
                 s(:sym, :token),
                 s(:sym, :_key),
                 s(:sym, :crypt),
                 s(:sym, :salt),
                 s(:sym, :certificate),
                 s(:sym, :otp),
                 s(:sym, :ssn),
                 s(:sym, :cvv),
                 s(:sym, :cvc))))
    ```
    
    Blocks https://github.com/iftheshoefritz/solargraph-rails/pull/173

--- lib/solargraph/parser/parser_gem/node_processors/opasgn_node.rb
+++ lib/solargraph/parser/parser_gem/node_processors/opasgn_node.rb
@@ -9,14 +9,71 @@ module Solargraph
         class OpasgnNode < Parser::NodeProcessor::Base
           # @return [void]
           def process
-            # Parser::CurrentRuby.parse("a += 2")
-            # => s(:op_asgn,
-            #      s(:lvasgn, :a), :+,
-            #      s(:int, 2))
-            asgn = node.children[0]
-            variable_name = asgn.children[0]
+            target = node.children[0]
             operator = node.children[1]
             argument = node.children[2]
+            if target.type == :send
+              process_send_target(target, operator, argument)
+            elsif target.type.to_s.end_with?('vasgn')
+              process_vasgn_target(target, operator, argument)
+            else
+              Solargraph.assert_or_log(:opasgn_unknown_target,
+                                       "Unexpected op_asgn target type: #{target.type}")
+            end
+          end
+
+          # @param call [Parser::AST::Node] the target of the assignment
+          # @param operator [Symbol] the operator, e.g. :+
+          # @param argument [Parser::AST::Node] the argument of the operation
+          #
+          # @return [void]
+          def process_send_target(call, operator, argument)
+            # if target is a call:
+            # [10] pry(main)> Parser::CurrentRuby.parse("Foo.bar += baz")
+            # => s(:op_asgn,
+            #      s(:send, # call
+            #        s(:const, nil, :Foo), # calee
+            #        :bar), # call_method
+            #      :+, # operator
+            #      s(:send, nil, :baz)) # argument
+            # [11] pry(main)>
+            callee = call.children[0]
+            call_method = call.children[1]
+            asgn_method = "#{call_method}=".to_sym
+
+            # [8] pry(main)> Parser::CurrentRuby.parse("Foo.bar = Foo.bar + baz")
+            # => s(:send,
+            #       s(:const, nil, :Foo), # callee
+            #       :bar=, # asgn_method
+            #        s(:send,
+            #          s(:send,
+            #             s(:const, nil, :Foo), # callee
+            #             :bar), # call_method
+            #          :+, # operator
+            #          s(:send, nil, :baz))) # argument
+            new_send = node.updated(:send,
+                                    [callee,
+                                     asgn_method,
+                                     node.updated(:send, [call, operator, argument])])
+            NodeProcessor.process(new_send, region, pins, locals)
+          end
+
+          # @param asgn [Parser::AST::Node] the target of the assignment
+          # @param operator [Symbol] the operator, e.g. :+
+          # @param argument [Parser::AST::Node] the argument of the operation
+          #
+          # @return [void]
+          def process_vasgn_target(asgn, operator, argument)
+            # => s(:op_asgn,
+            #      s(:lvasgn, :a), # asgn
+            #      :+, # operator
+            #      s(:int, 2)) # argument
+
+            # @sg-ignore https://github.com/castwide/solargraph/pull/1005
+            # @type [Parser::AST::Node]
+            variable_name = asgn.children[0]
+            # @sg-ignore https://github.com/castwide/solargraph/pull/1005
+            STDERR.puts("variable_name is #{variable_name.inspect}")
             # for lvasgn, gvasgn, cvasgn, convert to lvar, gvar, cvar
             # [6] pry(main)> Parser::CurrentRuby.parse("a = a + 1")
             # => s(:lvasgn, :a,
@@ -25,14 +82,16 @@ module Solargraph
             #     s(:int, 1)))
             # [7] pry(main)>
             variable_reference_type = asgn.type.to_s.sub(/vasgn$/, 'var').to_sym
-            variable_reference = node.updated(variable_reference_type, asgn.children)
+            target_reference = node.updated(variable_reference_type, asgn.children)
             send_children = [
-              variable_reference,
+              target_reference,
               operator,
               argument
             ]
+            STDERR.puts("send_children is #{send_children.inspect}")
             send_node = node.updated(:send, send_children)
             new_asgn = node.updated(asgn.type, [variable_name,  send_node])
+            STDERR.puts("new_asgn: #{new_asgn}")
             NodeProcessor.process(new_asgn, region, pins, locals)
           end
         end
