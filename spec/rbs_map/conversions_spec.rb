6a7a37feedad44e0be8bf0fba8c5f9d4e9b83964
Vince Broz <vince@broz.cc>
2025-09-24 16:49:01 -0400

    Fix RuboCop issues

--- spec/rbs_map/conversions_spec.rb
+++ spec/rbs_map/conversions_spec.rb
@@ -52,6 +52,8 @@ describe Solargraph::RbsMap::Conversions do
     end
 
     context 'with self alias to self method' do
+      subject(:alias_pin) { api_map.get_method_stack('Foo', 'bar?', scope: :class).first }
+
       let(:rbs) do
         <<~RBS
           class Foo
@@ -63,11 +65,9 @@ describe Solargraph::RbsMap::Conversions do
 
       let(:method_pin) { api_map.get_method_stack('Foo', 'bar', scope: :class).first }
 
-      subject(:alias_pin) { api_map.get_method_stack('Foo', 'bar?', scope: :class).first }
+      it { is_expected.not_to be_nil }
 
-      it { should_not be_nil }
-
-      it { should be_instance_of(Solargraph::Pin::Method) }
+      it { is_expected.to be_instance_of(Solargraph::Pin::Method) }
 
       it 'finds the type' do
         expect(alias_pin.return_type.tag).to eq('String')
