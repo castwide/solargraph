4898313a80331277b733ce827962ff5eaadf5a16
Vince Broz <vince@broz.cc>
2025-09-25 16:03:39 -0400

    [regression] Fix resolution in deep YARD namespace hierarchies
    YARD-parsed namespaces weren't correctly setting their gates, leading
    to unresolved types from methods.

--- spec/yard_map/mapper_spec.rb
+++ spec/yard_map/mapper_spec.rb
@@ -74,6 +74,14 @@ describe Solargraph::YardMap::Mapper do
     expect(inc).to be_a(Solargraph::Pin::Reference::Include)
   end
 
+  it 'adds corect gates' do
+    # Asssuming the ast gem exists because it's a known dependency
+    pin = pins_with('ast').find do |pin|
+      pin.is_a?(Solargraph::Pin::Namespace) && pin.name == 'Mixin' && pin.closure.path == 'AST::Processor'
+    end
+    expect(pin.gates).to eq(["AST::Processor::Mixin", "AST::Processor", "AST", ""])
+  end
+
   it 'adds extend references' do
     # Asssuming the yard gem exists because it's a known dependency
     gemspec = Gem::Specification.find_by_name('yard')
