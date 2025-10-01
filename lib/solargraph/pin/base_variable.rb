dd6a6d9b9c28a3ea39bd8756b922988d189ce0ab
Vince Broz <vince@broz.cc>
2025-10-01 11:33:59 -0400

    Add a TODO

--- lib/solargraph/pin/base_variable.rb
+++ lib/solargraph/pin/base_variable.rb
@@ -131,6 +131,7 @@ module Solargraph
       # @return [ComplexType]
       def generate_complex_type
         tag = docstring.tag(:type)
+        # TODO this should be complaining about returning nil
         return ComplexType.try_parse(*tag.types) unless tag.nil? || tag.types.nil? || tag.types.empty?
         ComplexType.new
       end
