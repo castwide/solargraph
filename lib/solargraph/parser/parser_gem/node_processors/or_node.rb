188d4457fe6d9b0be8e31e945c4203cfbbfa88e8
Vince Broz <vince@broz.cc>
2025-10-01 09:48:46 -0400

    Refactor some flow sensitive typing boilerplate

--- lib/solargraph/parser/parser_gem/node_processors/or_node.rb
+++ lib/solargraph/parser/parser_gem/node_processors/or_node.rb
@@ -10,13 +10,6 @@ module Solargraph
           def process
             process_children
 
-            position = get_node_start_position(node)
-            # @sg-ignore
-            # @type [Solargraph::Pin::Breakable, nil]
-            enclosing_breakable_pin = pins.select{|pin| pin.is_a?(Pin::Breakable) && pin.location.range.contain?(position)}.last
-            # @sg-ignore downcast output of Enumerable#select
-            # @type [Pin::CompoundStatementable]
-            enclosing_compound_statement_pin = pins.select{|pin| pin.is_a?(Solargraph::Pin::CompoundStatementable) && pin.location.range.contain?(position)}.last
             FlowSensitiveTyping.new(locals,
                                     enclosing_breakable_pin,
                                     enclosing_compound_statement_pin).process_or(node)
