350b01ea34c253c099d88806875667cdfb9ef2b8
Vince Broz <vince@broz.cc>
2025-10-22 09:22:58 -0400

    Deal with issue seen in typechecking

--- lib/solargraph/parser/parser_gem/node_processors/def_node.rb
+++ lib/solargraph/parser/parser_gem/node_processors/def_node.rb
@@ -8,12 +8,13 @@ module Solargraph
           def process
             name = node.children[0].to_s
             scope = region.scope || (region.closure.is_a?(Pin::Singleton) ? :class : :instance)
+            method_binder = scope == :instance ? region.closure.binder.namespace_type : region.closure.binder
             methpin = Solargraph::Pin::Method.new(
               location: get_node_location(node),
               closure: region.closure,
               name: name,
-              context: region.closure.binder.namespace_type,
-              binder: region.closure.binder.namespace_type,
+              context: method_binder,
+              binder: method_binder,
               comments: comments_for(node),
               scope: scope,
               visibility: scope == :instance && name == 'initialize' ? :private : region.visibility,
