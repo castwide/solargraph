a2cffcc400b24cb4e96ea278233409b88f597e5b
Vince Broz <vince@broz.cc>
2025-11-02 08:26:24 -0500

    Fix tags

--- lib/solargraph/pin/base_variable.rb
+++ lib/solargraph/pin/base_variable.rb
@@ -49,7 +49,9 @@ module Solargraph
       # @param presence [Range]
       # @param exclude_return_type [ComplexType, nil]
       # @param intersection_return_type [ComplexType, nil]
-      # @param source [Symbol]
+      # @param source [::Symbol]
+      #
+      # @return [self]
       def downcast presence:, exclude_return_type: nil, intersection_return_type: nil,
                    source: self.source
         result = dup
@@ -195,6 +197,7 @@ module Solargraph
 
       attr_accessor :exclude_return_type, :intersection_return_type
 
+      # @return [Range]
       attr_writer :presence
 
       private
