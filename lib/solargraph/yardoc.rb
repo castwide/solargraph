939786ed30bce6718895f5cc1b5cfcb395fb4ac7
Vince Broz <vince@broz.cc>
2025-12-01 16:38:44 -0500

    Remove @sg-ignores

--- lib/solargraph/yardoc.rb
+++ lib/solargraph/yardoc.rb
@@ -23,7 +23,6 @@ module Solargraph
       yard_plugins.each { |plugin| cmd << " --plugin #{plugin}" }
       Solargraph.logger.debug { "Running: #{cmd}" }
       # @todo set these up to run in parallel
-      # @sg-ignore Unrecognized keyword argument chdir to Open3.capture2e
       stdout_and_stderr_str, status = Open3.capture2e(current_bundle_env_tweaks, cmd, chdir: gemspec.gem_dir)
       unless status.success?
         Solargraph.logger.warn { "YARD failed running #{cmd.inspect} in #{gemspec.gem_dir}" }
