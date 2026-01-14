fecbb8e3647b54bf3f5f496bbb73ea4988c9af41
Vince Broz <vince@broz.cc>
2026-01-13 21:31:17 -0500

    RuboCop manual fixes

--- lib/solargraph/source/chain/literal.rb
+++ lib/solargraph/source/chain/literal.rb
@@ -11,7 +11,7 @@ module Solargraph
         # @param type [String]
         # @param node [Parser::AST::Node, Object]
         def initialize type, node
-          super("<#{@type}>")
+          super("<#{type}>")
 
           if node.is_a?(::Parser::AST::Node)
             # @sg-ignore flow sensitive typing needs to narrow down type with an if is_a? check
