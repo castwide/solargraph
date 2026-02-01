75fd25287d2df1a82b85afa1f011396aba391acd
Vince Broz <vince@broz.cc>
2026-01-31 21:42:52 -0500

    Simplify version spec

--- spec/shell_spec.rb
+++ spec/shell_spec.rb
@@ -7,37 +7,11 @@ describe Solargraph::Shell do
   let(:shell) { described_class.new }
 
   describe '--version' do
-    let(:temp_dir) { Dir.mktmpdir }
-    let(:output) { bundle_exec('solargraph', '--version') }
-
-    before do
-      File.open(File.join(temp_dir, 'Gemfile'), 'w') do |file|
-        file.puts "source 'https://rubygems.org'"
-        file.puts "gem 'solargraph', path: '#{File.expand_path('..', __dir__)}'"
-      end
-      output, status = Open3.capture2e('bundle install', chdir: temp_dir)
-      raise "Failure installing bundle: #{output}" unless status.success?
-    end
-
-    # @type cmd [Array<String>]
-    # @return [String]
-    def bundle_exec(*cmd)
-      # run the command in the temporary directory with bundle exec
-      output, status = Open3.capture2e("bundle exec #{cmd.join(' ')}", chdir: temp_dir)
-      expect(status.success?).to be(true), "Command failed: #{output}"
-      output
-    end
-
-    after do
-      # remove the temporary directory after the tests
-      FileUtils.rm_rf(temp_dir)
-    end
-
-    it 'returns output' do
-      expect(output).not_to be_empty
-    end
-
     it 'returns a version when run' do
+      output = capture_stdout do
+        shell.version
+      end
+
       expect(output).to eq("#{Solargraph::VERSION}\n")
     end
   end
