b5bcd4b06402c9e8f9861c31d87d81aa69e86c90
Vince Broz <vince@broz.cc>
2026-02-06 11:49:46 -0700

    Debug

--- lib/solargraph/library.rb
+++ lib/solargraph/library.rb
@@ -587,6 +587,7 @@ module Solargraph
     def handle_file_not_found filename, error
       raise error unless workspace.source(filename)
       Solargraph.logger.debug "#{filename} is not cataloged in the ApiMap"
+      STDERR.puts "#{filename} is not cataloged in the ApiMap"
       nil
     end
 
