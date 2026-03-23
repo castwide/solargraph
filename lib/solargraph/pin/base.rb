6e4f2f72b03f5cd4d55135823d78a212ab18fa3e
Fred Snyder <fsnyder@castwide.com>
2026-03-23 13:14:35 -0400

    Unalias unique types

--- lib/solargraph/pin/base.rb
+++ lib/solargraph/pin/base.rb
@@ -568,9 +568,6 @@ module Solargraph
       # @param api_map [ApiMap]
       # @return [ComplexType, ComplexType::UniqueType]
       def typify api_map
-        unaliased = api_map.unalias(return_type.to_s)
-        return unaliased if unaliased
-
         return_type.qualify(api_map, *(closure&.gates || ['']))
       end
 
