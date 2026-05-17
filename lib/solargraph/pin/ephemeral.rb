ab39c231036316c4b616d38f134de97b5ff295ec
Fred Snyder <fsnyder@castwide.com>
2026-05-16 23:46:46 -0400

    Deprecate Ephemeral::ClassMethodSend

--- lib/solargraph/pin/ephemeral.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Solargraph
-  module Pin
-    module Ephemeral
-      autoload :ClassMethodSend, 'solargraph/pin/ephemeral/class_method_send'
-    end
-  end
-end
