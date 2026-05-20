1cbde41189d204ffbdf6d9662aa86047b26489ca
Fred Snyder <fsnyder@castwide.com>
2026-05-20 07:47:39 -0400

    RBS inheritance parameters

--- lib/solargraph/parser/parser_gem/node_processors/namespace_node.rb
+++ lib/solargraph/parser/parser_gem/node_processors/namespace_node.rb
@@ -8,22 +8,26 @@ module Solargraph
           include ParserGem::NodeMethods
 
           def process
-            superclass_name = nil
-            superclass_name = unpack_name(node.children[1]) if node.type == :class && node.children[1]&.type == :const
+            name = unpack_name(node.children[0])
+            comments = comments_for(node)
+            superclass_name = if node.type == :class
+              "#{type_from_node}#{parameters_from_inline_rbs}"
+            end
 
             loc = get_node_location(node)
             nspin = Solargraph::Pin::Namespace.new(
               type: node.type,
               location: loc,
               closure: region.closure,
-              name: unpack_name(node.children[0]),
-              comments: comments_for(node),
+              name: name,
+              comments: comments,
               visibility: :public,
               gates: region.closure.gates.freeze,
               source: :parser
             )
             pins.push nspin
-            unless superclass_name.nil?
+            Solargraph.logger.warn "Superclass: #{superclass_name}" if superclass_name&.start_with?('Array')
+            if superclass_name
               pins.push Pin::Reference::Superclass.new(
                 location: loc,
                 closure: pins.last,
@@ -32,6 +36,25 @@ module Solargraph
               )
             end
             process_children region.update(closure: nspin, visibility: :public)
+          end
+
+          private
+
+          # @param comments [String]
+          # @return [String, nil]
+          def parameters_from_inline_rbs
+            source = region.source.code_for(node)
+            match = source.match(/[^\n]*?#\s?+\[([^\]]*)/)
+            return unless match && match[1]
+
+            code = match[1].strip
+            return if code.empty?
+
+            "<#{code}>"
+          end
+
+          def type_from_node
+            unpack_name(node.children[1]) if node.children[1]&.type == :const
           end
         end
       end
