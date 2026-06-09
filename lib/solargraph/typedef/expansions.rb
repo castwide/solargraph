6e7c1eda2e879f682af3086447c8527dbf086a3f
Fred Snyder <fsnyder@castwide.com>
2026-06-08 23:01:43 -0400

    First iteration of macro expansions

--- lib/solargraph/typedef/expansions.rb
+++ lib/solargraph/typedef/expansions.rb
@@ -5,6 +5,7 @@ module Solargraph
     module Expansions
       autoload :Base,              'solargraph/typedef/expansions/base'
       autoload :Generics,          'solargraph/typedef/expansions/generics'
+      autoload :Macros,            'solargraph/typedef/expansions/macros'
       autoload :Self,              'solargraph/typedef/expansions/self'
 
       def self.expand(api_map, pin, receiver)
