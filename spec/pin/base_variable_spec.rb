22cd364d4dbf8308d216bac01f96d66895b3e141
Vince Broz <vince@broz.cc>
2026-09-01 19:23:30 -0400

    Cover presence in BaseVariable#eql?/#hash
    equality_fields includes presence alongside narrowed/exclude return
    type, but nothing exercised presence differing on otherwise-identical
    pins. Two LocalVariable pins sharing name/location but built with
    different presence ranges must stay distinct under eql?/hash, since
    flow-sensitive typing keys downcast copies on exactly this.
    
    Split from the 2026-08-04 integration branch's bundled undercover
    coverage commit 4aab8b2d7f94c8ed73151804cdcfcc289641ca3c.

--- spec/pin/base_variable_spec.rb
+++ spec/pin/base_variable_spec.rb
@@ -12,6 +12,21 @@ describe Solargraph::Pin::BaseVariable do
     expect(pin1).not_to eq(pin2)
   end
 
+  it 'includes presence and narrowed/exclude return type in #eql? and #hash' do
+    # Equality#eql?/#hash (used by Array#uniq, Set, and Hash keys) key off
+    # of #equality_fields. Flow-sensitive typing downcasts a pin into
+    # copies that share name/location/closure/source but differ in
+    # presence/narrowed_return_type/exclude_return_type - those copies
+    # must stay distinct as eql?/hash keys.
+    location = Solargraph::Location.new('test.rb', Solargraph::Range.from_to(0, 0, 1, 0))
+    presence1 = Solargraph::Range.from_to(0, 0, 0, 5)
+    presence2 = Solargraph::Range.from_to(1, 0, 1, 5)
+    pin1 = Solargraph::Pin::LocalVariable.new(name: 'foo', location: location, presence: presence1)
+    pin2 = Solargraph::Pin::LocalVariable.new(name: 'foo', location: location, presence: presence2)
+    expect(pin1.eql?(pin2)).to be(false)
+    expect(pin1.hash).not_to eq(pin2.hash)
+  end
+
   it 'infers types from variable assignments with unparenthesized parameters' do
     source = Solargraph::Source.load_string(%(
       class Container
