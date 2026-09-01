4ef603db4ab0987eb516ecb40611795c2105f8a7
Vince Broz <vince@broz.cc>
2026-09-01 10:16:49 -0400

    Drop changelog comment from gemspec_spec.rb
    Move the why into the example name and keep a bare issue link,
    per repo comment-style conventions.

--- spec/gemspec_spec.rb
+++ spec/gemspec_spec.rb
@@ -4,11 +4,8 @@ describe Gem::Specification do
   describe 'loaded from solargraph.gemspec' do
     let(:spec) { described_class.load(File.expand_path('../solargraph.gemspec', __dir__)) }
 
-    it 'does not package a top-level sig/ directory that RBS auto-discovers in installed gems' do
-      # A prior sig/shims directory collided with consumers' own RBS
-      # collections (see https://github.com/castwide/solargraph/issues/1144).
-      # The shims now live under rbs/shims instead, which RBS does not scan
-      # automatically.
+    it "does not package a top-level sig/ directory, which RBS auto-discovers in installed gems and would collide with consumers' own RBS collections" do
+      # https://github.com/castwide/solargraph/issues/1144
       expect(spec.files.grep(%r{^sig/})).to be_empty
     end
   end
