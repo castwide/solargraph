f53aa8b8f8c1739a369b7560a38cc26597d03335
Fred Snyder <fsnyder@castwide.com>
2026-09-23 05:53:06 -0400

    ApiMap equality adjusted for Chain inference cache

--- lib/solargraph/api_map.rb
+++ lib/solargraph/api_map.rb
@@ -15,6 +15,8 @@ module Solargraph
     autoload :Index,          'solargraph/api_map/index'
     autoload :Constants,      'solargraph/api_map/constants'
 
+    include Equality
+
     @@core_pins = Collection::Core.load
 
     # @param pins [Array<Solargraph::Pin::Base>]
@@ -1022,7 +1024,7 @@ module Solargraph
     #   that this overload of 'protected' will typecheck @sg-ignore
     # @sg-ignore
     def equality_fields
-      [self.class, @source_map_hash, conventions_environ, @external, @unresolved_requires, @loose_unions]
+      [self.class, @source_map_hash, conventions_environ, @external.pins, @unresolved_requires, @loose_unions]
     end
   end
 end
